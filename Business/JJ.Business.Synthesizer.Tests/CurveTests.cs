5483a8ccdd556bcdad9f501d0f938bcccd33f001
jjvanzon <janjoostvanzon@hotmail.com>
2015-03-05 22:01:07 +0000

    Synthesizer: Curve unit tests

--- Business/JJ.Business.Synthesizer.Tests/CurveTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/CurveTests.cs
@@ -1,4 +1,5 @@
 ﻿using JJ.Business.Synthesizer.Extensions;
+using JJ.Business.Synthesizer.LinkTo;
 using JJ.Business.Synthesizer.Enums;
 using JJ.Business.Synthesizer.Tests.Helpers;
 using JJ.Framework.Common;
@@ -28,7 +29,7 @@ namespace JJ.Business.Synthesizer.Tests
             {
                 INodeTypeRepository nodeTypeRepository = PersistenceHelper.CreateRepository<INodeTypeRepository>(context);
 
-                Curve curve = CreateCurve();
+                Curve curve = CreateTestCurve();
                 curve.Nodes.ForEach(x => x.SetNodeTypeEnum(NodeTypeEnum.Off, nodeTypeRepository));
 
                 var calculator = new CurveCalculator(curve);
@@ -47,7 +48,7 @@ namespace JJ.Business.Synthesizer.Tests
             {
                 INodeTypeRepository nodeTypeRepository = PersistenceHelper.CreateRepository<INodeTypeRepository>(context);
 
-                Curve curve = CreateCurve();
+                Curve curve = CreateTestCurve();
                 curve.Nodes.ForEach(x => x.SetNodeTypeEnum(NodeTypeEnum.Block, nodeTypeRepository));
 
                 var calculator = new CurveCalculator(curve);
@@ -66,7 +67,7 @@ namespace JJ.Business.Synthesizer.Tests
             {
                 INodeTypeRepository nodeTypeRepository = PersistenceHelper.CreateRepository<INodeTypeRepository>(context);
 
-                Curve curve = CreateCurve();
+                Curve curve = CreateTestCurve();
                 curve.Nodes.ForEach(x => x.SetNodeTypeEnum(NodeTypeEnum.Line, nodeTypeRepository));
 
                 var calculator = new CurveCalculator(curve);
@@ -80,66 +81,66 @@ namespace JJ.Business.Synthesizer.Tests
 
         // TODO: Port this code.
 
-        //[TestMethod]
-        //public void Test_Curve_MixedNodeTypes()
-        //{
-        //    var curve = new Curve() { Name = "Curve" };
+        [TestMethod]
+        public void Test_Curve_MixedNodeTypes()
+        {
+            using (IContext context = PersistenceHelper.CreateContext())
+            {
+                ICurveRepository curveRepository = PersistenceHelper.CreateRepository<ICurveRepository>(context);
+                INodeRepository nodeRepository = PersistenceHelper.CreateRepository<INodeRepository>(context);
+                INodeTypeRepository nodeTypeRepository = PersistenceHelper.CreateRepository<INodeTypeRepository>(context);
 
-        //    curve.Nodes.Add
-        //    (
-        //        new Node()
-        //        {
-        //            Time = 0,
-        //            Value = 0.5,
-        //            Type = NodeType.Off
-        //        },
-        //        new Node()
-        //        {
-        //            Time = 1,
-        //            Value = 2,
-        //            Type = NodeType.Block
-        //        },
-        //        new Node()
-        //        {
-        //            Time = 2,
-        //            Value = 1,
-        //            Type = NodeType.Line
-        //        },
-        //        new Node()
-        //        {
-        //            Time = 3,
-        //            Value = 1,
-        //            Type = NodeType.Off
-        //        }
-        //        // TODO: Test other node types too.
-        //    );
+                Curve curve = curveRepository.Create();
+                curve.Name = "Curve";
 
-        //    // Off
-        //    Assert.AreEqual(0.0, () => curve.Value(0.0));
-        //    Assert.AreEqual(0.0, () => curve.Value(0.5));
-        //    // Block
-        //    Assert.AreEqual(2.0, () => curve.Value(1.0));
-        //    Assert.AreEqual(2.0, () => curve.Value(1.5));
-        //    // Line
-        //    Assert.AreEqual(1.0, () => curve.Value(2.0));
-        //    Assert.AreEqual(0.5, () => curve.Value(2.5));
-        //    // Off
-        //    Assert.AreEqual(0.0, () => curve.Value(3.0));
-        //}
+                Node node1 = nodeRepository.Create();
+                node1.Time = 0;
+                node1.Value = 0.5;
+                node1.SetNodeTypeEnum(NodeTypeEnum.Off, nodeTypeRepository);
+                node1.LinkTo(curve);
 
-        //[TestMethod]
-        //public void Test_Curve_NullNode()
-        //{
-        //    Curve curve = CreateCurve();
-        //    curve.Nodes.ForEach(x => x.Type = NodeType.Line);
-        //    Assert.ThrowsException(() =>
-        //        curve.Nodes[1] = null,
-        //        "Node cannot be null.");
-        //}
+                Node node2 = nodeRepository.Create();
+                node2.Time = 1;
+                node2.Value = 2;
+                node2.SetNodeTypeEnum(NodeTypeEnum.Block, nodeTypeRepository);
+                node2.LinkTo(curve);
+
+                Node node3 = nodeRepository.Create();
+                node3.Time = 2;
+                node3.Value = 1;
+                node3.SetNodeTypeEnum(NodeTypeEnum.Line, nodeTypeRepository);
+                node3.LinkTo(curve);
+
+                Node node4 = nodeRepository.Create();
+                node4.Time = 3;
+                // Behavior is different in JJ code base compared to the Circle code base. 
+                // In the Circle code base, an Off node means value = 0.
+                // In the JJ code base, a previous linear node will use the non-zero the value of the Off node.
+                node4.Value = 0.5;
+                node4.SetNodeTypeEnum(NodeTypeEnum.Off, nodeTypeRepository);
+                node4.LinkTo(curve);
+
+                var calculator = new CurveCalculator(curve);
+
+                // Off
+                AssertHelper.AreEqual(0.00, () => calculator.CalculateValue(0.0));
+                AssertHelper.AreEqual(0.00, () => calculator.CalculateValue(0.5));
+                // Block
+                AssertHelper.AreEqual(2.00, () => calculator.CalculateValue(1.0));
+                AssertHelper.AreEqual(2.00, () => calculator.CalculateValue(1.5));
+                // Line
+                AssertHelper.AreEqual(1.00, () => calculator.CalculateValue(2.0));
+                // Behavior is different in JJ code base compared to the Circle code base. 
+                // See earilier comment.
+                AssertHelper.AreEqual(0.75, () => calculator.CalculateValue(2.5));
+                // Off
+                AssertHelper.AreEqual(0.00, () => calculator.CalculateValue(3.0));
+            }
+        }
 
         // Helpers
 
-        public static Curve CreateCurve()
+        public static Curve CreateTestCurve()
         {
             var curve = new Curve() { Name = "Curve" };
 
