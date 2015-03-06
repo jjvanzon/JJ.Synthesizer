e72ac842fe268a13aaf7fd2070f0d872c2719693
jjvanzon <janjoostvanzon@hotmail.com>
2015-03-06 00:09:35 +0000

    Synthesizer: Sample entity model: persistence layer finished

---
+++ Persistence/JJ.Persistence.Synthesizer.Memory/Mappings/ChannelSetupChannelTypeMapping.cs
@@ -0,0 +1,19 @@
+﻿using JJ.Persistence.Synthesizer.Memory.Helpers;
+using JJ.Framework.Persistence.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Persistence.Synthesizer.Memory.Mappings
+{
+    public class ChannelSetupChannelTypeMapping : MemoryMapping<ChannelSetupChannelType>
+    {
+        public ChannelSetupChannelTypeMapping()
+        {
+            IdentityType = IdentityType.AutoIncrement;
+            IdentityPropertyName = PropertyNames.ID;
+        }
+    }
+}
---
+++ Persistence/JJ.Persistence.Synthesizer.Memory/Repositories/ChannelSetupChannelTypeRepository.cs
@@ -0,0 +1,35 @@
+﻿using JJ.Framework.Persistence;
+using JJ.Persistence.Synthesizer.DefaultRepositories.Interfaces;
+using JJ.Persistence.Synthesizer.Memory.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Persistence.Synthesizer.Memory.Repositories
+{
+    public class ChannelSetupChannelTypeRepository : JJ.Persistence.Synthesizer.DefaultRepositories.ChannelSetupChannelTypeRepository
+    {
+        public ChannelSetupChannelTypeRepository(IContext context)
+            : base(context)
+        {
+            IChannelSetupRepository channelSetupRepository = new ChannelSetupRepository(context);
+            IChannelTypeRepository channelTypeRepository = new ChannelTypeRepository(context);
+
+            ChannelSetupChannelType entity;
+
+            entity = Create();
+            entity.ChannelSetup = channelSetupRepository.Get(EntityIDs.ENTITY_ID_CHANNEL_SETUP_MONO);
+            entity.ChannelType = channelTypeRepository.Get(EntityIDs.ENTITY_ID_CHANNEL_TYPE_SINGLE);
+
+            entity = Create();
+            entity.ChannelSetup = channelSetupRepository.Get(EntityIDs.ENTITY_ID_CHANNEL_SETUP_STEREO);
+            entity.ChannelType = channelTypeRepository.Get(EntityIDs.ENTITY_ID_CHANNEL_TYPE_LEFT);
+
+            entity = Create();
+            entity.ChannelSetup = channelSetupRepository.Get(EntityIDs.ENTITY_ID_CHANNEL_SETUP_STEREO);
+            entity.ChannelType = channelTypeRepository.Get(EntityIDs.ENTITY_ID_CHANNEL_TYPE_RIGHT);
+        }
+    }
+}
