b0a6162420e4ad2b20c49458f80e4480b743f3cd
jjvanzon <janjoostvanzon@hotmail.com>
2015-10-20 19:39:38 +0200

    Correct file names

--- Business/JJ.Business.Synthesizer/Calculation/Operators/Sine_OperatorCalculator.cs
+++ Business/JJ.Business.Synthesizer/Calculation/Operators/Sine_OperatorCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using JJ.Framework.Reflection.Exceptions;
 using System;
+using JJ.Framework.Mathematics;
 
 namespace JJ.Business.Synthesizer.Calculation.Operators
 {
@@ -7,6 +8,9 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
     {
         private OperatorCalculatorBase _volumeCalculator;
         private OperatorCalculatorBase _pitchCalculator;
+
+        private double _phase;
+        private double _previousTime;
 
         public Sine_OperatorCalculator(OperatorCalculatorBase volumeCalculator, OperatorCalculatorBase pitchCalculator)
         {
@@ -20,8 +24,14 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         public override double Calculate(double time, int channelIndex)
         {
             double volume = _volumeCalculator.Calculate(time, channelIndex);
-            double pitch = _pitchCalculator.Calculate(time, channelIndex); 
-            return volume * Math.Sin(2 * Math.PI * pitch * time);
+            double pitch = _pitchCalculator.Calculate(time, channelIndex);
+
+            double dt = time - _previousTime;
+            _phase = _phase + Maths.TWO_PI * dt * pitch;
+            _previousTime = time;
+
+            double value = volume * Math.Sin(_phase);
+            return value;
         }
     }
 }
---
+++ Business/JJ.Business.Synthesizer/Calculation/Operators/Sine_OperatorCalculator_Old.cs
@@ -0,0 +1,27 @@
+﻿//using JJ.Framework.Reflection.Exceptions;
+//using System;
+
+//namespace JJ.Business.Synthesizer.Calculation.Operators
+//{
+//    internal class Sine_OperatorCalculator : OperatorCalculatorBase
+//    {
+//        private OperatorCalculatorBase _volumeCalculator;
+//        private OperatorCalculatorBase _pitchCalculator;
+
+//        public Sine_OperatorCalculator(OperatorCalculatorBase volumeCalculator, OperatorCalculatorBase pitchCalculator)
+//        {
+//            if (volumeCalculator == null) throw new NullException(() => volumeCalculator);
+//            if (pitchCalculator == null) throw new NullException(() => pitchCalculator);
+
+//            _volumeCalculator = volumeCalculator;
+//            _pitchCalculator = pitchCalculator;
+//        }
+
+//        public override double Calculate(double time, int channelIndex)
+//        {
+//            double volume = _volumeCalculator.Calculate(time, channelIndex);
+//            double pitch = _pitchCalculator.Calculate(time, channelIndex);
+//            return volume * Math.Sin(2 * Math.PI * pitch * time);
+//        }
+//    }
+//}
