a350b0e738196c6b8361c98ada2ba27bae0e93ce
jjvanzon <janjoostvanzon@hotmail.com>
2015-11-12 23:09:43 +0100

    Synthesizer: Experiment some more with approximation of a sine with block interpolation, but it does not seem much faster.

--- Business/JJ.Business.Synthesizer/Calculation/SineCalculator.cs
+++ Business/JJ.Business.Synthesizer/Calculation/SineCalculator.cs
@@ -8,18 +8,16 @@ namespace JJ.Business.Synthesizer.Calculation
 {
     internal class SineCalculator
     {
-#if DEBUG
         private readonly int _samplesPerCycle;
-#endif
         private readonly double _samplesPerRadian;
         private readonly double[] _samples;
 
         public SineCalculator(int samplesPerCycle)
         {
             if (samplesPerCycle < 1) throw new LessThanException(() => samplesPerCycle, 1);
-#if DEBUG
+
             _samplesPerCycle = samplesPerCycle;
-#endif
+
             // The calculation requires an extra entry at the end.
             _samples = new double[samplesPerCycle + 1];
 
@@ -39,22 +37,39 @@ namespace JJ.Business.Synthesizer.Calculation
             _samplesPerRadian = samplesPerCycle / Maths.TWO_PI;
         }
 
+        //public double Sin(double angleInRadians)
+        //{
+        //    angleInRadians = angleInRadians % Maths.TWO_PI;
+        //    if (angleInRadians < 0.0)
+        //    {
+        //        angleInRadians += Maths.TWO_PI;
+        //    }
+
+        //    double i = angleInRadians * _samplesPerRadian;
+
+        //    int i0 = (int)i;
+
+        //    double x0 = _samples[i0];
+        //    double x1 = _samples[i0 + 1];
+
+        //    double x = x0 + (x1 - x0) * (i - i0);
+        //    return x;
+        //}
+
         public double Sin(double angleInRadians)
         {
-            angleInRadians = angleInRadians % Maths.TWO_PI;
-            if (angleInRadians < 0.0)
-            {
-                angleInRadians += Maths.TWO_PI;
-            }
-
             double i = angleInRadians * _samplesPerRadian;
+
+            i = i % _samplesPerCycle;
+
+            if (i < 0)
+            {
+                i = _samplesPerCycle - i;
+            }
 
             int i0 = (int)i;
 
-            double x0 = _samples[i0];
-            double x1 = _samples[i0 + 1];
-
-            double x = x0 + (x1 - x0) * (i - i0);
+            double x = _samples[i0];
             return x;
         }
     }
