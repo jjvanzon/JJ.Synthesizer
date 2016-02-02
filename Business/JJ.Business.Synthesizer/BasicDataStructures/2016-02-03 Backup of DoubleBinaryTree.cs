f12e144d0ff7d732dadffb937300d27727c98f13
jjvanzon <janjoostvanzon@hotmail.com>
2016-02-03 00:06:47 +0100

    Synthesizer: Refactor RedBlackTree data structure code found on the internet.

---
+++ Business/JJ.Business.Synthesizer/BasicDataStructures/2016-02-03 Backup of DoubleBinaryTree.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace JJ.Business.Synthesizer.Helpers
+//{
+//    internal class DoubleBinaryTree
+//    {
+//        private class Node
+//        {
+//            public Node Left { get; set; }
+//            public Node Right { get; set; }
+
+//            public double Value { get; set; }
+//        }
+
+//        private Node _root;
+
+//        public void Add(double value)
+//        {
+//            if (_root == null)
+//            {
+//                _root = new Node
+//                {
+//                    Value = value
+//                };
+//            }
+//            else
+//            {
+//                Node node = FindNode(value);
+//            }
+//        }
+
+//        private Node FindNode(double value)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
