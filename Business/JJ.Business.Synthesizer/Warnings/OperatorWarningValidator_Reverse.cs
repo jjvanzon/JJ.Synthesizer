8c9674b90bb73eca37b5643418e4a99a9cd0b6a8
jjvanzon <janjoostvanzon@hotmail.com>
2016-02-12 22:58:06 +0100

    Synthesizer: Reverse Operator: Make the speed inlet optional and become 1 by default.

--- Business/JJ.Business.Synthesizer/Warnings/OperatorWarningValidator_Reverse.cs
+++ Business/JJ.Business.Synthesizer/Warnings/OperatorWarningValidator_Reverse.cs
@@ -2,10 +2,10 @@
 
 namespace JJ.Business.Synthesizer.Warnings
 {
-    public class OperatorWarningValidator_Reverse : OperatorWarningValidator_Base_AllInletsFilled
+    public class OperatorWarningValidator_Reverse : OperatorWarningValidator_Base_FirstXInletsFilledIn
     {
         public OperatorWarningValidator_Reverse(Operator obj)
-            : base(obj)
+            : base(obj, inletCount: 1)
         { }
     }
 }
