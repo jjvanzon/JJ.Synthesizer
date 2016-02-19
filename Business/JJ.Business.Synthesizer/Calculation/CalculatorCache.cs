e482edd3e3a5ca59ec48107d06f7f9fd46a1ae8d
jjvanzon <janjoostvanzon@hotmail.com>
2016-02-19 21:50:38 +0100

    Synthesizer: Cache the array calculators for the cache operators, so that the same cached arrays of samples can be multiply reused within a patch, or multiply used in case of polyphony.

--- Business/JJ.Business.Synthesizer/Calculation/CalculatorCache.cs
+++ Business/JJ.Business.Synthesizer/Calculation/CalculatorCache.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JJ.Business.Synthesizer.Calculation.Arrays;
 using JJ.Business.Synthesizer.Calculation.Curves;
+using JJ.Business.Synthesizer.Calculation.Operators;
 using JJ.Business.Synthesizer.Calculation.Samples;
+using JJ.Business.Synthesizer.EntityWrappers;
+using JJ.Business.Synthesizer.Enums;
 using JJ.Business.Synthesizer.Helpers;
 using JJ.Data.Synthesizer;
+using JJ.Data.Synthesizer.DefaultRepositories.Interfaces;
 using JJ.Framework.Reflection.Exceptions;
 
 namespace JJ.Business.Synthesizer.Calculation
@@ -27,6 +30,9 @@ namespace JJ.Business.Synthesizer.Calculation
         /// </summary>
         private readonly Dictionary<Sample, ISampleCalculator> _sample_SampleCalculator_Dictionary = new Dictionary<Sample, ISampleCalculator>();
         private readonly object _sampleLock = new object();
+
+        private readonly Dictionary<int, ArrayCalculatorBase[]> _cacheOperatorID_To_ArrayCalculators_Dictionary = new Dictionary<int, ArrayCalculatorBase[]>();
+        private readonly object _cacheOperatorID_To_ArrayCalculators_Dictionary_Lock = new object();
 
         /// <summary> Caches several calculators for shared use between PatchCalculators, to save memory. </summary>
         public CalculatorCache()
@@ -70,6 +76,81 @@ namespace JJ.Business.Synthesizer.Calculation
 
                 return sampleCalculator;
             }
+        }
+
+        internal ArrayCalculatorBase[] GetCacheArrayCalculators(
+            Operator op, 
+            OperatorCalculatorBase signalCalculator,
+            ISpeakerSetupRepository speakerSetupRepository)
+        {
+            if (op == null) throw new NullException(() => op);
+            if (signalCalculator == null) throw new NullException(() => signalCalculator);
+            if (speakerSetupRepository == null) throw new NullException(() => speakerSetupRepository);
+
+            lock (_cacheOperatorID_To_ArrayCalculators_Dictionary_Lock)
+            {
+                ArrayCalculatorBase[] arrayCalculators;
+                if (!_cacheOperatorID_To_ArrayCalculators_Dictionary.TryGetValue(op.ID, out arrayCalculators))
+                {
+                    var wrapper = new Cache_OperatorWrapper(op);
+
+                    SpeakerSetup speakerSetup = speakerSetupRepository.Get((int)wrapper.SpeakerSetupEnum);
+                    int channelCount = speakerSetup.SpeakerSetupChannels.Count;
+
+                    arrayCalculators = CreateCacheArrayCalculators(
+                        signalCalculator,
+                        channelCount,
+                        wrapper.StartTime,
+                        wrapper.EndTime,
+                        wrapper.SamplingRate,
+                        wrapper.ResampleInterpolationTypeEnum);
+
+                    _cacheOperatorID_To_ArrayCalculators_Dictionary.Add(op.ID, arrayCalculators);
+                }
+
+                return arrayCalculators;
+            }
+        }
+
+        private ArrayCalculatorBase[] CreateCacheArrayCalculators(
+            OperatorCalculatorBase signalCalculator,
+            int channelCount, double startTime, double endTime, double rate,
+            ResampleInterpolationTypeEnum resampleInterpolationTypeEnum)
+        {
+            if (signalCalculator == null) throw new NullException(() => signalCalculator);
+            if (channelCount < 1) throw new LessThanException(() => channelCount, 1);
+            if (Double.IsNaN(endTime)) throw new NaNException(() => endTime);
+            if (Double.IsInfinity(endTime)) throw new InfinityException(() => endTime);
+            if (Double.IsNaN(startTime)) throw new NaNException(() => startTime);
+            if (Double.IsInfinity(startTime)) throw new InfinityException(() => startTime);
+            if (Double.IsNaN(rate)) throw new NaNException(() => rate);
+            if (Double.IsInfinity(rate)) throw new InfinityException(() => rate);
+            if (rate == 0.0) throw new ZeroException(() => rate);
+            if (endTime <= startTime) throw new LessThanOrEqualException(() => endTime, () => startTime);
+
+            double duration = endTime - startTime;
+            int tickCount = (int)(duration * rate) + 1;
+            double tickDuration = 1.0 / rate;
+
+            var arrayCalculators = new ArrayCalculatorBase[channelCount];
+
+            for (int channelIndex = 0; channelIndex < channelCount; channelIndex++)
+            {
+                double[] samples = new double[tickCount];
+                double time = startTime;
+                for (int i = 0; i < tickCount; i++)
+                {
+                    double sample = signalCalculator.Calculate(time, channelIndex);
+                    samples[i] = sample;
+
+                    time += tickDuration;
+                }
+
+                ArrayCalculatorBase arrayCalculator = ArrayCalculatorFactory.CreateArrayCalculator(samples, rate, startTime, resampleInterpolationTypeEnum);
+                arrayCalculators[channelIndex] = arrayCalculator;
+            }
+
+            return arrayCalculators;
         }
     }
 }
--- Business/JJ.Business.Synthesizer/Calculation/Patches/OptimizedPatchCalculatorVisitor.cs
+++ Business/JJ.Business.Synthesizer/Calculation/Patches/OptimizedPatchCalculatorVisitor.cs
@@ -340,65 +340,13 @@ namespace JJ.Business.Synthesizer.Calculation.Patches
             }
             else
             {
-                var wrapper = new Cache_OperatorWrapper(op);
-
-                SpeakerSetup speakerSetup = _speakerSetupRepository.Get((int)wrapper.SpeakerSetupEnum);
-                int channelCount = speakerSetup.SpeakerSetupChannels.Count;
-
-                // TODO: Cache the array calculators.
-                ArrayCalculatorBase[] arrayCalculators = CreateCacheArrayCalculators(
-                    signalCalculator,
-                    channelCount,
-                    wrapper.StartTime,
-                    wrapper.EndTime,
-                    wrapper.SamplingRate,
-                    wrapper.ResampleInterpolationTypeEnum);
+                ArrayCalculatorBase[] arrayCalculators = _calculatorCache.GetCacheArrayCalculators(
+                    op, signalCalculator, _speakerSetupRepository);
 
                 calculator = new Cache_OperatorCalculator(arrayCalculators);
             }
 
             _stack.Push(calculator);
-        }
-
-        private ArrayCalculatorBase[] CreateCacheArrayCalculators(
-            OperatorCalculatorBase signalCalculator,
-            int channelCount, double startTime, double endTime, double rate,
-            ResampleInterpolationTypeEnum resampleInterpolationTypeEnum)
-        {
-            if (signalCalculator == null) throw new NullException(() => signalCalculator);
-            if (channelCount < 1) throw new LessThanException(() => channelCount, 1);
-            if (Double.IsNaN(endTime)) throw new NaNException(() => endTime);
-            if (Double.IsInfinity(endTime)) throw new InfinityException(() => endTime);
-            if (Double.IsNaN(startTime)) throw new NaNException(() => startTime);
-            if (Double.IsInfinity(startTime)) throw new InfinityException(() => startTime);
-            if (Double.IsNaN(rate)) throw new NaNException(() => rate);
-            if (Double.IsInfinity(rate)) throw new InfinityException(() => rate);
-            if (rate == 0.0) throw new ZeroException(() => rate);
-            if (endTime <= startTime) throw new LessThanOrEqualException(() => endTime, () => startTime);
-
-            double duration = endTime - startTime;
-            int tickCount = (int)(duration * rate) + 1;
-            double tickDuration = 1.0 / rate;
-
-            var arrayCalculators = new ArrayCalculatorBase[channelCount];
-
-            for (int channelIndex = 0; channelIndex < channelCount; channelIndex++)
-            {
-                double[] samples = new double[tickCount];
-                double time = startTime;
-                for (int i = 0; i < tickCount; i++)
-                {
-                    double sample = signalCalculator.Calculate(time, channelIndex);
-                    samples[i] = sample;
-
-                    time += tickDuration;
-                }
-
-                ArrayCalculatorBase arrayCalculator = ArrayCalculatorFactory.CreateArrayCalculator(samples, rate, startTime, resampleInterpolationTypeEnum);
-                arrayCalculators[channelIndex] = arrayCalculator;
-            }
-
-            return arrayCalculators;
         }
 
         protected override void VisitCurveOperator(Operator op)
