fd232b2fae8a4ab780e820b6da29f4b5fa8886c0
jjvanzon <janjoostvanzon@hotmail.com>
2016-03-23 20:08:10 +0100

    Trivial things

--- Business/JJ.Business.Synthesizer/Helpers/PropertyNames.cs
+++ Business/JJ.Business.Synthesizer/Helpers/PropertyNames.cs
@@ -5,56 +5,6 @@ namespace JJ.Business.Synthesizer.Helpers
 {
     public static class PropertyNames
     {
-        // Operator Names
-        public const string Add = "Add";
-        public const string Adder = "Adder";
-        public const string Divide = "Divide";
-        public const string Multiply = "Multiply";
-        public const string PatchInlet = "PatchInlet";
-        public const string PatchOutlet = "PatchOutlet";
-        public const string Power = "Power";
-        public const string Sine = "Sine";
-        public const string Subtract = "Subtract";
-        public const string Delay = "Delay";
-        public const string SpeedUp = "SpeedUp";
-        public const string SlowDown = "SlowDown";
-        public const string TimePower = "TimePower";
-        public const string Earlier = "Earlier";
-        public const string Number = "Number";
-        public const string Sample = "Sample";
-
-        // Inlet Names
-
-        // Do not delete outcommented code: I might need it later again.
-
-        //public const string A = "A";
-        //public const string B = "B";
-        //public const string Origin = "Origin";
-        //public const string Numerator = "Numerator";
-        //public const string Denominator = "Denominator";
-        public const string Input = "Input";
-        //public const string Base = "Base";
-        //public const string Exponent = "Exponent";
-        //public const string Volume = "Volume";
-        //public const string Frequency = "Frequency";
-        //public const string PhaseShift = "PhaseShift";
-        //public const string Signal = "Signal";
-        //public const string TimeDifference = "TimeDifference";
-        //public const string Factor = "Factor";
-        //public const string Operand = "Operand";
-        //public const string Low = "Low";
-        //public const string High = "High";
-        //public const string Ratio = "Ratio";
-        //public const string Attack = "Attack";
-        //public const string Start = "Start";
-        //public const string Sustain = "Sustain";
-        //public const string End = "End";
-        //public const string Release = "Release";
-        //public const string Time = "Time";
-
-        // Outlet Names
-        public const string Result = "Result";
-        
         // Wav Header Properties
         public const string ChunkID = "ChunkID";
         public const string Format = "Format";
@@ -72,46 +22,40 @@ namespace JJ.Business.Synthesizer.Helpers
         // Other
         public const string AudioFileOutputs = "AudioFileOutputs";
         public const string Cents = "Cents";
-        public const string ChildDocument = "ChildDocument";
+        public const string ChildDocumentID = "ChildDocumentID";
         public const string Curve = "Curve";
         public const string Curves = "Curves";
-        public const string DefaultValue = "DefaultValue";
         public const string Document = "Document";
         public const string DocumentReference = "DocumentReference";
         public const string Documents = "Documents";
+        public const string EndTime = "EndTime";
         public const string Exponents = "Exponents";
         public const string Factors = "Factors";
+        public const string FilterType = "FilterType";
+        public const string FrequencyCount = "FrequencyCount";
         public const string ID = "ID";
         public const string Inlet = "Inlet";
         public const string InletCount = "InletCount";
         public const string Inlets = "Inlets";
-        public const string InletTypeEnum = "InletTypeEnum";
-        public const string InputOutlet = "InputOutlet";
+        public const string InterpolationType = "InterpolationType";
         public const string ListIndex = "ListIndex";
         public const string LiteralFrequencies = "LiteralFrequencies";
         public const string Name = "Name";
-        public const string Node = "Node";
-        public const string Nodes = "Nodes";
         public const string Outlet = "Outlet";
         public const string OutletCount = "OutletCount";
         public const string Outlets = "Outlets";
-        public const string OutletTypeEnum = "OutletTypeEnum";
-        public const string ParentDocument = "ParentDocument";
         public const string Patch = "Patch";
         public const string Patches = "Patches";
+        public const string PatchInlet = "PatchInlet";
+        public const string PatchOutlet = "PatchOutlet";
+        public const string Sample = "Sample";
+        public const string SampleCount = "SampleCount";
         public const string Samples = "Samples";
         public const string Scales = "Scales";
         public const string SemiTones = "SemiTones";
-        public const string UnderlyingPatch = "UnderlyingPatch";
-        public const string ChildDocumentID = "ChildDocumentID";
-        public const string StartTime = "StartTime";
-        public const string EndTime = "EndTime";
-        public const string FrequencyCount = "FrequencyCount";
-        public const string InterpolationTypeEnum = "InterpolationTypeEnum";
-        public const string TimeSliceDuration = "TimeSliceDuration";
-        public const string SampleCount = "SampleCount";
-        public const string InterpolationType = "InterpolationType";
         public const string SpeakerSetup = "SpeakerSetup";
-        public const string FilterType = "FilterType";
+        public const string StartTime = "StartTime";
+        public const string TimeSliceDuration = "TimeSliceDuration";
+        public const string UnderlyingPatch = "UnderlyingPatch";
     }
 }
