9d5752b0bb6f57b1b80179c91ceeab897e5f752a
jjvanzon <janjoostvanzon@hotmail.com>
2016-05-21 00:32:51 +0200

    FAULTY: Synthesizer: Sounds are still incorrect after invariant dimension stack indices. Gradually yielding over dimension transformation in OperatorCalculator Reset and phase tracking only for time from the '668 + changes' version to the Latest Version.

--- Business/JJ.Business.Synthesizer/Calculation/Operators/Triangle OperatorCalculator Classes.cs
+++ Business/JJ.Business.Synthesizer/Calculation/Operators/Triangle OperatorCalculator Classes.cs
@@ -3,18 +3,140 @@ using System.Runtime.CompilerServices;
 
 namespace JJ.Business.Synthesizer.Calculation.Operators
 {
-    internal class Triangle_OperatorCalculator_ConstFrequency_ConstPhaseShift : OperatorCalculatorBase
+    // TODO: Program variations without phase shift.
+    // Phase shift checks are for that reason temporarily commented out.
+
+    internal class Triangle_OperatorCalculator_ConstFrequency_ConstPhaseShift_WithOriginShifting : OperatorCalculatorBase
     {
         private readonly double _frequency;
         private readonly double _phaseShift;
         private readonly DimensionStack _dimensionStack;
         private readonly int _dimensionStackIndex;
 
-        public Triangle_OperatorCalculator_ConstFrequency_ConstPhaseShift(
+        private double _origin;
+
+        public Triangle_OperatorCalculator_ConstFrequency_ConstPhaseShift_WithOriginShifting(
+            double frequency,
+            double phaseShift,
+            DimensionStack dimensionStack)
+        {
+            OperatorCalculatorHelper.AssertFrequency(frequency);
+            //OperatorCalculatorHelper.AssertPhaseShift(phaseShift);
+            OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
+
+            _frequency = frequency;
+            _phaseShift = phaseShift;
+            _dimensionStack = dimensionStack;
+            _dimensionStackIndex = dimensionStack.CurrentIndex;
+
+            // Correct the phase shift, because our calculation starts with value -1, but in practice you want to start at value 0 going up.
+            _phaseShift += 0.25;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override double Calculate()
+        {
+            double position = _dimensionStack.Get(_dimensionStackIndex);
+
+            double shiftedPhase = (position - _origin) * _frequency + _phaseShift;
+            double relativePhase = shiftedPhase % 1.0;
+            if (relativePhase < 0.5)
+            {
+                // Starts going up at a rate of 2 up over 1/2 a cycle.
+                double value = -1.0 + 4.0 * relativePhase;
+                return value;
+            }
+            else
+            {
+                // And then going down at phase 1/2.
+                // (Extending the line to x = 0 it ends up at y = 3.)
+                double value = 3.0 - 4.0 * relativePhase;
+                return value;
+            }
+        }
+
+        public override void Reset()
+        {
+            _origin = _dimensionStack.Get();
+
+            base.Reset();
+        }
+    }
+
+    internal class Triangle_OperatorCalculator_ConstFrequency_VarPhaseShift_WithOriginShifting : OperatorCalculatorBase_WithChildCalculators
+    {
+        private readonly double _frequency;
+        private readonly OperatorCalculatorBase _phaseShiftCalculator;
+        private readonly DimensionStack _dimensionStack;
+        private readonly int _dimensionStackIndex;
+
+        private double _origin;
+
+        public Triangle_OperatorCalculator_ConstFrequency_VarPhaseShift_WithOriginShifting(
+            double frequency,
+            OperatorCalculatorBase phaseShiftCalculator,
+            DimensionStack dimensionStack)
+            : base(new OperatorCalculatorBase[] { phaseShiftCalculator })
+        {
+            OperatorCalculatorHelper.AssertFrequency(frequency);
+            OperatorCalculatorHelper.AssertOperatorCalculatorBase(phaseShiftCalculator, () => phaseShiftCalculator);
+            OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
+
+            _frequency = frequency;
+            _phaseShiftCalculator = phaseShiftCalculator;
+            _dimensionStack = dimensionStack;
+            _dimensionStackIndex = dimensionStack.CurrentIndex;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override double Calculate()
+        {
+            double position = _dimensionStack.Get(_dimensionStackIndex);
+            double phaseShift = _phaseShiftCalculator.Calculate();
+
+            double shiftedPhase = (position - _origin) * _frequency + phaseShift;
+
+            // Correct the phase, because our calculation starts with value -1, but in practice you want to start at value 0 going up.
+            shiftedPhase += 0.25;
+
+            double relativePhase = shiftedPhase % 1.0;
+            if (relativePhase < 0.5)
+            {
+                // Starts going up at a rate of 2 up over 1/2 a cycle.
+                double value = -1.0 + 4.0 * relativePhase;
+                return value;
+            }
+            else
+            {
+                // And then going down at phase 1/2.
+                // (Extending the line to x = 0 it ends up at y = 3.)
+                double value = 3.0 - 4.0 * relativePhase;
+                return value;
+            }
+        }
+
+        public override void Reset()
+        {
+            _origin = _dimensionStack.Get();
+
+            base.Reset();
+        }
+    }
+
+    internal class Triangle_OperatorCalculator_ConstFrequency_ConstPhaseShift_NoOriginShifting : OperatorCalculatorBase
+    {
+        private readonly double _frequency;
+        private readonly double _phaseShift;
+        private readonly DimensionStack _dimensionStack;
+        private readonly int _dimensionStackIndex;
+
+        public Triangle_OperatorCalculator_ConstFrequency_ConstPhaseShift_NoOriginShifting(
             double frequency, 
             double phaseShift,
             DimensionStack dimensionStack)
         {
+            OperatorCalculatorHelper.AssertFrequency(frequency);
+            //OperatorCalculatorHelper.AssertPhaseShift(phaseShift);
             OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
 
             _frequency = frequency;
@@ -49,19 +171,20 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         }
     }
 
-    internal class Triangle_OperatorCalculator_ConstFrequency_VarPhaseShift : OperatorCalculatorBase_WithChildCalculators
+    internal class Triangle_OperatorCalculator_ConstFrequency_VarPhaseShift_NoOriginShifting : OperatorCalculatorBase_WithChildCalculators
     {
         private readonly double _frequency;
         private readonly OperatorCalculatorBase _phaseShiftCalculator;
         private readonly DimensionStack _dimensionStack;
         private readonly int _dimensionStackIndex;
 
-        public Triangle_OperatorCalculator_ConstFrequency_VarPhaseShift(
+        public Triangle_OperatorCalculator_ConstFrequency_VarPhaseShift_NoOriginShifting(
             double frequency,
             OperatorCalculatorBase phaseShiftCalculator,
             DimensionStack dimensionStack)
             : base(new OperatorCalculatorBase[] { phaseShiftCalculator })
         {
+            OperatorCalculatorHelper.AssertFrequency(frequency);
             OperatorCalculatorHelper.AssertOperatorCalculatorBase(phaseShiftCalculator, () => phaseShiftCalculator);
             OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
 
@@ -75,8 +198,8 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         public override double Calculate()
         {
             double position = _dimensionStack.Get(_dimensionStackIndex);
-
             double phaseShift = _phaseShiftCalculator.Calculate();
+
             double phase = position * _frequency + phaseShift;
 
             // Correct the phase, because our calculation starts with value -1, but in practice you want to start at value 0 going up.
@@ -99,7 +222,7 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         }
     }
 
-    internal class Triangle_OperatorCalculator_VarFrequency_ConstPhaseShift : OperatorCalculatorBase_WithChildCalculators
+    internal class Triangle_OperatorCalculator_VarFrequency_ConstPhaseShift_WithPhaseTracking : OperatorCalculatorBase_WithChildCalculators
     {
         private readonly OperatorCalculatorBase _frequencyCalculator;
         private readonly DimensionStack _dimensionStack;
@@ -108,18 +231,18 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         private double _phase;
         private double _previousPosition;
 
-        public Triangle_OperatorCalculator_VarFrequency_ConstPhaseShift(
+        public Triangle_OperatorCalculator_VarFrequency_ConstPhaseShift_WithPhaseTracking(
             OperatorCalculatorBase frequencyCalculator,
             double phaseShift,
             DimensionStack dimensionStack)
             : base(new OperatorCalculatorBase[] { frequencyCalculator })
         {
             OperatorCalculatorHelper.AssertOperatorCalculatorBase(frequencyCalculator, () => frequencyCalculator);
+            //OperatorCalculatorHelper.AssertPhaseShift(phaseShift);
             OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
 
             _frequencyCalculator = frequencyCalculator;
 
-            // TODO: Assert so it does not become NaN.
             _phase = phaseShift;
             _dimensionStack = dimensionStack;
             _dimensionStackIndex = dimensionStack.CurrentIndex;
@@ -168,7 +291,7 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         }
     }
 
-    internal class Triangle_OperatorCalculator_VarFrequency_VarPhaseShift : OperatorCalculatorBase_WithChildCalculators
+    internal class Triangle_OperatorCalculator_VarFrequency_VarPhaseShift_WithPhaseTracking : OperatorCalculatorBase_WithChildCalculators
     {
         private readonly OperatorCalculatorBase _frequencyCalculator;
         private readonly OperatorCalculatorBase _phaseShiftCalculator;
@@ -178,7 +301,7 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         private double _phase;
         private double _previousPosition;
 
-        public Triangle_OperatorCalculator_VarFrequency_VarPhaseShift(
+        public Triangle_OperatorCalculator_VarFrequency_VarPhaseShift_WithPhaseTracking(
             OperatorCalculatorBase frequencyCalculator,
             OperatorCalculatorBase phaseShiftCalculator,
             DimensionStack dimensionStack)
@@ -236,6 +359,111 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
             _phase = 0.0;
 
             base.Reset();
+        }
+    }
+
+    internal class Triangle_OperatorCalculator_VarFrequency_ConstPhaseShift_NoPhaseTracking : OperatorCalculatorBase_WithChildCalculators
+    {
+        private readonly OperatorCalculatorBase _frequencyCalculator;
+        private readonly DimensionStack _dimensionStack;
+        private readonly int _dimensionStackIndex;
+        private readonly double _phaseShiftPlusQuarter;
+
+        public Triangle_OperatorCalculator_VarFrequency_ConstPhaseShift_NoPhaseTracking(
+            OperatorCalculatorBase frequencyCalculator,
+            double phaseShift,
+            DimensionStack dimensionStack)
+            : base(new OperatorCalculatorBase[] { frequencyCalculator })
+        {
+            OperatorCalculatorHelper.AssertOperatorCalculatorBase(frequencyCalculator, () => frequencyCalculator);
+            //OperatorCalculatorHelper.AssertPhaseShift(phaseShift);
+            OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
+
+            _frequencyCalculator = frequencyCalculator;
+
+            _dimensionStack = dimensionStack;
+            _dimensionStackIndex = dimensionStack.CurrentIndex;
+
+            // Correct the phase, because our calculation starts with value -1, but in practice you want to start at value 0 going up.
+            _phaseShiftPlusQuarter = phaseShift + 0.25;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override double Calculate()
+        {
+            double position = _dimensionStack.Get(_dimensionStackIndex);
+            double frequency = _frequencyCalculator.Calculate();
+
+            double shiftedPhase = position * frequency + _phaseShiftPlusQuarter;
+
+            double value;
+            double relativePhase = shiftedPhase % 1.0;
+            if (relativePhase < 0.5)
+            {
+                // Starts going up at a rate of 2 up over 1/2 a cycle.
+                value = -1.0 + 4.0 * relativePhase;
+            }
+            else
+            {
+                // And then going down at phase 1/2.
+                // (Extending the line to x = 0 it ends up at y = 3.)
+                value = 3.0 - 4.0 * relativePhase;
+            }
+
+            return value;
+        }
+    }
+
+    internal class Triangle_OperatorCalculator_VarFrequency_VarPhaseShift_NoPhaseTracking : OperatorCalculatorBase_WithChildCalculators
+    {
+        private readonly OperatorCalculatorBase _frequencyCalculator;
+        private readonly OperatorCalculatorBase _phaseShiftCalculator;
+        private readonly DimensionStack _dimensionStack;
+        private readonly int _dimensionStackIndex;
+
+        public Triangle_OperatorCalculator_VarFrequency_VarPhaseShift_NoPhaseTracking(
+            OperatorCalculatorBase frequencyCalculator,
+            OperatorCalculatorBase phaseShiftCalculator,
+            DimensionStack dimensionStack)
+            : base(new OperatorCalculatorBase[] { frequencyCalculator, phaseShiftCalculator })
+        {
+            OperatorCalculatorHelper.AssertOperatorCalculatorBase(frequencyCalculator, () => frequencyCalculator);
+            OperatorCalculatorHelper.AssertOperatorCalculatorBase(phaseShiftCalculator, () => phaseShiftCalculator);
+            OperatorCalculatorHelper.AssertDimensionStack_ForReaders(dimensionStack);
+
+            _frequencyCalculator = frequencyCalculator;
+            _phaseShiftCalculator = phaseShiftCalculator;
+            _dimensionStack = dimensionStack;
+            _dimensionStackIndex = dimensionStack.CurrentIndex;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override double Calculate()
+        {
+            double position = _dimensionStack.Get(_dimensionStackIndex);
+            double frequency = _frequencyCalculator.Calculate();
+            double phaseShift = _phaseShiftCalculator.Calculate();
+
+            double shiftedPhase = position * frequency + phaseShift;
+
+            // Correct the phase with 0.25, because our calculation starts with value -1, but in practice you want to start at value 0 going up.
+            shiftedPhase += 0.25;
+
+            double relativePhase = shiftedPhase % 1.0;
+            double value;
+            if (relativePhase < 0.5)
+            {
+                // Starts going up at a rate of 2 up over 1/2 a cycle.
+                value = -1.0 + 4.0 * relativePhase;
+            }
+            else
+            {
+                // And then going down at phase 1/2.
+                // (Extending the line to x = 0 it ends up at y = 3.)
+                value = 3.0 - 4.0 * relativePhase;
+            }
+
+            return value;
         }
     }
 }
