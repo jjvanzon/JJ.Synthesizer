bbeb2830b8b02d9cf30bed8244a3baf7a5bc53ae
jjvanzon <janjoostvanzon@hotmail.com>
2016-06-08 00:36:44 +0200

    Synthesizer: Range Operator: Simplified the calculation, but then realized the range is extrapolated linearly out of the bounds.

--- Business/JJ.Business.Synthesizer/Calculation/Operators/Range_OperatorCalculator.cs
+++ Business/JJ.Business.Synthesizer/Calculation/Operators/Range_OperatorCalculator.cs
@@ -31,33 +31,17 @@ namespace JJ.Business.Synthesizer.Calculation.Operators
         public override double Calculate()
         {
             // Example:
-            // position { 0, 1, 2 } => value { 0.5, 2.25, 4 }
+            // index { 0, 1, 2 } => value { 0.5, 2.25, 4 }
 
-            // TODO: This calculation should be simplified to something like this,
-            // but then correct:
-            //double valueNonRounded = from + (destRange / step) * position;
-
-            double position = _dimensionStack.Get();
             double from = _fromCalculator.Calculate();
             double till = _tillCalculator.Calculate();
             double step = _stepCalculator.Calculate();
+            double index = _dimensionStack.Get();
 
-            double targetValueA = from;
-            double targetValueB = till;
-            double targetRange = targetValueB - targetValueA;
+            double nonRounded = from + index * step;
+            double rounded = Maths.RoundWithStep(nonRounded, step);
 
-            double count = targetRange / step;
-
-            double sourceRange = count;
-            double sourceValueA = 0;
-            double sourceValueB = count;
-
-            double between0And1 = (position - sourceValueA) / sourceRange;
-            double valueNonRounded = between0And1 * targetRange + targetValueA;
-
-            double valueRounded = Maths.RoundWithStep(valueNonRounded, step);
-
-            return valueRounded;
+            return rounded;
         }
     }
 }
