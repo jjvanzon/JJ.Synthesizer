063f9bd7d84d71c6c8d4798edf398fab3a47d188
jjvanzon <janjoostvanzon@hotmail.com>
2016-12-10 21:09:07 +0100

    PreProcessing_OperatorDtoVisitor

---
+++ Business/Synthesizer/Visitors/PreProcessing_OperatorDtoVisitor.cs
@@ -0,0 +1,26 @@
+﻿using JJ.Business.Synthesizer.Dto;
+using JJ.Framework.Reflection.Exceptions;
+
+namespace JJ.Business.Synthesizer.Visitors
+{
+    internal class PreProcessing_OperatorDtoVisitor
+    {
+        private readonly int _targetChannelCount;
+
+        public PreProcessing_OperatorDtoVisitor(int targetChannelCount)
+        {
+            _targetChannelCount = targetChannelCount;
+        }
+
+        public OperatorDtoBase Execute(OperatorDtoBase dto)
+        {
+            if (dto == null) throw new NullException(() => dto);
+
+            dto = new ClassSpecialization_OperatorDtoVisitor(_targetChannelCount).Execute(dto);
+            dto = new MathSimplification_OperatorDtoVisitor().Execute(dto);
+            dto = new MachineOptimization_OperatorDtoVisitor().Execute(dto);
+
+            return dto;
+        }
+    }
+}
