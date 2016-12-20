878d16eb24f4e49ff7ba7b8f2d11205a44f98233
jjvanzon <janjoostvanzon@hotmail.com>
2016-12-20 23:19:36 +0100

    Synthesizer: Refactor Away Bundle / Unbundle: Register the data migrations in the SQL upgrade scripts.

---
+++ Database/SynthesizerDB/2016-12-20 010 SynthesizerDB RUN UTILITY.sql
@@ -0,0 +1,3 @@
+RAISERROR (
+'RUN THE C# UTILITY: JJ.OneOff.Synthesizer.DataMigration.
+Select the option DeleteOrphanedEntityPositions.', 11, -1); 
---
+++ Database/SynthesizerDB/2016-12-20 020 SynthesizerDB RUN UTILITY.sql
@@ -0,0 +1,3 @@
+RAISERROR (
+'RUN THE C# UTILITY: JJ.OneOff.Synthesizer.DataMigration.
+Select the option Migrate_Bundle_AndUnbundle_ToInletsToDimension_AndDimensionToOutlets.', 11, -1); 
