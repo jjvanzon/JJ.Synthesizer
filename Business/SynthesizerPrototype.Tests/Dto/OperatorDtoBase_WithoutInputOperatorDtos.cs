d69a7df452943c1c30b63307230447cdee20a2c0
jjvanzon <janjoostvanzon@hotmail.com>
2016-12-27 23:54:16 +0100

    Synthesizer: Split up SynthesizerPrototype into multiple projects (in progress)

--- Business/SynthesizerPrototype.Tests/Dto/OperatorDtoBase_WithoutInputOperatorDtos.cs
+++ Business/SynthesizerPrototype.Tests/Dto/OperatorDtoBase_WithoutInputOperatorDtos.cs
@@ -5,6 +5,6 @@ namespace JJ.Business.SynthesizerPrototype.Tests.Dto
 {
     internal abstract class OperatorDtoBase_WithoutInputOperatorDtos : OperatorDtoBase
     {
-        public override IList<OperatorDtoBase> InputOperatorDtos { get; set; } = Array.Empty<OperatorDtoBase>();
+        public override IList<OperatorDtoBase> InputOperatorDtos { get; set; } = new OperatorDtoBase[0];
     }
 }
