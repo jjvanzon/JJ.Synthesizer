88c01323f8c3c4be4c08d5192adef8bfe6821a72
jjvanzon <janjoostvanzon@hotmail.com>
2017-08-06 21:26:31 +0200

    Synthesizer: Nano-Optimization: Reuse Results: Split up OperatorDtoVisitor_OperationIdentities and OperatorDtoVisitor_Deduplication. Program OperatorDtoVisitor_Deduplication (not debugged).

---
+++ Business/Synthesizer/Visitors/OperatorDtoVisitor_Deduplication.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using JJ.Business.Synthesizer.Dto;
+
+namespace JJ.Business.Synthesizer.Visitors
+{
+    /// <summary> OperatorDtoVisitor_AssignOperationIdentities must have run first. </summary>
+    internal class OperatorDtoVisitor_Deduplication : OperatorDtoVisitorBase
+    {
+        private Dictionary<string, IOperatorDto> _dictionary;
+
+        public IOperatorDto Execute(IOperatorDto dto)
+        {
+            _dictionary = new Dictionary<string, IOperatorDto>();
+
+            dto = Visit_OperatorDto_Polymorphic(dto);
+
+            return dto;
+        }
+
+        protected override IOperatorDto Visit_OperatorDto_Polymorphic(IOperatorDto dto)
+        {
+            string operationIdentity = dto.OperationIdentity;
+            if (_dictionary.TryGetValue(operationIdentity, out IOperatorDto existingDto))
+            {
+                return existingDto;
+            }
+            else
+            {
+                IOperatorDto dto2 = base.Visit_OperatorDto_Polymorphic(dto);
+                _dictionary.Add(operationIdentity, dto2);
+                return dto2;
+            }
+        }
+    }
+}
---
+++ Business/Synthesizer/Visitors/OperatorDtoVisitor_OperationIdentities.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JJ.Business.Synthesizer.Dto;
+using JJ.Business.Synthesizer.Enums;
+
+namespace JJ.Business.Synthesizer.Visitors
+{
+    internal class OperatorDtoVisitor_OperationIdentities : OperatorDtoVisitorBase
+    {
+        private const string OPERATION_IDENTITY_PATH_SEPARATOR = "$$";
+        private const char OPERATION_IDENTITY_KEY_PART_SEPARATOR = '|';
+
+        private Stack<string> _operationIdentityPartStack;
+
+        public IOperatorDto Execute(IOperatorDto dto)
+        {
+            _operationIdentityPartStack = new Stack<string>();
+
+            return Visit_OperatorDto_Polymorphic(dto);
+        }
+
+        protected override IOperatorDto Visit_OperatorDto_Polymorphic(IOperatorDto dto)
+        {
+            string operationIdentityPart = FormatIdentityPart(dto);
+
+            _operationIdentityPartStack.Push(operationIdentityPart);
+
+            dto.OperationIdentity = FormatOperationIdentity(_operationIdentityPartStack.Reverse());
+
+            base.Visit_OperatorDto_Polymorphic(dto);
+
+            _operationIdentityPartStack.Pop();
+
+            return dto;
+        }
+
+        private static string FormatIdentityPart(IOperatorDto dto)
+        {
+            // TODO: Different types of operator might get additional key parts,
+            // like the number operator would require the actual number to be the key.
+
+            switch (dto.OperatorTypeEnum)
+            {
+                case OperatorTypeEnum.Number:
+                    // TODO: You need the specific OperatorDto type,
+                    // but should you not just use specialized visitation methods for that?
+                    var dto2 = (Number_OperatorDto)dto;
+                    return $"{dto.OperatorTypeEnum}{OPERATION_IDENTITY_KEY_PART_SEPARATOR}{dto2.Number}";
+
+                //case OperatorTypeEnum.DimensionToOutlets:
+
+
+                default:
+                    return $"{dto.OperatorTypeEnum}{dto.OperatorID}";
+            }
+        }
+
+        private string FormatOperationIdentity(IEnumerable<string> operationIdentityParts)
+        {
+            return string.Join(OPERATION_IDENTITY_PATH_SEPARATOR, operationIdentityParts);
+        }
+    }
+}
