2d9f5f4c944e8ea49ce379452a45b3f29bc53c2f
jjvanzon <janjoostvanzon@hotmail.com>
2018-02-03 19:43:28 +0100

    Synthesizer: MidiMapping: Include MidiMappings and MidiMappingElements in the eager-loading DocumentRepository.TryGetComplete.

--- Data/Synthesizer.NHibernate/Repositories/DocumentRepository.cs
+++ Data/Synthesizer.NHibernate/Repositories/DocumentRepository.cs
@@ -1,8 +1,9 @@
-﻿using JJ.Framework.Data;
-using JJ.Framework.Data.NHibernate;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using JJ.Data.Synthesizer.Entities;
+using JJ.Framework.Data;
+using JJ.Framework.Data.NHibernate;
+// ReSharper disable SuggestVarOrType_Elsewhere
 
 // ReSharper disable UnusedVariable
 
@@ -13,62 +14,64 @@ namespace JJ.Data.Synthesizer.NHibernate.Repositories
 		private new readonly NHibernateContext _context;
 
 		public DocumentRepository(IContext context)
-			: base(context)
-		{
-			_context = (NHibernateContext)context;
-		}
+			: base(context) => _context = (NHibernateContext)context;
 
 		public override IList<Document> GetAll() => _context.Session.QueryOver<Document>().List();
 
 		public override IList<Document> OrderByName()
 		{
 			return _context.Session.QueryOver<Document>()
-								   .OrderBy(x => x.Name).Asc
-								   .List();
+			               .OrderBy(x => x.Name)
+			               .Asc
+			               .List();
 		}
 
 		/// <summary> TODO: Eager load the enum-like types too. </summary>
 		public override Document TryGetComplete(int documentID)
 		{
 			Document document = null;
-				AudioFileOutput audioFileOutput = null;
-				Curve curve = null;
-				Sample sample = null;
-				Scale scale = null;
-				Patch patch = null;
-					Operator op = null;
-				DocumentReference higherDocumentReference = null;
-				DocumentReference lowerDocumentReference = null;
+			AudioFileOutput audioFileOutput = null;
+			Curve curve = null;
+			MidiMapping midiMapping = null;
+			Sample sample = null;
+			Scale scale = null;
+			Patch patch = null;
+			Operator op = null;
+			DocumentReference higherDocumentReference = null;
+			DocumentReference lowerDocumentReference = null;
 
-			var level_1_documentQuery = _context.Session.QueryOver(() => document)
-														.Where(x => x.ID == documentID)
-														.Future<Document>();
+			var level_1_DocumentQuery = _context.Session.QueryOver(() => document)
+			                                    .Where(x => x.ID == documentID)
+			                                    .Future<Document>();
 
-			var level_2_audioFileOutputsQuery = _context.Session.QueryOver(() => document)
-																.Left.JoinAlias(() => document.AudioFileOutputs, () => audioFileOutput)
-																.Where(() => document.ID == documentID)
-																.Future<Document>();
+			var level_2_AudioFileOutputsQuery = _context.Session.QueryOver(() => document)
+			                                            .Left.JoinAlias(() => document.AudioFileOutputs, () => audioFileOutput)
+			                                            .Where(() => document.ID == documentID)
+			                                            .Future<Document>();
 
-			var level_2_patchesQuery = _context.Session.QueryOver(() => document)
-													   .Left.JoinAlias(() => document.Patches, () => patch)
-													   .Where(() => document.ID == documentID)
-													   .Future<Document>();
+			var level_2_PatchesQuery = _context.Session.QueryOver(() => document)
+			                                   .Left.JoinAlias(() => document.Patches, () => patch)
+			                                   .Where(() => document.ID == documentID)
+			                                   .Future<Document>();
 
-			var level_2_lowerDocumentReferencesQuery = _context.Session.QueryOver(() => document)
-															   .Left.JoinAlias(() => document.LowerDocumentReferences, () => lowerDocumentReference)
-															   .Where(() => document.ID == documentID)
-															   .Future<Document>();
+			var level_2_LowerDocumentReferencesQuery = _context.Session.QueryOver(() => document)
+			                                                   .Left.JoinAlias(() => document.LowerDocumentReferences, () => lowerDocumentReference)
+			                                                   .Where(() => document.ID == documentID)
+			                                                   .Future<Document>();
 
-			var level_2_higherDocumentReferencesQuery = _context.Session.QueryOver(() => document)
-																.Left.JoinAlias(() => document.HigherDocumentReferences, () => higherDocumentReference)
-																.Where(() => document.ID == documentID)
-																.Future<Document>();
+			var level_2_HigherDocumentReferencesQuery = _context.Session.QueryOver(() => document)
+			                                                    .Left.JoinAlias(
+				                                                    () => document.HigherDocumentReferences,
+				                                                    () => higherDocumentReference)
+			                                                    .Where(() => document.ID == documentID)
+			                                                    .Future<Document>();
 
-			var level_3_higherDocuments = _context.Session.QueryOver(() => higherDocumentReference)
-												  .JoinAlias(() => higherDocumentReference.LowerDocument, () => document)
-												  .Where(() => document.ID == documentID)
-												  .Fetch(x => x.HigherDocument).Eager
-												  .Future<DocumentReference>();
+			var level_3_HigherDocuments = _context.Session.QueryOver(() => higherDocumentReference)
+			                                      .JoinAlias(() => higherDocumentReference.LowerDocument, () => document)
+			                                      .Where(() => document.ID == documentID)
+			                                      .Fetch(x => x.HigherDocument)
+			                                      .Eager
+			                                      .Future<DocumentReference>();
 
 			// This partial query would cause duplicate patches to be loaded.
 			//var level_4_higherDocumentPatchesQuery = _context.Session.QueryOver(() => higherDocument)
@@ -78,57 +81,63 @@ namespace JJ.Data.Synthesizer.NHibernate.Repositories
 			//												 .Fetch(x => x.Patches).Eager
 			//												 .Future<Document>();
 
-			var level_2_scalesQuery = _context.Session.QueryOver(() => document)
-													  .Left.JoinAlias(() => document.Scales, () => scale)
-													  .Where(() => document.ID == documentID)
-													  .Future<Document>();
+			var level_2_ScalesQuery = _context.Session.QueryOver(() => document)
+			                                  .Left.JoinAlias(() => document.Scales, () => scale)
+			                                  .Where(() => document.ID == documentID)
+			                                  .Future<Document>();
 
-			var level_3_tonesQuery = _context.Session.QueryOver(() => scale)
-													 .Fetch(x => x.Tones).Eager
-													 .Where(x => x.Document.ID == documentID)
-													 .Future<Scale>();
+			var level_3_TonesQuery = _context.Session.QueryOver(() => scale)
+			                                 .Fetch(x => x.Tones)
+			                                 .Eager
+			                                 .Where(x => x.Document.ID == documentID)
+			                                 .Future<Scale>();
 
-			var level_3_operatorsQuery = _context.Session.QueryOver(() => patch)
-														 .JoinAlias(() => patch.Document, () => document)
-														 .Where(() => document.ID == documentID)
-														 .Fetch(x => x.Operators).Eager
-														 .Future<Patch>();
+			var level_3_OperatorsQuery = _context.Session.QueryOver(() => patch)
+			                                     .JoinAlias(() => patch.Document, () => document)
+			                                     .Where(() => document.ID == documentID)
+			                                     .Fetch(x => x.Operators)
+			                                     .Eager
+			                                     .Future<Patch>();
 
-			var level_4_inletsQuery = _context.Session.QueryOver(() => op)
-													  .JoinAlias(() => op.Patch, () => patch)
-													  .JoinAlias(() => patch.Document, () => document)
-													  .Where(() => document.ID == documentID)
-													  .Fetch(x => x.Inlets).Eager
-													  .Future<Operator>();
+			var level_4_InletsQuery = _context.Session.QueryOver(() => op)
+			                                  .JoinAlias(() => op.Patch, () => patch)
+			                                  .JoinAlias(() => patch.Document, () => document)
+			                                  .Where(() => document.ID == documentID)
+			                                  .Fetch(x => x.Inlets)
+			                                  .Eager
+			                                  .Future<Operator>();
 
-			var level_4_outletsQuery = _context.Session.QueryOver(() => op)
-													   .JoinAlias(() => op.Patch, () => patch)
-													   .JoinAlias(() => patch.Document, () => document)
-													   .Where(() => document.ID == documentID)
-													   .Fetch(x => x.Outlets).Eager
-													   .Future<Operator>();
+			var level_4_OutletsQuery = _context.Session.QueryOver(() => op)
+			                                   .JoinAlias(() => op.Patch, () => patch)
+			                                   .JoinAlias(() => patch.Document, () => document)
+			                                   .Where(() => document.ID == documentID)
+			                                   .Fetch(x => x.Outlets)
+			                                   .Eager
+			                                   .Future<Operator>();
 
-			var level_4_standardDimensionQuery = _context.Session.QueryOver(() => op)
-														 .JoinAlias(() => op.Patch, () => patch)
-														 .JoinAlias(() => patch.Document, () => document)
-														 .Where(() => document.ID == documentID)
-														 .Fetch(x => x.StandardDimension).Eager
-														 .Future<Operator>();
+			var level_4_StandardDimensionQuery = _context.Session.QueryOver(() => op)
+			                                             .JoinAlias(() => op.Patch, () => patch)
+			                                             .JoinAlias(() => patch.Document, () => document)
+			                                             .Where(() => document.ID == documentID)
+			                                             .Fetch(x => x.StandardDimension)
+			                                             .Eager
+			                                             .Future<Operator>();
 
-			var level_4_underlyingPatchQuery = _context.Session.QueryOver(() => op)
-													   .JoinAlias(() => op.Patch, () => patch)
-													   .JoinAlias(() => patch.Document, () => document)
-													   .Where(() => document.ID == documentID)
-													   .Fetch(x => x.UnderlyingPatch).Eager
-													   .Future<Operator>();
+			var level_4_UnderlyingPatchQuery = _context.Session.QueryOver(() => op)
+			                                           .JoinAlias(() => op.Patch, () => patch)
+			                                           .JoinAlias(() => patch.Document, () => document)
+			                                           .Where(() => document.ID == documentID)
+			                                           .Fetch(x => x.UnderlyingPatch)
+			                                           .Eager
+			                                           .Future<Operator>();
 
-			var level_4_curveQuery = _context.Session.QueryOver(() => op)
-											 .JoinAlias(() => op.Patch, () => patch)
-											 .JoinAlias(() => patch.Document, () => document)
-											 .Where(() => document.ID == documentID)
-											 .Fetch(x => x.Curve)
-											 .Eager
-											 .Future<Operator>();
+			var level_4_CurveQuery = _context.Session.QueryOver(() => op)
+			                                 .JoinAlias(() => op.Patch, () => patch)
+			                                 .JoinAlias(() => patch.Document, () => document)
+			                                 .Where(() => document.ID == documentID)
+			                                 .Fetch(x => x.Curve)
+			                                 .Eager
+			                                 .Future<Operator>();
 
 			// Too bad: There is no curve.Operator to link upward to.
 			//var level_5_nodesQuery = _context.Session.QueryOver(() => curve)
@@ -139,24 +148,36 @@ namespace JJ.Data.Synthesizer.NHibernate.Repositories
 			//								 .Fetch(x => x.Nodes).Eager
 			//								 .Future<Curve>();
 
-			var level_4_sampleQuery = _context.Session.QueryOver(() => op)
-											 .JoinAlias(() => op.Patch, () => patch)
-											 .JoinAlias(() => patch.Document, () => document)
-											 .Where(() => document.ID == documentID)
-											 .Fetch(x => x.Sample)
-											 .Eager
-											 .Future<Operator>();
+			var level_4_SampleQuery = _context.Session.QueryOver(() => op)
+			                                  .JoinAlias(() => op.Patch, () => patch)
+			                                  .JoinAlias(() => patch.Document, () => document)
+			                                  .Where(() => document.ID == documentID)
+			                                  .Fetch(x => x.Sample)
+			                                  .Eager
+			                                  .Future<Operator>();
 
-			Document outputDocument = level_1_documentQuery.FirstOrDefault();
+			var level_2_MidiMappingsQuery = _context.Session.QueryOver(() => document)
+			                                        .Left.JoinAlias(() => document.MidiMappings, () => midiMapping)
+			                                        .Where(() => document.ID == documentID)
+			                                        .Future<Document>();
+
+			var level_3_MidiMappingElementsQuery = _context.Session.QueryOver(() => midiMapping)
+			                                               .JoinAlias(() => midiMapping.Document, () => document)
+			                                               .Where(() => document.ID == documentID)
+			                                               .Fetch(x => x.MidiMappingElements)
+			                                               .Eager
+			                                               .Future<MidiMapping>();
+
+			Document outputDocument = level_1_DocumentQuery.FirstOrDefault();
 			return outputDocument;
 		}
 
 		public override Document TryGetByName(string name)
 		{
 			Document entity = _context.Session
-									  .QueryOver<Document>()
-									  .Where(x => x.Name == name)
-									  .SingleOrDefault();
+			                          .QueryOver<Document>()
+			                          .Where(x => x.Name == name)
+			                          .SingleOrDefault();
 			return entity;
 		}
 	}
