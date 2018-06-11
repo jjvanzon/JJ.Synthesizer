dae896128a0fb339924e42a89900bf537668457d
jjvanzon <janjoostvanzon@hotmail.com>
2018-06-11 19:56:13 +0200

    Make most unit test pass again, including those that use EntityFramework.

--- Business/Synthesizer.Tests/PatchCalculator_Interpolate_Line_Tests.cs
+++ Business/Synthesizer.Tests/PatchCalculator_Interpolate_Line_Tests.cs
@@ -13,114 +13,114 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JJ.Business.Synthesizer.Tests
 {
-	[TestClass]
-	public class PatchCalculator_Interpolate_Line_Tests
-	{
-		[TestMethod]
-		public void Test_PatchCalculator_Interpolate_Line_Forward_StartPositionNegative()
-		{
-			using (IContext context = PersistenceHelper.CreateContext())
-			{
-				// Arrange
-				RepositoryWrapper repositories = PersistenceHelper.CreateRepositories(context);
-				PatchFacade patchFacade = new PatchFacade(repositories);
-				Patch patch = patchFacade.CreatePatch();
-				OperatorFactory o = new OperatorFactory(patch, repositories);
+    [TestClass]
+    public class PatchCalculator_Interpolate_Line_Tests
+    {
+        [TestMethod]
+        public void Test_PatchCalculator_Interpolate_Line_Forward_StartPositionNegative()
+        {
+            using (IContext context = PersistenceHelper.CreateContext())
+            {
+                // Arrange
+                RepositoryWrapper repositories = PersistenceHelper.CreateRepositories(context);
+                PatchFacade patchFacade = new PatchFacade(repositories);
+                Patch patch = patchFacade.CreatePatch();
+                OperatorFactory o = new OperatorFactory(patch, repositories);
 
-				const DimensionEnum dimensionEnum = DimensionEnum.Number;
+                const DimensionEnum dimensionEnum = DimensionEnum.Number;
 
-				var curve = o.Curve(
-					dimensionEnum,
-					"",
-					(-1.5, 1, NodeTypeEnum.Block),
-					(-0.5, -1, NodeTypeEnum.Block),
-					(0.5, 2, NodeTypeEnum.Block),
-					(1.5, -2, NodeTypeEnum.Block));
+                var curve = o.Curve(
+                    dimensionEnum,
+                    "",
+                    (-1.5, 1, NodeTypeEnum.Block),
+                    (-0.5, -1, NodeTypeEnum.Block),
+                    (0.5, 2, NodeTypeEnum.Block),
+                    (1.5, -2, NodeTypeEnum.Block));
 
-				var interpolate = o.Interpolate(curve, o.Number(1), InterpolationTypeEnum.Line, dimensionEnum);
+                var interpolate = o.Interpolate(curve, o.Number(1), InterpolationTypeEnum.Line, dimensionEnum);
 
-				var buffer = new float[1];
-				IPatchCalculator calculator = patchFacade.CreateCalculator(interpolate, 44800, channelCount: 1, channelIndex: 0, new CalculatorCache());
+                var buffer = new float[1];
+                IPatchCalculator calculator = patchFacade.CreateCalculator(interpolate, 44800, 1, 0, new CalculatorCache());
 
-				// Execute
-				var values = new float[9];
-				double x = -2;
-				calculator.Reset(x);
+                // Execute
+                var values = new float[9];
+                double x = -2;
+                calculator.Reset(x);
 
-				for (int i = 0; i < values.Length; i++)
-				{
-					Array.Clear(buffer, 0, buffer.Length);
-					calculator.SetValue(dimensionEnum, x);
-					calculator.Calculate(buffer, buffer.Length, x);
-					values[i] = buffer[0];
-					x += 0.5;
-				}
+                for (var i = 0; i < values.Length; i++)
+                {
+                    Array.Clear(buffer, 0, buffer.Length);
+                    calculator.SetValue(dimensionEnum, x);
+                    calculator.Calculate(buffer, buffer.Length, x);
+                    values[i] = buffer[0];
+                    x += 0.5;
+                }
 
-				// Assert
-				AssertHelper.AreEqual(1, () => values[0]);
-				AssertHelper.AreEqual(1, () => values[1]);
-				AssertHelper.AreEqual(0, () => values[2]);
-				AssertHelper.AreEqual(-1, () => values[3]);
-				AssertHelper.AreEqual(0.5, () => values[4]);
-				AssertHelper.AreEqual(2, () => values[5]);
-				AssertHelper.AreEqual(0, () => values[6]);
-				AssertHelper.AreEqual(-2, () => values[7]);
-				AssertHelper.AreEqual(-2, () => values[8]);
-			}
-		}
+                // Assert
+                AssertHelper.AreEqual(1, () => values[0]);
+                AssertHelper.AreEqual(1, () => values[1]);
+                AssertHelper.AreEqual(0, () => values[2]);
+                AssertHelper.AreEqual(-1, () => values[3]);
+                AssertHelper.AreEqual(0.5, () => values[4]);
+                AssertHelper.AreEqual(2, () => values[5]);
+                AssertHelper.AreEqual(0, () => values[6]);
+                AssertHelper.AreEqual(-2, () => values[7]);
+                AssertHelper.AreEqual(-2, () => values[8]);
+            }
+        }
 
-		[TestMethod]
-		public void Test_PatchCalculator_Interpolate_Line_Backward_StartPositionNegative()
-		{
-			using (IContext context = PersistenceHelper.CreateContext())
-			{
-				// Arrange
-				RepositoryWrapper repositories = PersistenceHelper.CreateRepositories(context);
-				PatchFacade patchFacade = new PatchFacade(repositories);
-				Patch patch = patchFacade.CreatePatch();
-				OperatorFactory o = new OperatorFactory(patch, repositories);
+        [TestMethod]
+        public void Test_PatchCalculator_Interpolate_Line_Backward_StartPositionNegative()
+        {
+            using (IContext context = PersistenceHelper.CreateContext())
+            {
+                // Arrange
+                RepositoryWrapper repositories = PersistenceHelper.CreateRepositories(context);
+                PatchFacade patchFacade = new PatchFacade(repositories);
+                Patch patch = patchFacade.CreatePatch();
+                OperatorFactory o = new OperatorFactory(patch, repositories);
 
-				const DimensionEnum dimensionEnum = DimensionEnum.Number;
+                const DimensionEnum dimensionEnum = DimensionEnum.Number;
 
-				var curve = o.Curve(
-					dimensionEnum,
-					"",
-					(-1.5, 1, NodeTypeEnum.Block),
-					(-0.5, -1, NodeTypeEnum.Block),
-					(0.5, 2, NodeTypeEnum.Block),
-					(1.5, -2, NodeTypeEnum.Block));
+                var curve = o.Curve(
+                    dimensionEnum,
+                    "",
+                    (-1.5, 1, NodeTypeEnum.Block),
+                    (-0.5, -1, NodeTypeEnum.Block),
+                    (0.5, 2, NodeTypeEnum.Block),
+                    (1.5, -2, NodeTypeEnum.Block));
 
-				var interpolate = o.Interpolate(curve, o.Number(1), InterpolationTypeEnum.Line, dimensionEnum);
+                var interpolate = o.Interpolate(curve, o.Number(1), InterpolationTypeEnum.Line, dimensionEnum);
 
-				var buffer = new float[1];
+                var buffer = new float[1];
 
-				// Execute
-				IPatchCalculator calculator = patchFacade.CreateCalculator(interpolate, 44800, channelCount: 1, channelIndex: 0, new CalculatorCache());
+                // Execute
+                IPatchCalculator calculator = patchFacade.CreateCalculator(interpolate, 44800, 1, 0, new CalculatorCache());
 
-				var values = new float[9];
-				double x = 3;
-				calculator.Reset(x);
+                var values = new float[9];
+                double x = 3;
+                calculator.Reset(x);
 
-				for (int i = 0; i < values.Length; i++)
-				{
-					Array.Clear(buffer, 0, buffer.Length);
-					calculator.SetValue(dimensionEnum, x);
-					calculator.Calculate(buffer, buffer.Length, x);
-					values[i] = buffer[0];
-					x -= 0.5;
-				}
+                for (var i = 0; i < values.Length; i++)
+                {
+                    Array.Clear(buffer, 0, buffer.Length);
+                    calculator.SetValue(dimensionEnum, x);
+                    calculator.Calculate(buffer, buffer.Length, x);
+                    values[i] = buffer[0];
+                    x -= 0.5;
+                }
 
-				// Assert
-				AssertHelper.AreEqual(-2, () => values[0]);
-				AssertHelper.AreEqual(-2, () => values[1]);
-				AssertHelper.AreEqual(0, () => values[2]);
-				AssertHelper.AreEqual(2, () => values[3]);
-				AssertHelper.AreEqual(0.5, () => values[4]);
-				AssertHelper.AreEqual(-1, () => values[5]);
-				AssertHelper.AreEqual(0, () => values[6]);
-				AssertHelper.AreEqual(1, () => values[7]);
-				AssertHelper.AreEqual(1, () => values[8]);
-			}
-		}
-	}
+                // Assert
+                AssertHelper.AreEqual(-2, () => values[0]);
+                AssertHelper.AreEqual(-2, () => values[1]);
+                AssertHelper.AreEqual(0, () => values[2]);
+                AssertHelper.AreEqual(2, () => values[3]);
+                AssertHelper.AreEqual(0.5, () => values[4]);
+                AssertHelper.AreEqual(-1, () => values[5]);
+                AssertHelper.AreEqual(0, () => values[6]);
+                AssertHelper.AreEqual(1, () => values[7]);
+                AssertHelper.AreEqual(1, () => values[8]);
+            }
+        }
+    }
 }
