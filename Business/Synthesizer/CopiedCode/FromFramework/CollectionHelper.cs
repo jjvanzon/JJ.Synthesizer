eff4f625319d77c3ac6248e6c41b4a38217984ea
jjvanzon <janjoostvanzon@hotmail.com>
2018-08-04 16:43:56 +0200

    Synthesizer: Copy code from Framework not manually, but under build event.

--- Business/Synthesizer/CopiedCode/FromFramework/CollectionHelper.cs
+++ Business/Synthesizer/CopiedCode/FromFramework/CollectionHelper.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
 
 namespace JJ.Framework.Collections
 {
-    [PublicAPI]
     public static class CollectionHelper
     {
         /// <summary>
