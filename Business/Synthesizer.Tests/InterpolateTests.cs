ac11957bb638db60d112dc92e09f65fcdd9ed542
jjvanzon <janjoostvanzon@hotmail.com>
2018-08-15 23:48:39 +0200

    Synthesizer.Tests: * Make Test_Interpolate_Stripe_LookAhead_DimensionNotTime_Forward_StartPosition0, even though it does not work yet. * Make base test code assert inconclusive if no database connection.

--- Business/Synthesizer.Tests/InterpolateTests.cs
+++ Business/Synthesizer.Tests/InterpolateTests.cs
@@ -8,7 +8,9 @@ using JJ.Business.Synthesizer.Helpers;
 using JJ.Business.Synthesizer.Tests.Helpers;
 using JJ.Data.Synthesizer.Entities;
 using JJ.Framework.Data;
+using JJ.Framework.Testing.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 // ReSharper disable UnusedVariable
 // ReSharper disable AccessToModifiedClosure
 // ReSharper disable SuggestVarOrType_SimpleTypes
@@ -21,8 +23,42 @@ namespace JJ.Business.Synthesizer.Tests
         //Assert.Inconclusive("The assertions are known to not be correct yet.");
 
         [TestMethod]
-        public void Test_PatchCalculator_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPosition0()
+        public void Test_Interpolate_Stripe_LookAhead_DimensionNotTime_Forward_StartPosition0()
             => Test_PatchCalculator_Interpolate_Base(
+                InterpolationTypeEnum.Stripe,
+                FollowingModeEnum.LookAhead,
+                DimensionEnum.Number,
+                new List<(double x, double y)>
+                {
+                    (0, 1),
+                    (1, -1),
+                    (2, 2),
+                    (3, -2)
+                },
+                new List<(double x, double y)>
+                {
+                    (0.00, 1.0),
+                    (0.33, 1.0),
+
+                    (0.67, -1.0),
+                    (1.00, -1.0),
+                    (1.33, -1.0),
+
+                    (1.67, 2.0),
+                    (2.00, 2.0),
+                    (2.33, 2.0),
+
+                    (2.67, -2.0),
+                    (3.00, -2.0),
+                    (3.33, -2.0),
+
+                    (3.67, -2.0),
+                    (4.00, -2.0)
+                });
+
+        [TestMethod]
+        public void Test_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPosition0()
+            => Test_Interpolate_Base(
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
                 DimensionEnum.Time,
@@ -47,8 +83,8 @@ namespace JJ.Business.Synthesizer.Tests
                 });
 
         [TestMethod]
-        public void Test_PatchCalculator_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPositionNegative()
-            => Test_PatchCalculator_Interpolate_Base(
+        public void Test_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPositionNegative()
+            => Test_Interpolate_Base(
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
                 DimensionEnum.Time,
@@ -73,8 +109,8 @@ namespace JJ.Business.Synthesizer.Tests
                 });
 
         [TestMethod]
-        public void Test_PatchCalculator_Interpolate_Line_LagBehind_DimensionNotTime_Backward_StartPositionPositive()
-            => Test_PatchCalculator_Interpolate_Base(
+        public void Test_Interpolate_Line_LagBehind_DimensionNotTime_Backward_StartPositionPositive()
+            => Test_Interpolate_Base(
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
                 DimensionEnum.Number,
@@ -98,65 +134,70 @@ namespace JJ.Business.Synthesizer.Tests
                     (1.0, 1.0)
                 });
 
-        private void Test_PatchCalculator_Interpolate_Base(
+        private void Test_Interpolate_Base(
             InterpolationTypeEnum interpolationTypeEnum,
             FollowingModeEnum followingModeEnum,
             DimensionEnum dimensionEnum,
             IList<(double x, double y)> inputPoints,
             IList<(double x, double y)> expectedOutputPoints)
-        {
-            using (IContext context = PersistenceHelper.CreateContext())
-            {
-                // Arrange
-                RepositoryWrapper repositories = PersistenceHelper.CreateRepositories(context);
-                PatchFacade patchFacade = new PatchFacade(repositories);
-                Patch patch = patchFacade.CreatePatch();
-                OperatorFactory o = new OperatorFactory(patch, repositories);
-
-                var curve = o.Curve(
-                    dimensionEnum,
-                    "",
-                    inputPoints.Select(p => (p.x, p.y, InterpolationTypeEnum.Stripe)).ToArray());
-
-                var interpolate = o.Interpolate(
-                    curve,
-                    o.Number(1),
-                    interpolationTypeEnum,
-                    dimensionEnum,
-                    "",
-                    followingModeEnum);
-
-                var buffer = new float[1];
-                IPatchCalculator calculator = patchFacade.CreateCalculator(interpolate, 2, 1, 0, new CalculatorCache());
-
-                // Execute
-                var actualYs = new double[expectedOutputPoints.Count];
-                double firstX = expectedOutputPoints.First().x;
-                calculator.Reset(firstX);
-
-                for (var i = 0; i < expectedOutputPoints.Count; i++)
+            => AssertInconclusiveHelper.WithConnectionInconclusiveAssertion(
+                () =>
                 {
-                    (double expectedX, double expectedY) = expectedOutputPoints[i];
-
-                    Array.Clear(buffer, 0, buffer.Length);
-                    calculator.SetValue(dimensionEnum, expectedX);
-                    calculator.Calculate(buffer, buffer.Length, expectedX);
-                    double actualY = buffer[0];
-                    actualYs[i] = actualY;
-                }
-
-                // Assert
-                for (var i = 0; i < expectedOutputPoints.Count; i++)
-                {
-                    (double expectedX, double expectedY) = expectedOutputPoints[i];
-                    double actualY = actualYs[0];
-
-                    if (Math.Abs(expectedY - actualY) > 0.00000001)
+                    using (IContext context = PersistenceHelper.CreateContext())
                     {
-                        Assert.Fail($"Point [{i}] is expected to be ({expectedX}, {expectedY}), but it is ({expectedX}, {actualY}) instead.");
+                        // Arrange
+                        RepositoryWrapper repositories = PersistenceHelper.CreateRepositories(context);
+                        PatchFacade patchFacade = new PatchFacade(repositories);
+                        Patch patch = patchFacade.CreatePatch();
+                        OperatorFactory o = new OperatorFactory(patch, repositories);
+
+                        var curve = o.Curve(
+                            dimensionEnum,
+                            "",
+                            inputPoints.Select(p => (p.x, p.y, InterpolationTypeEnum.Line)).ToArray());
+
+                        var interpolate = o.Interpolate(
+                            curve,
+                            o.Number(1),
+                            interpolationTypeEnum,
+                            dimensionEnum,
+                            "",
+                            followingModeEnum);
+
+                        var buffer = new float[1];
+                        IPatchCalculator calculator = patchFacade.CreateCalculator(interpolate, 2, 1, 0, new CalculatorCache());
+
+                        // Execute
+                        var actualYs = new double[expectedOutputPoints.Count];
+                        double firstX = expectedOutputPoints.First().x;
+                        calculator.Reset(firstX);
+
+                        for (var i = 0; i < expectedOutputPoints.Count; i++)
+                        {
+                            (double expectedX, double expectedY) = expectedOutputPoints[i];
+
+                            Array.Clear(buffer, 0, buffer.Length);
+                            calculator.SetValue(dimensionEnum, expectedX);
+                            calculator.Calculate(buffer, buffer.Length, expectedX);
+                            double actualY = buffer[0];
+                            actualYs[i] = actualY;
+                        }
+
+                        // Assert
+                        for (var i = 0; i < expectedOutputPoints.Count; i++)
+                        {
+                            (double expectedX, double expectedY) = expectedOutputPoints[i];
+                            double actualY = actualYs[0];
+
+                            if (Math.Abs(expectedY - actualY) > 0.00000001)
+                            {
+                                string message =
+                                    $"Point [{i}] is expected to be ({expectedX}, {expectedY}), but it is ({expectedX}, {actualY}) instead.";
+
+                                Assert.Fail(message);
+                            }
+                        }
                     }
-                }
-            }
-        }
+                });
     }
 }
