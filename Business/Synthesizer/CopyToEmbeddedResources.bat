b530b2abacebd25cf255b00a431b46edce44577c
jjvanzon <janjoostvanzon@hotmail.com>
2018-09-03 15:49:32 +0200

    Synthesizer: Include a copy of SineCalculator as an embedded resource instead of a file to statically compile SineCalculator at run-time compilation.

--- Business/Synthesizer/CopyToEmbeddedResources.bat
+++ Business/Synthesizer/CopyToEmbeddedResources.bat
@@ -1,13 +1,12 @@
 prompt @
 
-echo EXECUTING CopyFrameworkCode.bat
+echo EXECUTING CopyToEmbeddedResources.bat
 
 cd /D "%~dp0"
 
 rem /R = Overwrites read-only files.
 rem /K = Copies attributes. Normal Xcopy will reset read-only attributes.
 rem /Y = Suppresses prompting to confirm you want to overwrite an existing destination file.
-xcopy "..\..\Framework\Mathematics\*.cs" "CopiedCode\FromFramework\" /R /K /Y
-xcopy "..\..\Framework\Collections\CollectionHelper.cs" "CopiedCode\FromFramework\" /R /K /Y
+xcopy "Calculation\SineCalculator.cs" "Calculation\SineCalculator_EmbeddedResource.cs" /R /K /Y
 
 pause
