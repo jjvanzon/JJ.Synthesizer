f69354d9f1d9f1590179563396c43ecb7449fbc9
jjvanzon <janjoostvanzon@hotmail.com>
2018-09-07 01:22:04 +0200

    Synthesizer_RootsAndLogsSingleInput_Tests: Normalize code.

--- Business/Synthesizer.Tests/Synthesizer_RootsAndLogsSingleInput_Tests.cs
+++ Business/Synthesizer.Tests/Synthesizer_RootsAndLogsSingleInput_Tests.cs
@@ -12,52 +12,47 @@ namespace JJ.Business.Synthesizer.Tests
     {
         private static readonly double[] _values = MathHelper.SpreadDoubles(1, 15, 29);
 
-        [TestMethod]
-        public void Test_Synthesizer_SquareRoot_WithRoslyn()
-            => TestExecutor.TestMultipleValues(
-                x => x.New(nameof(SystemPatchNames.SquareRoot), x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
-                Math.Sqrt,
-                _values,
-                CalculationMethodEnum.Roslyn);
+        // SquareRoot
 
         [TestMethod]
-        public void Test_Synthesizer_SquareRoot_WithCalculatorClasses()
-            => TestExecutor.TestMultipleValues(
-                x => x.New(nameof(SystemPatchNames.SquareRoot), x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
-                Math.Sqrt,
-                _values,
-                CalculationMethodEnum.CalculatorClasses);
+        public void Test_Synthesizer_SquareRoot_WithRoslyn() => Test_Synthesizer_SquareRoot(CalculationMethodEnum.Roslyn);
 
         [TestMethod]
-        public void Test_Synthesizer_CubeRoot_WithRoslyn()
-            => TestExecutor.TestMultipleValues(
-                x => x.New(nameof(SystemPatchNames.CubeRoot), x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
-                x => Math.Pow(x, 1.0 / 3.0),
-                _values,
-                CalculationMethodEnum.Roslyn);
+        public void Test_Synthesizer_SquareRoot_WithCalculatorClasses() 
+            => Test_Synthesizer_SquareRoot(CalculationMethodEnum.CalculatorClasses);
+
+        private void Test_Synthesizer_SquareRoot(CalculationMethodEnum calculationMethodEnum)
+            => ExecuteTest(nameof(SystemPatchNames.SquareRoot), Math.Sqrt, calculationMethodEnum);
+
+        // CubeRoot
 
         [TestMethod]
-        public void Test_Synthesizer_CubeRoot_WithCalculatorClasses()
-            => TestExecutor.TestMultipleValues(
-                x => x.New(nameof(SystemPatchNames.CubeRoot), x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
-                x => Math.Pow(x, 1.0 / 3.0),
-                _values,
-                CalculationMethodEnum.CalculatorClasses);
+        public void Test_Synthesizer_CubeRoot_WithRoslyn() => Test_Synthesizer_CubeRoot(CalculationMethodEnum.Roslyn);
 
         [TestMethod]
-        public void Test_Synthesizer_Ln_WithRoslyn()
-            => TestExecutor.TestMultipleValues(
-                x => x.New(nameof(SystemPatchNames.Ln), x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
-                Math.Log,
-                _values,
-                CalculationMethodEnum.Roslyn);
+        public void Test_Synthesizer_CubeRoot_WithCalculatorClasses() => Test_Synthesizer_CubeRoot(CalculationMethodEnum.CalculatorClasses);
+
+        private void Test_Synthesizer_CubeRoot(CalculationMethodEnum calculationMethodEnum)
+            => ExecuteTest(nameof(SystemPatchNames.CubeRoot), x => Math.Pow(x, 1.0 / 3.0), calculationMethodEnum);
+
+        // Ln
 
         [TestMethod]
-        public void Test_Synthesizer_Ln_WithCalculatorClasses()
+        public void Test_Synthesizer_Ln_WithRoslyn() => Test_Synthesizer_Ln(CalculationMethodEnum.Roslyn);
+
+        [TestMethod]
+        public void Test_Synthesizer_Ln_WithCalculatorClasses() => Test_Synthesizer_Ln(CalculationMethodEnum.CalculatorClasses);
+
+        private void Test_Synthesizer_Ln(CalculationMethodEnum calculationMethodEnum)
+            => ExecuteTest(nameof(SystemPatchNames.Ln), Math.Log, calculationMethodEnum);
+
+        // Generalized Method
+
+        private void ExecuteTest(string systemPatchName, Func<double, double> func, CalculationMethodEnum calculationMethodEnum)
             => TestExecutor.TestMultipleValues(
-                x => x.New(nameof(SystemPatchNames.Ln), x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
-                Math.Log,
+                x => x.New(systemPatchName, x.PatchInlet(TestExecutor.DEFAULT_DIMENSION_ENUM)),
+                func,
                 _values,
-                CalculationMethodEnum.CalculatorClasses);
+                calculationMethodEnum);
     }
 }
