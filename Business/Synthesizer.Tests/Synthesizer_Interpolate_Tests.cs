4a4d752c853365ba569865035ff086de369cd1f4
jjvanzon <janjoostvanzon@hotmail.com>
2018-09-23 20:55:29 +0200

    Synthesizer_Interpolate_Tests: * Add LagBehind Stripe Interpolation Test on top of the LookAhead test variation. * Remove distinction between Dimension Time and non-Time. * Make dx constant for Stripe interpolation tests.

--- Business/Synthesizer.Tests/Synthesizer_Interpolate_Tests.cs
+++ Business/Synthesizer.Tests/Synthesizer_Interpolate_Tests.cs
@@ -17,60 +17,100 @@ namespace JJ.Business.Synthesizer.Tests
     public class Synthesizer_Interpolate_Tests
     {
         [TestMethod]
-        public void Test_Synthesizer_Interpolate_Stripe_LookAhead_DimensionNotTime_Forward_StartPosition0_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Stripe_LookAhead_DimensionNotTime_Forward_StartPosition0(
+        public void Test_Synthesizer_Interpolate_Stripe_LookAhead_Forward_StartPosition0_WithCalculatorClasses()
+            => Test_Synthesizer_Interpolate_Stripe_LookAhead_Forward_StartPosition0(
                 CalculationEngineEnum.CalculatorClasses);
 
-        private void Test_Synthesizer_Interpolate_Stripe_LookAhead_DimensionNotTime_Forward_StartPosition0(
+        private void Test_Synthesizer_Interpolate_Stripe_LookAhead_Forward_StartPosition0(
             CalculationEngineEnum calculationEngineEnum)
             => Test_Synthesizer_Interpolate_Base(
                 calculationEngineEnum,
                 InterpolationTypeEnum.Stripe,
                 FollowingModeEnum.LookAhead,
-                DimensionEnum.Number,
-                rate: 8.0 / MathHelper.TWO_PI,
+                rate: 4.0 / Math.PI,
                 new[]
                 {
-                    (Math.PI * 0.00, 0.0),
-                    (Math.PI * 0.01, 0.0),
-                    (Math.PI * 0.24, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.25, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.26, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.49, 1.0),
-                    (Math.PI * 0.50, 1.0),
-                    (Math.PI * 0.51, 1.0),
-                    (Math.PI * 0.74, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.75, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.76, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.99, 0.0),
-                    (Math.PI * 1.00, 0.0),
-                    (Math.PI * 1.01, 0.0),
-                    (Math.PI * 1.24, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.25, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.26, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.49, -1.0),
-                    (Math.PI * 1.50, -1.0),
-                    (Math.PI * 1.51, -1.0),
-                    (Math.PI * 1.74, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.75, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.76, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.99, 0.0),
-                    (Math.PI * 2.00, 0.0),
-                    (Math.PI * 2.01, 0.0)
+                    (Math.PI * 0.000000, 0.0),
+                    (Math.PI * 0.083333, 0.0),
+                    (Math.PI * 0.166667, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.250000, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.333333, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.416667, 1.0),
+                    (Math.PI * 0.500000, 1.0),
+                    (Math.PI * 0.583333, 1.0),
+                    (Math.PI * 0.666667, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.750000, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.833333, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.916667, 0.0),
+                    (Math.PI * 1.000000, 0.0),
+                    (Math.PI * 1.083333, 0.0),
+                    (Math.PI * 1.166667, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.250000, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.333333, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.416667, -1.0),
+                    (Math.PI * 1.500000, -1.0),
+                    (Math.PI * 1.583333, -1.0),
+                    (Math.PI * 1.666667, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.750000, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.833333, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.916667, 0.0),
+                    (Math.PI * 2.000000, 0.0),
+                    (Math.PI * 2.083333, 0.0)
                 });
 
         [TestMethod]
-        public void Test_Synthesizer_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPosition0_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPosition0(
+        public void Test_Synthesizer_Interpolate_Stripe_LagBehind_Forward_StartPosition0_WithCalculatorClasses()
+            => Test_Synthesizer_Interpolate_Stripe_LagBehind_Forward_StartPosition0(
                 CalculationEngineEnum.CalculatorClasses);
 
-        private void Test_Synthesizer_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPosition0(
+        private void Test_Synthesizer_Interpolate_Stripe_LagBehind_Forward_StartPosition0(
+            CalculationEngineEnum calculationEngineEnum)
+            => Test_Synthesizer_Interpolate_Base(
+                calculationEngineEnum,
+                InterpolationTypeEnum.Stripe,
+                FollowingModeEnum.LagBehind,
+                4.0 / Math.PI,
+                new[]
+                {
+                    (Math.PI * 0.00000000, 0.0),
+                    (Math.PI * 0.08333333, 0.0),
+                    (Math.PI * 0.16666667, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.25000000, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.33333333, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.41666667, 1.0),
+                    (Math.PI * 0.50000000, 1.0),
+                    (Math.PI * 0.58333333, 1.0),
+                    (Math.PI * 0.66666667, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.75000000, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.83333333, MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 0.91666667, 0.0),
+                    (Math.PI * 1.00000000, 0.0),
+                    (Math.PI * 1.08333333, 0.0),
+                    (Math.PI * 1.16666667, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.25000000, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.33333333, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.41666667, -1.0),
+                    (Math.PI * 1.50000000, -1.0),
+                    (Math.PI * 1.58333333, -1.0),
+                    (Math.PI * 1.66666667, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.75000000, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.83333333, -MathHelper.SQRT_2 / 2.0),
+                    (Math.PI * 1.91666667, 0.0),
+                    (Math.PI * 2.00000000, 0.0),
+                    (Math.PI * 2.08333333, 0.0)
+                });
+
+        [TestMethod]
+        public void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPosition0_WithCalculatorClasses()
+            => Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPosition0(
+                CalculationEngineEnum.CalculatorClasses);
+
+        private void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPosition0(
             CalculationEngineEnum calculationEngineEnum)
             => Test_Synthesizer_Interpolate_Base(
                 calculationEngineEnum,
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
-                DimensionEnum.Time,
                 rate: 8.0 / MathHelper.TWO_PI,
                 new[]
                 {
@@ -86,17 +126,16 @@ namespace JJ.Business.Synthesizer.Tests
                 });
 
         [TestMethod]
-        public void Test_Synthesizer_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPositionNegative_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPositionNegative(
+        public void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPositionNegative_WithCalculatorClasses()
+            => Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPositionNegative(
                 CalculationEngineEnum.CalculatorClasses);
 
-        private void Test_Synthesizer_Interpolate_Line_LagBehind_DimensionTime_Forward_StartPositionNegative(
+        private void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPositionNegative(
             CalculationEngineEnum calculationEngineEnum)
             => Test_Synthesizer_Interpolate_Base(
                 calculationEngineEnum,
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
-                DimensionEnum.Time,
                 rate: 12.0 / MathHelper.TWO_PI,
                 new[]
                 {
@@ -112,17 +151,16 @@ namespace JJ.Business.Synthesizer.Tests
                 });
 
         [TestMethod]
-        public void Test_Synthesizer_Interpolate_Line_LagBehind_DimensionNotTime_Backward_StartPositionPositive_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Line_LagBehind_DimensionNotTime_Backward_StartPositionPositive(
+        public void Test_Synthesizer_Interpolate_Line_LagBehind_Backward_StartPositionPositive_WithCalculatorClasses()
+            => Test_Synthesizer_Interpolate_Line_LagBehind_Backward_StartPositionPositive(
                 CalculationEngineEnum.CalculatorClasses);
 
-        private void Test_Synthesizer_Interpolate_Line_LagBehind_DimensionNotTime_Backward_StartPositionPositive(
+        private void Test_Synthesizer_Interpolate_Line_LagBehind_Backward_StartPositionPositive(
             CalculationEngineEnum calculationEngineEnum)
             => Test_Synthesizer_Interpolate_Base(
                 calculationEngineEnum,
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
-                DimensionEnum.Number,
                 rate: 12.0 / MathHelper.TWO_PI,
                 new[]
                 {
@@ -141,21 +179,20 @@ namespace JJ.Business.Synthesizer.Tests
             CalculationEngineEnum calculationEngineEnum,
             InterpolationTypeEnum interpolationTypeEnum,
             FollowingModeEnum followingModeEnum,
-            DimensionEnum dimensionEnum,
             double rate,
             IList<(double dimensionValue, double outputValue)> points)
         {
             // Transform this method's input into something the TestExecutor wants.
-            IList<DimensionEnum> inputDimensionEnums = new[] { dimensionEnum, DimensionEnum.Rate };
+            IList<DimensionEnum> inputDimensionEnums = new[] { TestConstants.DEFAULT_DIMENSION_ENUM, DimensionEnum.Rate };
             IList<(double, double)> inputTuples = points.Select(point => (point.dimensionValue, rate)).ToArray();
             IList<double> outputValues = points.Select(point => point.outputValue).ToArray();
 
             TestExecutor.ExecuteTest(
                 x => x.Interpolate(
-                    x.New(nameof(SystemPatchNames.Sin), x.PatchInlet(dimensionEnum)),
+                    x.New(nameof(SystemPatchNames.Sin), x.PatchInlet(TestConstants.DEFAULT_DIMENSION_ENUM)),
                     x.PatchInlet(DimensionEnum.Rate),
                     interpolationTypeEnum,
-                    dimensionEnum,
+                    TestConstants.DEFAULT_DIMENSION_ENUM,
                     "",
                     followingModeEnum),
                 inputDimensionEnums,
