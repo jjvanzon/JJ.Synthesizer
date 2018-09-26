523f11a7d937e2df03084f06dfb78af73d923214
jjvanzon <janjoostvanzon@hotmail.com>
2018-09-26 20:55:43 +0200

    Synthesizer_Interpolate_Line_Tests: Remove variations with different starting positions from the LagBehind tests.

--- Business/Synthesizer.Tests/Synthesizer_Interpolate_Line_Tests.cs
+++ Business/Synthesizer.Tests/Synthesizer_Interpolate_Line_Tests.cs
@@ -119,67 +119,16 @@ namespace JJ.Business.Synthesizer.Tests
         // LagBehind
 
         [TestMethod]
-        public void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPosition0_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPosition0(
-                CalculationEngineEnum.CalculatorClasses);
+        public void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_WithCalculatorClasses()
+            => Test_Synthesizer_Interpolate_Line_LagBehind_Forward(CalculationEngineEnum.CalculatorClasses);
 
-        private void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPosition0(
+        private void Test_Synthesizer_Interpolate_Line_LagBehind_Forward(
             CalculationEngineEnum calculationEngineEnum)
             => Test_Synthesizer_Interpolate_Base(
                 calculationEngineEnum,
                 InterpolationTypeEnum.Line,
                 FollowingModeEnum.LagBehind,
                 rate: 8.0 / MathHelper.TWO_PI,
-                new[]
-                {
-                    (Math.PI * 0.00, 0.0),
-                    (Math.PI * 0.25, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.50, 1.0),
-                    (Math.PI * 0.75, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.00, 0.0),
-                    (Math.PI * 1.25, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.50, -1.0),
-                    (Math.PI * 1.75, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 2.00, 0.0)
-                });
-
-        [TestMethod]
-        public void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPositionNegative_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPositionNegative(
-                CalculationEngineEnum.CalculatorClasses);
-
-        private void Test_Synthesizer_Interpolate_Line_LagBehind_Forward_StartPositionNegative(
-            CalculationEngineEnum calculationEngineEnum)
-            => Test_Synthesizer_Interpolate_Base(
-                calculationEngineEnum,
-                InterpolationTypeEnum.Line,
-                FollowingModeEnum.LagBehind,
-                rate: 12.0 / MathHelper.TWO_PI,
-                new[]
-                {
-                    (Math.PI * 0.00, 0.0),
-                    (Math.PI * 0.25, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 0.50, 1.0),
-                    (Math.PI * 0.75, MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.00, 0.0),
-                    (Math.PI * 1.25, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 1.50, -1.0),
-                    (Math.PI * 1.75, -MathHelper.SQRT_2 / 2.0),
-                    (Math.PI * 2.00, 0.0)
-                });
-
-        [TestMethod]
-        public void Test_Synthesizer_Interpolate_Line_LagBehind_Backward_StartPositionPositive_WithCalculatorClasses()
-            => Test_Synthesizer_Interpolate_Line_LagBehind_Backward_StartPositionPositive(
-                CalculationEngineEnum.CalculatorClasses);
-
-        private void Test_Synthesizer_Interpolate_Line_LagBehind_Backward_StartPositionPositive(
-            CalculationEngineEnum calculationEngineEnum)
-            => Test_Synthesizer_Interpolate_Base(
-                calculationEngineEnum,
-                InterpolationTypeEnum.Line,
-                FollowingModeEnum.LagBehind,
-                rate: 12.0 / MathHelper.TWO_PI,
                 new[]
                 {
                     (Math.PI * 0.00, 0.0),
