406c6d93ed1b253db7a9438dd414078b7d790831
jjvanzon <janjoostvanzon@hotmail.com>
2018-10-27 13:03:31 +0200

    JJ.Framework.Collections: Make BinarySearchInexact extension methods that delegate to the already existing CollectionHelper.BinarySearchInexact. Document BinarySearchInexact in the AssemblyDescription, the README.MD. Started with unit test, but oucommented them until finished. Slightly changed the docs of JJ.Framework.Exceptions and JJ.Framework.Mathematics and published that to NuGet.org.

--- Business/Synthesizer/CopiedCode/FromFramework/CollectionHelper.cs
+++ Business/Synthesizer/CopiedCode/FromFramework/CollectionHelper.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace JJ.Framework.Collections
 {
+    // Do not use JetBrains.Annotations.
+    // There is an app that statically includes and compiles this code
+    // at run-time, and JetBrains.Annotations is not included there.
+
     public static class CollectionHelper
     {
-        /// <summary>
-        /// This BinarySearch has not been thoroughly (unit) tested.
-        /// It has been used in the development of audio processing,
-        /// during which it was debugged, and produced expected audio output.
-        /// The biggest concern is what happens with integer round-off for certain edge cases.
-        /// </summary>
+        /// <param name="sortedArray"> Not checked for null, for performance. </param>
+        public static (double valueBefore, double valueAfter) BinarySearchInexact(double[] sortedArray, double input)
+        {
+            BinarySearchInexact(sortedArray, input, out double valueBefore, out double valueAfter);
+            return (valueBefore, valueAfter);
+        }
+
         /// <param name="sortedArray"> Not checked for null, for performance. </param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void BinarySearchInexact(
@@ -31,10 +37,23 @@ namespace JJ.Framework.Collections
         /// <summary>
         /// Overload with more values you supply yourself: halfLength, min and max,
         /// that you could cache yourself for performance.
-        /// This BinarySearch has not been thoroughly (unit) tested.
-        /// It has been used in the development of audio processing,
-        /// during which it was debugged, and produced expected audio output.
-        /// The biggest concern is what happens with integer round-off for certain edge cases.
+        /// </summary>
+        /// <param name="sortedArray"> Not checked for null, for performance. </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (double valueBefore, double valueAfter) BinarySearchInexact(
+            double[] sortedArray,
+            int halfCount,
+            double min,
+            double max,
+            double input)
+        {
+            BinarySearchInexact(sortedArray, halfCount, min, max, input, out double valueBefore, out double valueAfter);
+            return (valueBefore, valueAfter);
+        }
+
+        /// <summary>
+        /// Overload with more values you supply yourself: halfLength, min and max,
+        /// that you could cache yourself for performance.
         /// </summary>
         /// <param name="sortedArray"> Not checked for null, for performance. </param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
