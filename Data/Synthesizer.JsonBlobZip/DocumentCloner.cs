e5f1121f2ef9d1ac81118f3898ba05cdebc332eb
jjvanzon <janjoostvanzon@hotmail.com>
2018-12-05 21:40:24 +0100

    Add project JJ.Data.Synthesizer.JsonBlobZip.

---
+++ Data/Synthesizer.JsonBlobZip/DocumentCloner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using JJ.Data.Synthesizer.Entities;
+
+namespace JJ.Data.Synthesizer.JsonBlobZip
+{
+    // TODO: Possibility for dependency injection instead of a static class?
+    internal static class DocumentCloner
+    {
+        public static Document Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Data/Synthesizer.NHibernate/Repositories/SampleRepository.cs
+++ Data/Synthesizer.NHibernate/Repositories/SampleRepository.cs
@@ -6,9 +6,9 @@ namespace JJ.Data.Synthesizer.NHibernate.Repositories
 	[UsedImplicitly]
 	public class SampleRepository : DefaultRepositories.SampleRepository
 	{
-		private new readonly SynthesizerContext _context;
+		private new readonly SynthesizerNHibernateContext _context;
 
-		public SampleRepository(IContext context) : base(context) => _context = (SynthesizerContext)context;
+		public SampleRepository(IContext context) : base(context) => _context = (SynthesizerNHibernateContext)context;
 
 		public override byte[] TryGetBytes(int sampleID) => _context.TryGetSampleBytes(sampleID);
 		public override void SetBytes(int sampleID, byte[] bytes) => _context.SetSampleBytes(sampleID, bytes);
