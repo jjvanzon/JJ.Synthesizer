13823f3cfb33b2ad47b3b3cc50c20ce042433438
jjvanzon <janjoostvanzon@hotmail.com>
2018-12-12 21:58:26 +0100

    Trivial cleanup. Correct JJ.sln.

--- Data/Synthesizer.JsonBlobZip/SynthesizerJsonBlobZipContext.cs
+++ Data/Synthesizer.JsonBlobZip/SynthesizerJsonBlobZipContext.cs
@@ -1,38 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Reflection;
+using JJ.Data.Synthesizer.Entities;
 using JJ.Framework.Data.JsonBlobZip;
 
 namespace JJ.Data.Synthesizer.JsonBlobZip
 {
-    public class SynthesizerJsonBlobZipContext : JsonBlobZipContext
+    public class SynthesizerJsonBlobZipContext : JsonBlobZipContext<Document>
     {
         public SynthesizerJsonBlobZipContext(string location, Assembly modelAssembly, Assembly mappingAssembly, string dialect)
             : base(location, modelAssembly, mappingAssembly, dialect)
-        {
-
-        }
-
-        public override TEntity TryGet<TEntity>(object id) => throw new NotImplementedException();
-
-        public override TEntity Create<TEntity>() => throw new NotImplementedException();
-
-        public override void Insert(object entity) => throw new NotImplementedException();
-
-        public override void Update(object entity) => throw new NotImplementedException();
-
-        public override void Delete(object entity) => throw new NotImplementedException();
-
-        public override IEnumerable<TEntity> Query<TEntity>() => throw new NotImplementedException();
-
-        public override void Commit() => throw new NotImplementedException();
-
-        public override void Flush() => throw new NotImplementedException();
-
-        public override void Dispose() => throw new NotImplementedException();
-
-        public override void Rollback() => throw new NotImplementedException();
-
-
+        { }
     }
 }
