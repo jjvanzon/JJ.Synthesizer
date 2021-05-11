891c391b4b1820e71583d1f429600d2fe8abe412
jjvanzon <janjoostvanzon@hotmail.com>
2021-05-11 16:20:27 +0200

    ✍ Synthesizer AssemblyInfo including version number update.

--- Third Party/Lomont/Properties/AssemblyInfo.cs
+++ Third Party/Lomont/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lomont")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Utilities/Synthesizer.DataMigration/Properties/AssemblyInfo.cs
+++ Utilities/Synthesizer.DataMigration/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jan-Joost van Zon")]
-[assembly: AssemblyProduct("JJ.Utilities.Synthesizer.DataMigration")]
+[assembly: AssemblyProduct("JJ.Synthesizer")]
 [assembly: AssemblyCopyright("Copyright © 2015 - 2018 Jan-Joost van Zon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -30,6 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.*")]
