bf11277b8e94f442009703797c5d2d180823fbc1
jjvanzon <janjoostvanzon@hotmail.com>
2023-04-07 21:23:00 +0200

    🖥️ RawToWav Utility: Trying to use SimpleProcessForm from JJ.Framework.WinForms, but designer seems not to be loadable.

--- Utilities/RawToWav/MainForm.cs
+++ Utilities/RawToWav/MainForm.cs
@@ -1,9 +1,16 @@
 namespace JJ.Utilities.Synthesizer.RawToWav;
 
-public partial class MainForm : Form
+public partial class MainForm : SimpleProcessForm
 {
     public MainForm()
     {
         InitializeComponent();
+
+        OnRunProcess += MainForm_OnRunProcess;
+    }
+
+    private void MainForm_OnRunProcess(object? sender, EventArgs e)
+    {
+        
     }
 }
