fd75df9f4897b84217de2fa1ff99aca50b7457b9
jjvanzon <janjoostvanzon@hotmail.com>
2024-01-14 06:34:45 +0100

    Work in progress. Quasi-stach.

---
+++ Utilities/RawToWav/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using JJ.Framework.WinForms.Forms;
+
+namespace RawToWav
+{
+    public partial class MainForm : SimpleProcessForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
