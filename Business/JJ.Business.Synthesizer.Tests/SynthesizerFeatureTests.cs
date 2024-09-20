63745da79cff824036f5571ec64025790dd509cb
jjvanzon <jjvanzon79@gmail.com>
2024-09-20 06:39:11 +0200

    🧪 Additive test

--- Business/JJ.Business.Synthesizer.Tests/SynthesizerFeatureTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/SynthesizerFeatureTests.cs
@@ -25,6 +25,10 @@ namespace JJ.Business.Synthesizer.Tests
 	[TestClass]
 	public class SynthesizerFeatureTests
 	{
+		/// <summary>
+		/// Generates a Sine wave with a Volume Curve, testing both Block and Line Interpolation.
+		/// Verifies data using (Warning)Validators and writes the output audio to a file.
+		/// </summary>
 		[TestMethod]
 		public void Test_Synthesizer_Sine_With_Volume_Curve()
 		{
@@ -63,6 +67,123 @@ namespace JJ.Business.Synthesizer.Tests
 				IValidator[] validators =
 				{
 					new CurveValidator(curve),
+					new VersatileOperatorValidator(outlet.Operator),
+					new AudioFileOutputValidator(audioFileOutput),
+					new AudioFileOutputWarningValidator(audioFileOutput),
+					new VersatileOperatorWarningValidator(outlet.Operator)
+				};
+				validators.ForEach(x => x.Verify());
+
+				// Calculate
+				IAudioFileOutputCalculator audioFileOutputCalculator = AudioFileOutputCalculatorFactory.CreateAudioFileOutputCalculator(audioFileOutput);
+				var sw = Stopwatch.StartNew();
+				audioFileOutputCalculator.Execute();
+				sw.Stop();
+
+				// Report
+				string filePath = Path.GetFullPath(audioFileOutput.FilePath);
+				Assert.Inconclusive($"{sw.ElapsedMilliseconds}ms{Environment.NewLine}Output file: {filePath}");
+			}
+		}
+
+		/// <summary>
+		/// TODO: Some harmonics. and use high-pitch sample in attack. Use separate curves on each.
+		/// Trigger wav header auto-detect.
+		/// </summary>
+		[TestMethod]
+		public void Test_Synthesizer_Feature_Additive()
+		{
+			using (IContext context = PersistenceHelper.CreateContext())
+			{
+				// Arrange
+				double duration = 4;
+
+				Sample sample;
+				{
+					SampleManager sampleManager = TestHelper.CreateSampleManager(context);
+					Stream stream = TestHelper.GetViolin16BitMono44100WavStream();
+					sample = sampleManager.CreateSample(stream);
+				}
+
+				Curve curve1;
+				Curve curve2;
+				Curve curve3;
+				Curve curve4;
+				{
+					var curveFactory = TestHelper.CreateCurveFactory(context);
+
+					curve1 = curveFactory.CreateCurve(duration,
+						0.00, 0.80, 1.00, null, null, null, null, null,
+						0.25, null, null, null, null, null, null, null,
+						null, null, null, null, null, null, null, null,
+						0.10, null, null, 0.02, null, null, null, 0.00);
+
+					curve2 = curveFactory.CreateCurve(duration,
+						0.00, 1.00, 0.80, null, null, null, null, null,
+						0.10, null, null, null, null, null, null, null,
+						null, null, null, null, null, null, null, null,
+						0.05, null, null, 0.01, null, null, null, 0.00);
+
+					curve3 = curveFactory.CreateCurve(duration,
+						0.30, 0.00, 0.30, null, null, null, null, null,
+						0.10, null, null, null, null, null, null, null,
+						null, null, null, null, null, null, null, null,
+						0.25, null, null, 0.10, null, null, null, 0.00);
+
+					curve4 = curveFactory.CreateCurve(duration,
+						1.00, 0.10, 0.00, null, null, null, null, null,
+						null, null, null, null, null, null, null, null,
+						null, null, null, null, null, null, null, null,
+						null, null, null, null, null, null, null, 0.00);
+				}
+
+				Outlet outlet;
+				{
+					var x = TestHelper.CreateOperatorFactory(context);
+
+					double noteFrequency = 440;
+					outlet = x.Adder
+					(
+						x.Sine
+						(
+							x.Multiply(x.CurveIn(curve1), x.Value(1.0)), 
+							x.Value(noteFrequency)
+						),
+						x.Sine
+						(
+							x.Multiply(x.CurveIn(curve2), x.Value(0.7)), 
+							x.Multiply(x.Value(noteFrequency), x.Value(2))
+						),
+						x.Sine
+						(
+							x.Multiply(x.CurveIn(curve3), x.Value(0.4)), 
+							x.Multiply(x.Value(noteFrequency), x.Value(5))
+						),
+						x.Multiply
+						(
+							x.Sample(sample), 
+							x.Multiply(x.CurveIn(curve4), x.Value(0.0))
+						)
+					);
+				}
+
+				AudioFileOutput audioFileOutput;
+				{
+					AudioFileOutputManager audioFileOutputManager = TestHelper.CreateAudioFileOutputManager(context);
+					audioFileOutput = audioFileOutputManager.CreateAudioFileOutput();
+					audioFileOutput.AudioFileOutputChannels[0].Outlet = outlet;
+					audioFileOutput.FilePath = $"{MethodBase.GetCurrentMethod().Name}.wav";
+					audioFileOutput.Duration = 4;
+					audioFileOutput.Amplifier = short.MaxValue / 4.0;
+				}
+
+				// Verify
+				IValidator[] validators =
+				{
+					new SampleValidator(sample),
+					new CurveValidator(curve1),
+					new CurveValidator(curve2),
+					new CurveValidator(curve3),
 					new VersatileOperatorValidator(outlet.Operator),
 					new AudioFileOutputValidator(audioFileOutput),
 					new AudioFileOutputWarningValidator(audioFileOutput),
