ac4c03ef2b75c96eb61d28fce78583d4f8ee764a
jjvanzon <jjvanzon79@gmail.com>
2024-09-23 07:07:14 +0200

    🧪 FM tests hard-coded: Share method Test_NoPhaseTracking_32BitFloat 2x.

--- Business/JJ.Business.Synthesizer.Tests/SynthesizerTester_FMHardCoded.cs
+++ Business/JJ.Business.Synthesizer.Tests/SynthesizerTester_FMHardCoded.cs
@@ -4,56 +4,29 @@ using System;
 using System.Reflection;
 using JJ.Business.Synthesizer.Managers;
 using JJ.Business.Synthesizer.Enums;
+using System.Runtime.CompilerServices;
 
 [TestClass]
 public class SynthesizerTester_FMHardCoded
 {
 	[TestMethod]
 	public void Test_Synthesizer_FM_HardCoded_NoPhaseTracking_32BitFloat_HardFlute_IsNoise()
-	{
-		// Audio parameters
-		int sampleRate = 44100;
-		float carrierFrequency = 440f;
-		float modulationFrequency = 880f;
-		float modulationDepth = 5f;
-		int duration = 5; // Duration in seconds
-		string outputFilePath = MethodBase.GetCurrentMethod().Name + ".wav";
-		int sampleCount = sampleRate * duration;
-
-		using (var fs = new FileStream(outputFilePath, FileMode.Create))
-		{
-			using (var bw = new BinaryWriter(fs))
-			{
-				// Write WAV header
-				bw.WriteWavHeader<Int16>(SpeakerSetupEnum.Mono, sampleRate, sampleCount);
-
-				// Generate samples
-				for (int i = 0; i < sampleCount; i++)
-				{
-					// Calculate the modulator and carrier signals
-					float modulator = (float)Math.Sin(2 * Math.PI * modulationFrequency * i / sampleRate);
-					float modulatedFrequency = carrierFrequency + modulationDepth * carrierFrequency * modulator;
-					float sample = (float)Math.Sin(2 * Math.PI * modulatedFrequency * i / sampleRate);
-					short scaledSample = (short)(sample * 32000); // Scale to 16-bit range
-
-					bw.Write(scaledSample);
-				}
-			}
-		}
-
-		Console.WriteLine($"FM sound saved to {outputFilePath}");
-	}
+		=> Test_NoPhaseTracking_32BitFloat(carrierFrequency: 440f, modulationFrequency: 880f, modulationDepth: 5f);
 
 	[TestMethod]
-	public void Test_Synthesizer_FM_HardCoded_NoPhaseTracking_32BitFloat_WapWap()
+	public void Test_Synthesizer_FM_HardCoded_NoPhaseTracking_32BitFloat_WapWap() 
+		=> Test_NoPhaseTracking_32BitFloat(carrierFrequency: 440f, modulationFrequency: 5f, modulationDepth: 0.1f);
+
+	private void Test_NoPhaseTracking_32BitFloat(
+		float carrierFrequency, 
+		float modulationFrequency, 
+		float modulationDepth,
+		[CallerMemberName] string callerMemberName = null)
 	{
 		// Audio parameters
 		int sampleRate = 44100;
-		float carrierFrequency = 440f; 
-		float modulationFrequency = 5f;
-		float modulationDepth = 0.1f;
 		int duration = 5; // Duration in seconds
-		string outputFilePath = MethodBase.GetCurrentMethod().Name + ".wav";
+		string outputFilePath = $"{callerMemberName}.wav";
 		int sampleCount = sampleRate * duration;
 
 		using (var fs = new FileStream(outputFilePath, FileMode.Create))
