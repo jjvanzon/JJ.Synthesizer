be515d56fc3bfa78dcb58578c9a0bb3538e3b98b
jjvanzon <jjvanzon79@gmail.com>
2024-09-25 06:59:01 +0200

    🧪 FM tests: busy refactoring

--- Business/JJ.Business.Synthesizer.Tests/SynthesizerTester_FM.cs
+++ Business/JJ.Business.Synthesizer.Tests/SynthesizerTester_FM.cs
@@ -8,8 +8,12 @@ using JJ.Framework.Persistence;
 using JJ.Persistence.Synthesizer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.IO;
+// ReSharper disable LocalizableElement
+// ReSharper disable BuiltInTypeReferenceStyleForMemberAccess
 
 #pragma warning disable CS0219 // Variable is assigned but its value is never used
 
@@ -33,6 +37,11 @@ namespace JJ.Business.Synthesizer.Tests
 			_operatorFactory = TestHelper.CreateOperatorFactory(_context);
 		}
 
+		public void Test_Synthesizer_FM_Tuba()
+		{
+			throw new NotImplementedException();
+		}
+
 		/// <summary>
 		/// NOTE: Version 0.0.250 does not have time tracking in its oscillator,
 		/// making the FM synthesis behave differently.
@@ -40,106 +49,58 @@ namespace JJ.Business.Synthesizer.Tests
 		public void Test_Synthesizer_FM()
 		{
 			// Arrange
-			var x = _operatorFactory;
 
-			double soundFreq;
-			double modDepth;
-			double modSpeed;
-
-			Outlet sound;
-
-			// Tuba at first: mod speed below sound freq, changes sound freq to +/- 5Hz
-			sound = FMInHertz(soundFreq: 440, modSpeed: 220, modDepth: 5);
-
-			// Flutes
-
-			// Modulated hard flute: mod speed below sound freq, changes sound freq * [-0.005, 0.005]
-			sound = FMAround0(soundFreq: 440, modSpeed: 220, modDepth: 0.005);
-
-			// High hard flute: mod speed above sound freq, changes sound freq * [-0.005, 0.005]
-			sound = FMAround0(soundFreq: 440, modSpeed: 880, modDepth: 0.005);
-
-			// Yet another flute: mod speed above sound freq, changes sound freq * 1 +/- 0.005
-			sound = FMAround1(soundFreq: 440, modSpeed: 880, modDepth: 0.005);
-
-			// Yet another flute (same?): mod speed above sound freq, changes sound freq * 1 +/- 0.005
-			sound = FMAround1(soundFreq: 220, modSpeed: 880, modDepth: 0.005);
-
-			// Ripple Effects
-
-			// Fat Metallic Ripple: mod speed below sound freq, changes sound freq +/- 10Hz
+			var soundOutlets = new List<Outlet>
 			{
-				soundFreq = 440;
-				modSpeed = 220;
-				modDepth = 10;
-				// FM with Addition
-				Outlet modulator = x.Sine(x.Value(modDepth), x.Value(modSpeed));
-				x.Sine(x.Value(1), x.Add(x.Value(soundFreq), modulator));
-			}
+				// Tuba at beginning: mod speed below sound freq, changes sound freq to +/- 5Hz
+				FMInHertz(soundFreq: 440, modSpeed: 220, modDepth: 5),
 
-			// Deep Metallic Ripple: mod speed way below sound freq, changes sound freq * 1 +/- 0.005
-			{
-				soundFreq = 880;
-				modSpeed = 55;
-				modDepth = 0.005;
+				// Flutes
+
+				// Modulated hard flute: mod speed below sound freq, changes sound freq * [-0.005, 0.005]
+				FMAround0(soundFreq: 440, modSpeed: 220, modDepth: 0.005),
+
+				// High hard flute: mod speed above sound freq, changes sound freq * [-0.005, 0.005]
+				FMAround0(soundFreq: 440, modSpeed: 880, modDepth: 0.005),
+
+				// Yet another flute: mod speed above sound freq, changes sound freq * 1 +/- 0.005
+				FMAroundFreq(soundFreq: 440, modSpeed: 880, modDepth: 0.005),
+
+				// Yet another flute: mod speed above sound freq, changes sound freq * 1 +/- 0.005
+				FMAroundFreq(soundFreq: 220, modSpeed: 880, modDepth: 0.005),
+
+				// Ripple Effects
+
+				// Fat Metallic Ripple: mod speed below sound freq, changes sound freq +/- 10Hz
+				FMInHertz(soundFreq: 440, modSpeed: 220, modDepth: 10),
+
+				// Deep Metallic Ripple: mod speed way below sound freq, changes sound freq * 1 +/- 0.005
+				FMAroundFreq(soundFreq: 880, modSpeed: 55, modDepth: 0.005),
+
+				// Fantasy Ripple Effect: mod speed way below sound freq, changes sound freq * 1 +/- 0.02
+				FMAroundFreq(soundFreq: 880, modSpeed: 10, modDepth: 0.02),
+
+				// Clean Ripple: mod speed way below sound freq, changes sound freq * 1 +/- 0.005
+				FMAroundFreq(soundFreq: 880, modSpeed: 20, modDepth: 0.005),
+
+				// Cool Double Ripple: Ensure modulator remains above 1 (ChatGPT being weird)
 				// FM with multiplication around 1
-				Outlet modulator = x.Add(x.Value(1), x.Sine(x.Value(modDepth), x.Value(modSpeed)));
-				sound = x.Sine(x.Value(1), x.Multiply(x.Value(soundFreq), modulator));
-			}
+				FMAroundFreq(soundFreq: 880, modSpeed: 10, modDepth: 0.05),
 
-			// Fantasy Ripple Effect: mod speed way below sound freq, changes sound freq * 1 +/- 0.02
-			{
-				soundFreq = 880;
-				modSpeed = 10;
-				modDepth = 0.02;
-				// FM with multiplication around 1
-				Outlet modulator = x.Add(x.Value(1), x.Sine(x.Value(modDepth), x.Value(modSpeed)));
-				sound = x.Sine(x.Value(1), x.Multiply(x.Value(soundFreq), modulator));
-			}
+				// Noise
 
-			// Clean Ripple: mod speed way below sound freq, changes sound freq * 1 +/- 0.005
-			{
-				soundFreq = 880;
-				modSpeed = 20;
-				modDepth = 0.005;
-				// FM modulate around 1
-				Outlet modulator = x.Add(x.Value(1), x.Sine(x.Value(modDepth), x.Value(modSpeed)));
-				sound = x.Sine(x.Value(1), x.Multiply(x.Value(soundFreq), modulator));
-			}
-
-			// Extreme Ripple: Ensure modulator remains above 1 (ChatGPT being weird)
-			{
-				soundFreq = 880;
-				modSpeed = 10;
-				// FM with multiplication around 1
-				//modDepth = 0.1;
-				//Outlet modulator = x.Add(x.Value(1), x.Multiply(x.Sine(x.Value(modDepth), x.Value(modSpeed)), x.Value(0.5)));
-				// Same?
-				modDepth = 0.05;
-				Outlet modulator = x.Add(x.Value(1), x.Sine(x.Value(modDepth), x.Value(modSpeed)));
-				sound = x.Sine(x.Value(1), x.Multiply(x.Value(soundFreq), modulator));
-			}
-
-			// Noise
-
-			// Beating Noise (further along the sound): mod speed much below sound freq, changes sound freq * [0.5, 1.5]
-			{
-				soundFreq = 880;
-				modSpeed = 55;
-				modDepth = 0.5;
-				// FM with multiplication around 1
-				Outlet modulator = x.Add(x.Value(1), x.Sine(x.Value(modDepth), x.Value(modSpeed)));
-				sound = x.Sine(x.Value(1), x.Multiply(x.Value(soundFreq), modulator));
-			}
+				// Beating Noise (further along the sound): mod speed much below sound freq, changes sound freq * [0.5, 1.5]
+				FMAroundFreq(soundFreq: 880, modSpeed: 55, modDepth: 0.5)
+			};
 
 			// TODO: Slowly sweeping timbre
 
 			// Configure AudioFileOutput
 			_audioFileOutput = ConfigureAudioFileOutput();
-			_audioFileOutput.AudioFileOutputChannels[0].Outlet = sound;
+			_audioFileOutput.AudioFileOutputChannels[0].Outlet = soundOutlets[0];
 
 			// Verify
-			AssertEntities(sound);
+			AssertEntities(soundOutlets);
 
 			// Calculate
 			Stopwatch stopWatch = Calculate();
@@ -177,7 +138,7 @@ namespace JJ.Business.Synthesizer.Tests
 		/// <summary>
 		/// FM with multiplication around 1.
 		/// </summary>
-		private Outlet FMAround1(double soundFreq, double modSpeed, double modDepth)
+		private Outlet FMAroundFreq(double soundFreq, double modSpeed, double modDepth)
 		{
 			OperatorFactory x = _operatorFactory;
 
@@ -195,11 +156,21 @@ namespace JJ.Business.Synthesizer.Tests
 			return audioFileOutput;
 		}
 
-		private void AssertEntities(Outlet outlet)
+		private void AssertEntities(IList<Outlet> outlets)
 		{
-			new VersatileOperatorValidator(outlet.Operator).Verify();
+			Console.WriteLine($"Validating {nameof(AudioFileOutput)}.");
+
 			_audioFileOutputManager.ValidateAudioFileOutput(_audioFileOutput).Verify();
-			new VersatileOperatorWarningValidator(outlet.Operator).Verify();
+
+			for (var i = 0; i < outlets.Count; i++)
+			{
+				Outlet outlet = outlets[i];
+
+				Console.WriteLine($"Validating {nameof(outlet)} {i + 1}.");
+
+				new VersatileOperatorValidator(outlet.Operator).Verify();
+				new VersatileOperatorWarningValidator(outlet.Operator).Verify();
+			}
 		}
 
 		private Stopwatch Calculate()
