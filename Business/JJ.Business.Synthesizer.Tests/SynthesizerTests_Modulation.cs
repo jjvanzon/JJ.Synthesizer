d0d6d75a7fc5bc88e0e7f6f7496f3563b6d1e016
jjvanzon <jjvanzon79@gmail.com>
2024-10-04 18:24:45 +0200

    📈 Synth tests: ASCII curves first draft

---
+++ Business/JJ.Business.Synthesizer.Tests/SynthesizerTests_Modulation.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using JJ.Business.Synthesizer.EntityWrappers;
+using JJ.Business.Synthesizer.Tests.Wishes;
+using JJ.Framework.Persistence;
+using JJ.Persistence.Synthesizer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Business.Synthesizer.Tests
+{
+    [TestClass]
+    public class SynthesizerTests_Modulation : SynthesizerSugarBase
+    {
+        /// <summary> Constructor for test runner. </summary>
+        [UsedImplicitly]
+        public SynthesizerTests_Modulation()
+        { }
+
+        /// <summary> Constructor allowing each test to run in its own instance. </summary>
+        public SynthesizerTests_Modulation(IContext context)
+            : base(context, beat: 0.4, bar: 4 * 0.4)
+        { }
+        
+        [TestMethod]
+        public void Test_Synthesizer_Modulations_DrawnOutCurve()
+        {
+            Curve curve = DrawnOutCurve;
+            Outlet outlet = CurveIn(curve);
+            WriteToAudioFile(outlet, 1.0, 1.0);
+        }
+        
+        private Curve DrawnOutCurve => CurveFactory.CreateCurve(
+            "   x                 ",
+            " x   x               ",
+            "                     ",
+            "           x         ",
+            "x                   x");
+    }
+}
--- Business/JJ.Business.Synthesizer.Tests/Wishes/CurveWishes.cs
+++ Business/JJ.Business.Synthesizer.Tests/Wishes/CurveWishes.cs
@@ -11,21 +11,99 @@ namespace JJ.Business.Synthesizer.Tests.Wishes
 {
     public static class CurveFactoryExtensions
     {
+        public static Curve CreateCurve(this CurveFactory curveFactory, IList<NodeInfo> nodeInfos)
+            => curveFactory.CreateCurve(nodeInfos.ToArray());
+
         /// <inheritdoc cref="CreateCurveDocs" />
         public static Curve CreateCurve(this CurveFactory curveFactory, params (double time, double value)[] nodeTuples)
-            => CreateCurve(curveFactory, (IList<(double x, double y)>)nodeTuples);
+            => curveFactory.CreateCurve((IList<(double x, double y)>)nodeTuples);
 
         /// <inheritdoc cref="CreateCurveDocs" />
-        public static Curve CreateCurve(this CurveFactory curveFactory, IList<(double time, double value)> nodeTuples)
+        public static Curve CreateCurve([NotNull] this CurveFactory curveFactory, [NotNull] IList<(double time, double value)> nodeTuples)
         {
+            if (curveFactory == null) throw new NullException(() => curveFactory);
             if (nodeTuples == null) throw new NullException(() => nodeTuples);
-
             var nodeInfos = nodeTuples.Select(x => new NodeInfo(x.Item1, x.Item2)).ToArray();
-            
             Curve curve = curveFactory.CreateCurve(nodeInfos);
-
             return curve;
         }
+
+        public static Curve CreateCurve([NotNull] this CurveFactory curveFactory, [NotNull] IList<string> lines)
+            => curveFactory.CreateCurve(lines.ToArray());
+
+        /*
+        public static Curve CreateCurve([NotNull] this CurveFactory curveFactory, [NotNull] params string[] lines)
+            => curveFactory.CreateCurve(0, 1, 0, 1, lines);*/
+
+        /// <summary>
+        /// Create a Curve from a list of strings, that 'ASCII-encode' the curve, creating a block with time on the horizontal axis and values on the vertical axis. A space character is just empty space, but that character is configurable. Any other character is seen as a data point.
+        /// </summary>
+        public static Curve CreateCurve([NotNull] this CurveFactory curveFactory, [NotNull] params string[] lines)
+        {
+            if (curveFactory == null) throw new NullException(() => curveFactory);
+            if (lines == null) throw new NullException(() => lines);
+            if (lines.Length == 0) throw new Exception($"{lines} collection empty.");
+            if (lines.Any(x => x.Length == 0)) throw new Exception($"There are empty {lines}.");
+            
+            // Helper variables
+            double startTime = 0.0;
+            double totalTime = 1.0;
+            double minValue = 0.0;
+            double maxValue = 1.0;
+            double timeSpan = totalTime - startTime;
+            double valueSpan = maxValue - startTime;
+            double characterSpan = lines.Max(x => x.Length) - 1;
+            double lineSpan = lines.Length - 1;
+            
+            // Background character is the most used character
+            char backgroundChar = lines.SelectMany(c => c)
+                                       .GroupBy(c => c)
+                                       .OrderByDescending(g => g.Count())
+                                       .Select(g => g.Key)
+                                       .FirstOrDefault();
+
+            // Note that the values rise from top to bottom,
+            // So reverse the lines
+            
+            var nodes = new List<NodeInfo>();
+            for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
+            {
+                var line = lines[lineIndex];
+                
+                for (int charIndex = 0; charIndex < line.Length; charIndex++)
+                {
+                    if (line[charIndex] == backgroundChar)
+                    {
+                        continue;
+                    }
+
+                    // Note that values rise from bottom to top, 
+                    // which is the opposite of how text lines are usually numbered.
+                    double reversedLineIndex = lineSpan - lineIndex; 
+
+                    double time = startTime + charIndex / characterSpan * timeSpan;
+                    double value = minValue + reversedLineIndex / lineSpan * valueSpan;
+                    
+                    nodes.Add(new NodeInfo(time, value));
+                }
+            }
+            
+            return curveFactory.CreateCurve(nodes.OrderBy(x => x.Time).ToArray());
+        }
+        
+        /*
+        // Create a Curve from a list of strings, that 'ASCII-encode' the curve, creating a block with time on the horizontal axis and values on the vertical axis. A space character is just empty space, but that character is configurable. Any other character is seen as a data point.    
+        public static Curve CreateCurveFromUnicode(this CurveFactory curveFactory, IList<string> lines, char spaceChar)
+        {
+            // Note that the values rise from top to bottom, which is the opposite of how text lines are usually numbered.
+            var linesReversed = lines.Reverse().ToList();
+
+            // Note that the data point character is not monospace. Does a trick to find the center of the character, by counting the in-between space characters and using halves to represent intermediate data point characters, then scaling it to a given time span in seconds. Values enjoy a more equally spaced mapping due to lines being of equal height.
+            throw new NotImplementedException();
+        }
+        */
+
+        #region Docs
 
         /// <summary>
         /// Shorthand that takes tuples like (0, 0), (0.1, 0.2), (0.2, 1.0)
@@ -40,5 +118,7 @@ namespace JJ.Business.Synthesizer.Tests.Wishes
         [UsedImplicitly]
         private static Curve CreateCurveDocs(this CurveFactory curveFactory, object nodeTuples) 
             => throw new NotSupportedException();
+        
+        #endregion
     }
 }
