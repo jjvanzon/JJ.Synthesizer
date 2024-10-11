a088d8c7d7d256a1f6f4fbea12e34feb13835710
jjvanzon <jjvanzon79@gmail.com>
2024-10-11 22:23:08 +0200

    🍳 Synth tests: Working on PitchPan

--- Business/JJ.Business.Synthesizer.Tests/Wishes/OperatorWishes.SynthSugarBase.cs
+++ Business/JJ.Business.Synthesizer.Tests/Wishes/OperatorWishes.SynthSugarBase.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Security.AccessControl;
 using JJ.Business.Synthesizer.EntityWrappers;
 using JJ.Business.Synthesizer.Factories;
 using JJ.Business.Synthesizer.Tests.Helpers;
 using JJ.Persistence.Synthesizer;
+using static JJ.Business.Synthesizer.Tests.Wishes.Notes;
 
 // ReSharper disable MemberCanBeProtected.Global
 // ReSharper disable AssignmentInsteadOfDiscard
@@ -45,24 +47,115 @@ namespace JJ.Business.Synthesizer.Tests.Wishes
             return Multiply(sound, modulator);
         }
 
-        public (Outlet left, Outlet right) Panning((Outlet left, Outlet right) channels, Outlet panning)
+        /// <summary>
+        /// Applies panning to a stereo signal by adjusting the left and right
+        /// channel volumes based on the specified panning value.
+        /// </summary>
+        /// <param name="channels">
+        /// A tuple containing the left and right channels of the stereo signal.
+        /// </param>
+        /// <param name="panning">
+        /// An <see cref="Outlet"/> representing the panning value,
+        /// where 0 is fully left, 1 is fully right, and 0.5 is centered.
+        /// </param>
+        /// <returns>
+        /// A tuple containing the adjusted left and right channels
+        /// after applying the panning.
+        /// </returns>
+        public (Outlet left, Outlet right) Panning(
+            (Outlet left, Outlet right) channels, 
+            Outlet panning)
         {
             var leftPan = Multiply(channels.left, Substract(_[1], panning));
             var rightPan = Multiply(channels.right, panning);
             return (leftPan, rightPan);
         }
 
+        /// <summary>
+        /// Applies a panbrello effect to a stereo signal by modulating the panning
+        /// with a sine wave based on the specified speed and depth.
+        /// </summary>
+        /// <param name="channels">
+        /// A tuple containing the left and right channels of the stereo signal.
+        /// </param>
+        /// <param name="panbrello">
+        /// A tuple containing the speed and depth of the panbrello effect.
+        /// If not provided, default values will be used.
+        /// </param>
+        /// <returns>
+        /// A tuple containing the adjusted left and right channels
+        /// after applying the panbrello effect.
+        /// </returns>
         public (Outlet left, Outlet right) Panbrello(
             (Outlet left, Outlet right) channels,
-            (Outlet speed, Outlet depth) panbrello)
+            (Outlet speed, Outlet depth) panbrello = default)
         {
             panbrello.speed = panbrello.speed ?? _[3];
             panbrello.depth = panbrello.depth ?? _[0.33];
 
-            var modulator = Add(_[1], Sine(panbrello.depth, panbrello.speed));
+            // 0.5 is in the middle. 0 is left, 1 is right.
+            var sine = Sine(panbrello.depth, panbrello.speed); // [-1,+1]
+            var halfSine = Multiply(_[0.5], sine); // [-0.5,+0.5]
+            var zeroToOne = Add(_[0.5], halfSine); // [0,1]
 
-            return Panning(channels, modulator);
+            return Panning(channels, zeroToOne);
         }
+
+        /// <summary>
+        /// If the frequency is the referenceFrequency,
+        /// then the new panning is the referencePanning.
+        /// Calculates the new panning by extrapolating.
+        /// </summary>
+        /// <param name="actualFrequency"></param>
+        /// <param name="centerFrequency"></param>
+        /// <param name="referenceFrequency"></param>
+        /// <param name="referencePanning"></param>
+        /// <returns></returns>
+        public Outlet PitchPan(
+            Outlet actualFrequency, Outlet centerFrequency,
+            Outlet referenceFrequency, Outlet referencePanning)
+        {
+            // Defaults
+            centerFrequency = centerFrequency ?? _[A4];
+            referenceFrequency = referenceFrequency ?? _[E4];
+            referencePanning = referencePanning ?? _[0.6];
+
+            Outlet centerPanning = _[0.5];
+
+            // Calculate intervals relative to the center frequency
+            Outlet referenceInterval = Divide(referenceFrequency, centerFrequency);
+            Outlet actualInterval = Divide(actualFrequency, centerFrequency);
+
+            Outlet factor = Multiply(actualInterval, referenceInterval);
+
+            Outlet newPanningDeviation = Multiply(Substract(referencePanning, centerPanning), factor);
+            // AI's correction:
+            //Outlet newPanningDeviation = Multiply(Substract(referencePanning, centerPanning), Substract(factor, _[1]));
+            Outlet newPanning = Add(centerPanning, newPanningDeviation);
+
+
+            return newPanning;
+        }
+
+        //public (Outlet left, Outlet right) PitchPan(
+        //    (Outlet left, Outlet right) channels, 
+        //    (Outlet centerPitch, Outlet pitchOffset, Outlet panningOffset) pitchPan = default)
+        //{
+        //    // Defaults
+        //    pitchPan.centerPitch = pitchPan.centerPitch ?? _[A4]; 
+        //    pitchPan.pitchOffset = pitchPan.pitchOffset ?? _[E4]; 
+        //    pitchPan.panningOffset = pitchPan.panningOffset ?? _[0.1];
+
+        //    // Calculate panning, shifted in accordance to pitch
+        //    var pitchModulation = Add(pitchPan.centerPitch, 
+        //                              Multiply(pitchPan.pitchOffset, panningOffset ));
+
+
+        //    //var pitchModulation = Add(pitchPan.centerPitch, Multiply(pitchPan.pitchOffset, Sine(_[1], _[0.5])));
+        //    //var pannedChannels = Panning(channels, pitchPan.panningOffset);
+
+        //    //return (Multiply(pannedChannels.left, pitchModulation), Multiply(pannedChannels.right, pitchModulation));
+        //}
 
         /// <inheritdoc cref="ValueIndexer" />
         public ValueIndexer _;
