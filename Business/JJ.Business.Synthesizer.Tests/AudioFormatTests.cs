9203c4c4a68f8914093f70e129d6751add1a918d
jjvanzon <jjvanzon79@gmail.com>
2024-10-14 21:21:09 +0200

    🧑‍💻 Audio format tests: reorganize test code.

--- Business/JJ.Business.Synthesizer.Tests/AudioFormatTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/AudioFormatTests.cs
@@ -9,59 +9,61 @@ namespace JJ.Business.Synthesizer.Tests
     public class AudioFormatTests : SynthesizerSugar
     {
         [TestMethod]
-        public void Test_AudioFileFormat_Stereo_16Bit_Wav()
+        public void Test_AudioFileFormat_Wav_Stereo_16Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Wav);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Wav);
+            
+            Outlet sample = Sample()
         }
         
         [TestMethod]
-        public void Test_AudioFileFormat_Mono_16Bit_Wav()
+        public void Test_AudioFileFormat_Wav_Mono_16Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Wav);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Wav);
         }
         
         [TestMethod]
-        public void Test_AudioFileFormat_Stereo_8Bit_Wav()
+        public void Test_AudioFileFormat_Wav_Stereo_8Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Wav);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Wav);
         }
         
         [TestMethod]
-        public void Test_AudioFileFormat_Mono_8Bit_Wav()
+        public void Test_AudioFileFormat_Wav_Mono_8Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Wav);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Wav);
         }
 
         [TestMethod]
-        public void Test_AudioFileFormat_Stereo_16Bit_Raw()
+        public void Test_AudioFileFormat_Raw_Stereo_16Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Raw);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Raw);
         }
         
         [TestMethod]
-        public void Test_AudioFileFormat_Mono_16Bit_Raw()
+        public void Test_AudioFileFormat_Raw_Mono_16Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Raw);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Int16, AudioFileFormatEnum.Raw);
         }
         
         [TestMethod]
-        public void Test_AudioFileFormat_Stereo_8Bit_Raw()
+        public void Test_AudioFileFormat_Raw_Stereo_8Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Raw);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Stereo, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Raw);
         }
         
         [TestMethod]
-        public void Test_AudioFileFormat_Mono_8Bit_Raw()
+        public void Test_AudioFileFormat_Raw_Mono_8Bit()
         {
-            Outlet createOutlet() => Panning(Sine(_[1]), _[0.25]);
-            SaveWav(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Raw);
+            Outlet createOutlet() => Panning(Sine(A4), _[0.25]);
+            SaveAudio(createOutlet, 0, 0, SpeakerSetupEnum.Mono, SampleDataTypeEnum.Byte, AudioFileFormatEnum.Raw);
         }
     }
 }
