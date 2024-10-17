94b1d47648878bc2b3ac8d57e14d49d854f2e65f
jjvanzon <jjvanzon79@gmail.com>
2024-10-17 21:29:57 +0200

    🧪 Synth tests: Test some throw statements for better code coverage.

--- Business/JJ.Business.Synthesizer.Tests/ThrowTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/ThrowTests.cs
@@ -1,9 +1,11 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 using JJ.Business.Synthesizer.Enums;
 using JJ.Business.Synthesizer.Tests.Helpers;
 using JJ.Business.Synthesizer.Tests.Wishes;
 using JJ.Framework.Persistence;
 using JJ.Framework.Testing;
+using JJ.Persistence.Synthesizer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 #pragma warning disable CS0414 // Field is assigned but its value is never used
 #pragma warning disable CS0169 // Field is never used
@@ -11,15 +13,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JJ.Business.Synthesizer.Tests
 {
-    /// <summary>
-    /// Aims to tests rare exception for code coverage of throw statements.
-    /// </summary>
+    /// <summary> Aims to test rare exception for code coverage. </summary>
     [TestCategory("Wip")]
     [TestClass]
     public class ThrowTests : SynthesizerSugar
     {
         private ChannelEnum _invalidChannelEnum;
-        private int _dummy;
+        private int _channelIndex;
 
         [UsedImplicitly]
         public ThrowTests()
@@ -34,15 +34,15 @@ namespace JJ.Business.Synthesizer.Tests
         public void Test_Exceptions()
         {             
             using (IContext context = PersistenceHelper.CreateContext())
-                new ThrowTests(context).Throw_OperatorWishes_Extensions_RunTest();
+                new ThrowTests(context).ExceptionTests();
         }
 
-        public void Throw_OperatorWishes_Extensions_RunTest()
+        public void ExceptionTests()
         {
             Channel = _invalidChannelEnum = 0;
 
             // OperatorWishes.SynthesizerSugar.ChannelIndex InvalidChannelEnum
-            AssertHelper.ThrowsException(() => { _dummy = ChannelIndex; });
+            AssertHelper.ThrowsException(() => { _channelIndex = ChannelIndex; });
             
             // OperatorWishes.SynthesizerSugar.Panning WithConst_InvalidChannelEnum 
             AssertHelper.ThrowsException(() => Panning(Sine(), _[0.25]));
@@ -60,7 +60,31 @@ namespace JJ.Business.Synthesizer.Tests
             AssertHelper.ThrowsException(() => SaveAudio(() => Sine(), speakerSetupEnum: SpeakerSetupEnum.Undefined));
             
             // AudioFileWishes.Extensions.GetChannelCount SpeakerSetupNotSupported
-            // ...
+            AssertHelper.ThrowsException(() => SpeakerSetupEnum.Undefined.GetChannelCount());
+
+            // AudioFileWishes.Extensions.GetSpeakerSetupEnum ChannelCountNotSupported
+            AssertHelper.ThrowsException(() => 0.GetSpeakerSetupEnum());
+            
+            // AudioFileWishes.Extensions.GetSampleDataTypeEnum SampleDataTypeNotSupported
+            AssertHelper.ThrowsException(() => AudioConversionExtensionWishes.GetSampleDataTypeEnum<long>());
+
+            // AudioFileWishes.Extensions.GetFileExtension AudioFileFormatNotSupported
+            AssertHelper.ThrowsException(() => AudioFileFormatEnum.Undefined.GetFileExtension());
+            
+            // AudioFileWishes.Extensions.GetMaxAmplitude SampleDataTypeNotSupported
+            AssertHelper.ThrowsException(() => SampleDataTypeEnum.Undefined.GetMaxAmplitude());
+            
+            // AudioFileWishes.Extensions.GetHeaderLength AudioFileFormatNotSupported
+            AssertHelper.ThrowsException(() => AudioFileFormatEnum.Undefined.GetHeaderLength());
+            
+            // ModulationTests.Detunica EnvelopeVariationNotSupported
+            AssertHelper.ThrowsException(() => new ModulationTests().Detunica(envelopeVariation: -1));
+        
+            // ModulationTests.DeepEcho ChannelEnumNotSupported
+            AssertHelper.ThrowsException(() => new ModulationTests().DeepEcho(Sine()));
+            
+            // AudioFormatTests.GetValueTolerance CombinationOfValues_NotSupported
+            AssertHelper.ThrowsException(() => new AudioFormatTests().GetValueTolerance(true, InterpolationTypeEnum.Undefined, SampleDataTypeEnum.Undefined));
         }
     }
 }
