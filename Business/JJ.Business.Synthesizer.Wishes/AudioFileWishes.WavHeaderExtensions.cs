def23eff2ca6f4d24b15f0e0abff2bd29a9edf5b
jjvanzon <jjvanzon79@gmail.com>
2024-10-19 01:45:25 +0200

    📄 SynthWishes: Working on WavHeader extension overloads.

--- Business/JJ.Business.Synthesizer.Wishes/AudioFileWishes.WavHeaderExtensions.cs
+++ Business/JJ.Business.Synthesizer.Wishes/AudioFileWishes.WavHeaderExtensions.cs
@@ -3,7 +3,7 @@ using System.IO;
 using JJ.Business.Synthesizer.Converters;
 using JJ.Business.Synthesizer.EntityWrappers;
 using JJ.Business.Synthesizer.Enums;
-using JJ.Business.Synthesizer.Helpers;
+using JJ.Business.Synthesizer.Extensions;
 using JJ.Business.Synthesizer.Infos;
 using JJ.Business.Synthesizer.Managers;
 using JJ.Business.Synthesizer.Structs;
@@ -18,170 +18,185 @@ namespace JJ.Business.Synthesizer.Wishes
         
         /// <summary> Overload that takes <see cref="TSampleDataType"/> and <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader<TSampleDataType>(
-            this BinaryWriter bw,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-        {
-            WriteWavHeader(
-                bw, AudioFileExtensionWishes.GetSampleDataTypeEnum<TSampleDataType>(),
+            this BinaryWriter writer,
+            SpeakerSetupEnum speakerSetupEnum, int samplingRate, int sampleCount)
+            => WriteWavHeader(
+                writer, AudioFileExtensionWishes.GetSampleDataTypeEnum<TSampleDataType>(),
                 speakerSetupEnum, samplingRate, sampleCount);
-        }
 
         /// <summary> Overload that takes <see cref="TSampleDataType"/> </summary>
         public static void WriteWavHeader<TSampleDataType>(
-            this BinaryWriter bw,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-        {
-            WriteWavHeader(
-                bw, AudioFileExtensionWishes.GetSampleDataTypeEnum<TSampleDataType>(),
+            this BinaryWriter writer,
+            int channelCount, int samplingRate, int sampleCount)
+            => WriteWavHeader(
+                writer, AudioFileExtensionWishes.GetSampleDataTypeEnum<TSampleDataType>(),
                 channelCount, samplingRate, sampleCount);
-        }
 
         /// <summary> Overload that takes <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader(
-            this BinaryWriter bw,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-        {
-            WriteWavHeader(
-                bw, sampleDataTypeEnum, speakerSetupEnum.GetChannelCount(),
-                samplingRate, sampleCount);
-        }
+            this BinaryWriter writer,
+            SampleDataTypeEnum sampleDataTypeEnum, SpeakerSetupEnum speakerSetupEnum, int samplingRate, int sampleCount)
+            => WriteWavHeader(
+                writer, sampleDataTypeEnum, speakerSetupEnum.GetChannelCount(), samplingRate, sampleCount);
 
         /// <summary> Overload that takes a more flat list of values. </summary>
         public static void WriteWavHeader(
-            this BinaryWriter bw,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
+            this BinaryWriter writer,
+            SampleDataTypeEnum sampleDataTypeEnum, int channelCount, int samplingRate, int sampleCount)
         {
-            var audioFileInfo = new AudioFileInfo
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
+
+            WriteWavHeader(writer, new AudioFileInfo
             {
+                BytesPerValue = sampleDataTypeEnum.SizeOf(),
+                ChannelCount = channelCount,
                 SamplingRate = samplingRate,
                 SampleCount = sampleCount,
-                BytesPerValue = SampleDataTypeHelper.SizeOf(sampleDataTypeEnum),
-                ChannelCount = channelCount
-            };
-
-            var wavHeaderStruct = WavHeaderManager.CreateWavHeaderStruct(audioFileInfo);
-
-            bw.WriteStruct(wavHeaderStruct);
+            });
         }
 
+        public static void WriteWavHeader(
+            this BinaryWriter writer, 
+            AudioFileOutput audioFileOutput, int sampleCount)
+        {
+            WriteWavHeader(writer, new AudioFileInfo
+            {
+                BytesPerValue = audioFileOutput.SampleDataType.SizeOf(),
+                ChannelCount = audioFileOutput.GetChannelCount(),
+                SamplingRate = audioFileOutput.SamplingRate,
+                SampleCount = sampleCount
+            });
+        }
+
+        public static void WriteWavHeader(
+            this BinaryWriter writer, 
+            AudioFileInfo audioFileInfo)
+        {
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
+            
+            var wavHeaderStruct = WavHeaderManager.CreateWavHeaderStruct(audioFileInfo);
+
+            writer.WriteStruct(wavHeaderStruct);
+        }
+        
         // WriteWavHeader (with Stream)
 
         /// <summary> Overload that takes <see cref="TSampleDataType"/> and <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader<TSampleDataType>(
             this Stream stream,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            SpeakerSetupEnum speakerSetupEnum, int samplingRate, int sampleCount)
+            => new BinaryWriter(stream).WriteWavHeader<TSampleDataType>(speakerSetupEnum, samplingRate, sampleCount);
 
         /// <summary> Overload that takes <see cref="TSampleDataType"/> </summary>
         public static void WriteWavHeader<TSampleDataType>(
             this Stream stream,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            int channelCount, int samplingRate, int sampleCount)
+            => new BinaryWriter(stream).WriteWavHeader<TSampleDataType>(channelCount, samplingRate, sampleCount);
 
         /// <summary> Overload that takes <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader(
             this Stream stream,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            SampleDataTypeEnum sampleDataTypeEnum, SpeakerSetupEnum speakerSetupEnum, int samplingRate, int sampleCount)
+            => new BinaryWriter(stream).WriteWavHeader(sampleDataTypeEnum, speakerSetupEnum, samplingRate, sampleCount);
 
         /// <summary> Overload that takes a more flat list of values. </summary>
         public static void WriteWavHeader(
             this Stream stream,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            SampleDataTypeEnum sampleDataTypeEnum, int channelCount, int samplingRate, int sampleCount)
+            => new BinaryWriter(stream).WriteWavHeader(sampleDataTypeEnum, channelCount, samplingRate, sampleCount);
+
+        public static void WriteWavHeader(
+            this Stream stream,
+            AudioFileOutput audioFileOutput, int sampleCount)
+            => new BinaryWriter(stream).WriteWavHeader(audioFileOutput, sampleCount);
+
+        public static void WriteWavHeader(
+            this Stream stream,
+            AudioFileInfo audioFileInfo)
+            => new BinaryWriter(stream).WriteWavHeader(audioFileInfo);
 
         // WriteWavHeader (with FilePath)
 
         /// <summary> Overload that takes <see cref="TSampleDataType"/> and <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader<TSampleDataType>(
             this string filePath,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            SpeakerSetupEnum speakerSetupEnum, int samplingRate, int sampleCount)
+        {
+            using (var stream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.Read))
+                WriteWavHeader<TSampleDataType>(stream, speakerSetupEnum, samplingRate, sampleCount);
+        }
 
         /// <summary> Overload that takes <see cref="TSampleDataType"/> </summary>
         public static void WriteWavHeader<TSampleDataType>(
             this string filePath,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            int channelCount, int samplingRate, int sampleCount)
+        {
+            using (var stream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.Read))
+                WriteWavHeader<TSampleDataType>(stream, channelCount, samplingRate, sampleCount);
+        }
 
         /// <summary> Overload that takes <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader(
             this string filePath,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            SampleDataTypeEnum sampleDataTypeEnum, SpeakerSetupEnum speakerSetupEnum, int samplingRate, int sampleCount)
+        {
+            using (var stream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.Read))
+                WriteWavHeader(stream, sampleDataTypeEnum, speakerSetupEnum, samplingRate, sampleCount);
+        }
 
         /// <summary> Overload that takes a more flat list of values. </summary>
         public static void WriteWavHeader(
             this string filePath,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            SampleDataTypeEnum sampleDataTypeEnum, int channelCount, int samplingRate, int sampleCount)
+        {
+            using (var stream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.Read))
+                WriteWavHeader(stream, sampleDataTypeEnum, channelCount, samplingRate, sampleCount);
+        }
         
+        public static void WriteWavHeader(
+            this string filePath,
+            AudioFileOutput audioFileOutput, int sampleCount)
+        {
+            using (var stream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.Read))
+                WriteWavHeader(stream, audioFileOutput, sampleCount);
+        }
+
+        public static void WriteWavHeader(
+            this string filePath,
+            AudioFileInfo audioFileInfo)
+        {
+            using (var stream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.Read))
+                WriteWavHeader(stream, audioFileInfo);
+        }
+
         // WriteWavHeader (with AudioFileOutput)
         
-        /// <summary> Overload that takes <see cref="TSampleDataType"/> and <see cref="SpeakerSetupEnum"/>. </summary>
-        public static void WriteWavHeader<TSampleDataType>(
-            this AudioFileOutput audioFileOutput,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
-
-        /// <summary> Overload that takes <see cref="TSampleDataType"/> </summary>
-        public static void WriteWavHeader<TSampleDataType>(
-            this AudioFileOutput audioFileOutput,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
-
-        /// <summary> Overload that takes <see cref="SpeakerSetupEnum"/>. </summary>
         public static void WriteWavHeader(
-            this AudioFileOutput audioFileOutput,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            SpeakerSetupEnum speakerSetupEnum,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
+            this AudioFileOutput audioFileOutput, BinaryWriter writer, int sampleCount)
+            => WriteWavHeader(writer, audioFileOutput, sampleCount);
 
-        /// <summary> Overload that takes a more flat list of values. </summary>
         public static void WriteWavHeader(
-            this AudioFileOutput audioFileOutput,
-            SampleDataTypeEnum sampleDataTypeEnum,
-            int channelCount,
-            int samplingRate,
-            int sampleCount)
-            => throw new NotImplementedException();
-        
+            this AudioFileOutput audioFileOutput, Stream stream, int sampleCount)
+            => WriteWavHeader(stream, audioFileOutput, sampleCount);
+
+        public static void WriteWavHeader(
+            this AudioFileOutput audioFileOutput, string filePath, int sampleCount)
+            => WriteWavHeader(filePath, audioFileOutput, sampleCount);
+
+        // WriteWavHeader (with AudioFileInfo)
+
+        public static void WriteWavHeader(
+            this AudioFileInfo audioFileInfo, BinaryWriter writer)
+            => WriteWavHeader(writer, audioFileInfo);
+
+        public static void WriteWavHeader(
+            this AudioFileInfo audioFileInfo, Stream stream)
+            => WriteWavHeader(stream, audioFileInfo);
+
+        public static void WriteWavHeader(
+            this AudioFileInfo audioFileInfo, string filePath)
+            => WriteWavHeader(filePath, audioFileInfo);
+
         // Reading Wav Header
 
         public static WavHeaderStruct ReadWavHeaderStruct(this string filePath)
