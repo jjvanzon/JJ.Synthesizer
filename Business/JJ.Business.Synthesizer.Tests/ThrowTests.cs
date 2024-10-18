9f0f69cb66a47fc67f57969a4f59ac23eff5bf32
jjvanzon <jjvanzon79@gmail.com>
2024-10-18 14:00:49 +0200

    🍱 Throw tests: Move around and subdivise test code.

--- Business/JJ.Business.Synthesizer.Tests/ThrowTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/ThrowTests.cs
@@ -27,8 +27,8 @@ namespace JJ.Business.Synthesizer.Tests
     [TestClass]
     public class ThrowTests : SynthesizerSugar
     {
-        private ChannelEnum _invalidChannelEnum;
         private int _channelIndex;
+        private ChannelEnum _invalidChannelEnum;
         private Stream _emptyStream = new MemoryStream(new byte[] { });
         private SampleDataType _invalidSampleDataType = new SampleDataType();
         private AudioFileFormat _invalidAudioFileFormat = new AudioFileFormat();
@@ -43,16 +43,93 @@ namespace JJ.Business.Synthesizer.Tests
         private ThrowTests(IContext context)
             : base(context)
         { }
-                
+        
         [TestMethod]
-        public void Test_Exceptions_HardToReach()
+        public void Test_Exceptions_InWishes()
         {             
             using (IContext context = PersistenceHelper.CreateContext())
-                new ThrowTests(context).ExceptionHardToReachTests();
+                new ThrowTests(context).ExceptionsInWishes();
         }
 
-        void ExceptionHardToReachTests()
+        void ExceptionsInWishes()
         {
+            Channel = _invalidChannelEnum = 0;
+
+            // OperatorWishes.SynthesizerSugar.ChannelIndex InvalidChannelEnum
+            ThrowsException(() => { _channelIndex = ChannelIndex; });
+
+            // OperatorWishes.SynthesizerSugar.Panning WithConst_InvalidChannelEnum 
+            ThrowsException(() => Panning(Sine(), _[0.25]));
+
+            // OperatorWishes.SynthesizerSugar.Panning Dynamic_InvalidChannelEnum
+            ThrowsException(() => Panning(Sine(), CurveIn((0, 0), (0, 1))));
+
+            // CurveWishes.SynthesizerSugar.GetCurve NotFound
+            ThrowsException(() => GetCurve("Curve"));
+
+            // CurveWishes.SynthesizerSugar.GetOrCreateCurveIn Internal_CacheKeyUnresolvableFromContext
+            ThrowsException(() => GetOrCreateCurveIn(null, () => CurveIn(0)));
+
+            // AudioFileWishes.SynthesizerSugar.SaveAudio SpeakerSetupNotSupported
+            ThrowsException(() => SaveAudio(() => Sine(), speakerSetupEnum: SpeakerSetupEnum.Undefined));
+
+            // AudioFileWishes.Extensions.GetChannelCount SpeakerSetupNotSupported
+            ThrowsException(() => SpeakerSetupEnum.Undefined.GetChannelCount());
+
+            // AudioFileWishes.Extensions.GetSpeakerSetupEnum ChannelCountNotSupported
+            ThrowsException(() => 0.GetSpeakerSetupEnum());
+
+            // AudioFileWishes.Extensions.GetSampleDataTypeEnum SampleDataTypeNotSupported
+            ThrowsException(() => AudioConversionExtensionWishes.GetSampleDataTypeEnum<long>());
+
+            // AudioFileWishes.Extensions.GetFileExtension AudioFileFormatNotSupported
+            ThrowsException(() => AudioFileFormatEnum.Undefined.GetFileExtension());
+
+            // AudioFileWishes.Extensions.GetMaxAmplitude SampleDataTypeNotSupported
+            ThrowsException(() => SampleDataTypeEnum.Undefined.GetMaxAmplitude());
+
+            // AudioFileWishes.Extensions.GetHeaderLength AudioFileFormatNotSupported
+            ThrowsException(() => AudioFileFormatEnum.Undefined.GetHeaderLength());
+        }
+
+        [TestMethod]
+        public void Test_Exceptions_InTests()
+        {             
+            using (IContext context = PersistenceHelper.CreateContext())
+                new ThrowTests(context).ExceptionsInTests();
+        }
+
+        void ExceptionsInTests()
+        {
+
+            // ModulationTests.Detunica EnvelopeVariationNotSupported
+            ThrowsException(() => new ModulationTests().Detunica(envelopeVariation: -1));
+        
+            // ModulationTests.DeepEcho ChannelEnumNotSupported
+            ThrowsException(() => new ModulationTests().DeepEcho(Sine()));
+            
+            // AudioFormatTests.GetValueTolerance CombinationOfValuesNotSupported
+            ThrowsException(() => new AudioFormatTests().GetValueTolerance(true, InterpolationTypeEnum.Undefined, SampleDataTypeEnum.Undefined));
+        }
+                    
+        [TestMethod]
+        public void Test_Exceptions_InBackEnd()
+        {             
+            using (IContext context = PersistenceHelper.CreateContext())
+                new ThrowTests(context).ExceptionsInBackEnd();
+        }
+
+        void ExceptionsInBackEnd()
+        {
+            // SampleManager.CreateSample AudioFileFormatEnumNotSupported
+            ThrowsException(() => Samples.CreateSample(TestHelper.GetViolin16BitMono44100WavStream(), AudioFileFormatEnum.Undefined));
+            
+            // SampleManager.CreateWavSample WavFileAtLeast44Bytes
+            ThrowsException(() => Samples.CreateSample(_emptyStream, AudioFileFormatEnum.Wav));
+            
+            // SampleDataTypeHelper.SizeOf SampleDataTypeInvalid
+            ThrowsException(() => SampleDataTypeHelper.SizeOf(SampleDataTypeEnum.Undefined));
+
             // WavHeaderStructToAudioFileInfoConverter ChannelCountCannotBe0
             {
                 WavHeaderStruct wavHeaderStruct = TestHelper.GetValidWavHeaderStruct();
@@ -142,71 +219,5 @@ namespace JJ.Business.Synthesizer.Tests
                 ThrowsException(() => calculator.Execute());
             }
         }
-        
-        [TestMethod]
-        public void Test_Exceptions()
-        {             
-            using (IContext context = PersistenceHelper.CreateContext())
-                new ThrowTests(context).ExceptionTests();
-        }
-
-        void ExceptionTests()
-        {
-            Channel = _invalidChannelEnum = 0;
-
-            // OperatorWishes.SynthesizerSugar.ChannelIndex InvalidChannelEnum
-            ThrowsException(() => { _channelIndex = ChannelIndex; });
-            
-            // OperatorWishes.SynthesizerSugar.Panning WithConst_InvalidChannelEnum 
-            ThrowsException(() => Panning(Sine(), _[0.25]));
-            
-            // OperatorWishes.SynthesizerSugar.Panning Dynamic_InvalidChannelEnum
-            ThrowsException(() => Panning(Sine(), CurveIn((0, 0), (0, 1))));
-            
-            // CurveWishes.SynthesizerSugar.GetCurve NotFound
-            ThrowsException(() => GetCurve("Curve"));
-            
-            // CurveWishes.SynthesizerSugar.GetOrCreateCurveIn Internal_CacheKeyUnresolvableFromContext
-            ThrowsException(() => GetOrCreateCurveIn(null, () => CurveIn(0)));
-            
-            // AudioFileWishes.SynthesizerSugar.SaveAudio SpeakerSetupNotSupported
-            ThrowsException(() => SaveAudio(() => Sine(), speakerSetupEnum: SpeakerSetupEnum.Undefined));
-            
-            // AudioFileWishes.Extensions.GetChannelCount SpeakerSetupNotSupported
-            ThrowsException(() => SpeakerSetupEnum.Undefined.GetChannelCount());
-
-            // AudioFileWishes.Extensions.GetSpeakerSetupEnum ChannelCountNotSupported
-            ThrowsException(() => 0.GetSpeakerSetupEnum());
-            
-            // AudioFileWishes.Extensions.GetSampleDataTypeEnum SampleDataTypeNotSupported
-            ThrowsException(() => AudioConversionExtensionWishes.GetSampleDataTypeEnum<long>());
-
-            // AudioFileWishes.Extensions.GetFileExtension AudioFileFormatNotSupported
-            ThrowsException(() => AudioFileFormatEnum.Undefined.GetFileExtension());
-            
-            // AudioFileWishes.Extensions.GetMaxAmplitude SampleDataTypeNotSupported
-            ThrowsException(() => SampleDataTypeEnum.Undefined.GetMaxAmplitude());
-            
-            // AudioFileWishes.Extensions.GetHeaderLength AudioFileFormatNotSupported
-            ThrowsException(() => AudioFileFormatEnum.Undefined.GetHeaderLength());
-            
-            // ModulationTests.Detunica EnvelopeVariationNotSupported
-            ThrowsException(() => new ModulationTests().Detunica(envelopeVariation: -1));
-        
-            // ModulationTests.DeepEcho ChannelEnumNotSupported
-            ThrowsException(() => new ModulationTests().DeepEcho(Sine()));
-            
-            // AudioFormatTests.GetValueTolerance CombinationOfValuesNotSupported
-            ThrowsException(() => new AudioFormatTests().GetValueTolerance(true, InterpolationTypeEnum.Undefined, SampleDataTypeEnum.Undefined));
-            
-            // SampleManager.CreateSample AudioFileFormatEnumNotSupported
-            ThrowsException(() => Samples.CreateSample(TestHelper.GetViolin16BitMono44100WavStream(), AudioFileFormatEnum.Undefined));
-            
-            // SampleManager.CreateWavSample WavFileAtLeast44Bytes
-            ThrowsException(() => Samples.CreateSample(_emptyStream, AudioFileFormatEnum.Wav));
-            
-            // SampleDataTypeHelper.SizeOf SampleDataTypeInvalid
-            ThrowsException(() => SampleDataTypeHelper.SizeOf(SampleDataTypeEnum.Undefined));
-        }
-    }
+}
 }
