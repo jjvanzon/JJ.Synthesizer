cea51a3945ce57c3bc3bbdf09f6c1afb17e52dff
jjvanzon <jjvanzon79@gmail.com>
2024-10-23 07:52:54 +0200

    📻 Additive tests: Use new Sample operator creation method + refactorings.

--- Business/JJ.Business.Synthesizer.Tests/Functional/AdditiveTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/Functional/AdditiveTests.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using JJ.Business.Synthesizer.Tests.Helpers;
+﻿using JJ.Business.Synthesizer.EntityWrappers;
 using JJ.Business.Synthesizer.Wishes;
+using JJ.Business.Synthesizer.Wishes.Helpers;
 using JJ.Persistence.Synthesizer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Math;
+using static JJ.Business.Synthesizer.Tests.Helpers.TestHelper;
 
 // ReSharper disable LocalizableElement
 
@@ -21,8 +23,6 @@ namespace JJ.Business.Synthesizer.Tests.Functional
             : base(beat: 0.4, bar: 1.6)
         { }
 
-        #region Tests
-
         [TestMethod]
         public void Sines_Samples_Metallophone_Jingle() => new AdditiveTests().Sines_Samples_Metallophone_Jingle_RunTest();
 
@@ -31,34 +31,19 @@ namespace JJ.Business.Synthesizer.Tests.Functional
         /// separate curves for each partial, triggers a wav header auto-detect.
         /// </summary>
         void Sines_Samples_Metallophone_Jingle_RunTest()
-        {
-            AssertEntities();
-
-            PlayMono(
+            => PlayMono(
                 () => Echo(MetallophoneJingle),
-                volume: 0.3,
-                duration: 1.2 + DEFAULT_NOTE_DURATION + ECHO_TIME);
-        }
+                duration: 1.2 + DEFAULT_NOTE_DURATION + ECHO_TIME,
+                volume: 0.3);
 
         [TestMethod]
         public void Sines_Samples_Metallophone_Note() => new AdditiveTests().Sines_Samples_Metallophone_Note_RunTest();
 
         void Sines_Samples_Metallophone_Note_RunTest()
-        {
-            AssertEntities();
-
-            PlayMono(
+            => PlayMono(
                 () => Echo(Metallophone(F4_Sharp)),
                 duration: DEFAULT_NOTE_DURATION + ECHO_TIME,
                 volume: 0.5);
-        }
-
-        /// <summary> Assert some entities that WriteToAudioFile won't. </summary>
-        void AssertEntities() => Samples.ValidateSample(GetSample()).Verify();
-
-        #endregion
-
-        #region Patches
 
         Outlet MetallophoneJingle => Add
         (
@@ -78,74 +63,68 @@ namespace JJ.Business.Synthesizer.Tests.Functional
 
             var sound = Add
             (
-                SinePartial(           frequency,        volume: _[1.0], Stretch(SinePartialCurve1,  duration)),
-                SinePartial(  Multiply(frequency, _[2]), volume: _[0.7], Stretch(SinePartialCurve2,  duration)),
-                SinePartial(  Multiply(frequency, _[5]), volume: _[0.4], Stretch(SinePartialCurve3,  duration)),
-                SamplePartial(Multiply(frequency, _[2]), volume: _[3.0], Stretch(SamplePartialCurve, duration)),
-                SamplePartial(Multiply(frequency, _[7]), volume: _[5.0], Stretch(SamplePartialCurve, duration))
+                SinePartial(           frequency,        volume: _[1.0], Stretch(Sine1Envelope,  duration)),
+                SinePartial(  Multiply(frequency, _[2]), volume: _[0.7], Stretch(Sine2Envelope,  duration)),
+                SinePartial(  Multiply(frequency, _[5]), volume: _[0.4], Stretch(Sine3Envelope,  duration)),
+                SamplePartial(Multiply(frequency, _[2]), volume: _[3.0], Stretch(SampleEnvelope, duration)),
+                SamplePartial(Multiply(frequency, _[7]), volume: _[5.0], Stretch(SampleEnvelope, duration))
             );
 
             return StrikeNote(sound, delay, volume);
         }
 
-        Outlet SinePartial(Outlet frequency, Outlet volume, Outlet curve)
-            =>  Multiply(Sine(frequency), Multiply(volume, curve));
+        Outlet SinePartial(Outlet frequency, Outlet volume, Outlet envelope)
+            =>  Multiply(Sine(frequency), Multiply(volume, envelope));
 
-        Outlet SamplePartial(Outlet frequency, Outlet volume, Outlet curve)
+        Outlet SamplePartial(Outlet frequency, Outlet volume, Outlet envelope)
             => Squash
             (
-                Multiply(Multiply(Sample(_sample), curve), volume),
+                Multiply(Multiply(Sample(), envelope), volume),
                 Divide(frequency, A4)
             );
 
         const double ECHO_TIME = 0.66 * 3;
 
         /// <inheritdoc cref="Wishes.Helpers.docs._default" />
-        Outlet Echo(Outlet sound)
-            => Echo(sound, count: 4, magnitude: _[1 / 3d], delay: _[0.66]);
+        Outlet Echo(Outlet sound) => Echo(sound, count: 4, magnitude: _[0.33], delay: _[0.66]);
 
-        #endregion
 
-        #region Samples
-
-        Sample _sample;
+        SampleOperatorWrapper _sample;
 
         /// <summary>
         /// Load a sample, skip some old header's bytes, maximize volume and tune to 440Hz.
         /// </summary>
-        Sample GetSample()
+        SampleOperatorWrapper Sample()
         {
             if (_sample != null) return _sample;
 
-            _sample = Samples.CreateSample(TestHelper.GetViolin16BitMono44100WavStream());
-
             // Skip over Header (from some other file format, that slipped into the audio data).
-            _sample.BytesToSkip = 62;
+            int bytesToSkip = 62;
 
             // Skip for Sharper Attack
-            _sample.BytesToSkip += 1000;
+            bytesToSkip += 1000;
 
             // Maximize and Normalize sample values (from 16-bit numbers to [-1, 1]).
-            _sample.Amplifier = 1.467 / short.MaxValue;
+            double amplifier = 1.467;
 
             // Tune to A 440Hz
-            double octaveFactor = Math.Pow(2, -1);
+            double octaveFactor   = 0.5;
             double intervalFactor = 4.0 / 5.0;
             double fineTuneFactor = 0.94;
-            _sample.TimeMultiplier = 1.0 / (octaveFactor * intervalFactor * fineTuneFactor);
+            double speedFactor    = octaveFactor * intervalFactor * fineTuneFactor;
+
+            _sample = Sample(GetViolin16BitMono44100WavStream(), amplifier, speedFactor, bytesToSkip);
 
             return _sample;
         }
 
-        #endregion
-
-        #region Curves
+        // Curves
 
         /// <summary>
         /// Creates a curve representing the volume modulation for the first sine partial.
         /// Starts quietly, peaks at a strong volume, and then fades gradually.
         /// </summary>
-        Outlet SinePartialCurve1 => CurveIn
+        Outlet Sine1Envelope => CurveIn
         (
             0.00, 0.80, 1.00, null, null, null, null, null,
             0.25, null, null, null, null, null, null, null,
@@ -156,7 +135,7 @@ namespace JJ.Business.Synthesizer.Tests.Functional
         /// Creates a curve for volume modulation of the second sine partial.
         /// Begins with a quick rise, reaches a high peak, and then slightly drops before fading.
         /// </summary>
-        Outlet SinePartialCurve2 => CurveIn
+        Outlet Sine2Envelope => CurveIn
         (
             0.00, 1.00, 0.80, null, null, null, null, null,
             0.10, null, null, null, null, null, null, null,
@@ -168,7 +147,7 @@ namespace JJ.Business.Synthesizer.Tests.Functional
         /// Starts at a moderate volume, dips to a very low level,
         /// and then has a slight resurgence before fading out.
         /// </summary>
-        Outlet SinePartialCurve3 => CurveIn
+        Outlet Sine3Envelope => CurveIn
         (
             0.30, 1.00, 0.30, null, null, null, null, null,
             0.10, null, null, null, null, null, null, null,
@@ -179,13 +158,11 @@ namespace JJ.Business.Synthesizer.Tests.Functional
         /// Generates a volume curve for the sample, starting at full volume
         /// and quickly diminishing to a lower level.
         /// </summary>
-        Outlet SamplePartialCurve => CurveIn
+        Outlet SampleEnvelope => CurveIn
         (
             1.00, 0.50, 0.20, null, null, null, null, 0.00,
             null, null, null, null, null, null, null, null,
             null, null, null, null, null, null, null, null
         );
-
-        #endregion
     }
 }
