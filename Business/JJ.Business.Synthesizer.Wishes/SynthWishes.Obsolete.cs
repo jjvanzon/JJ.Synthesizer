6b9b86c07c62e9b9d2710dbb2297903553987529
jjvanzon <jjvanzon79@gmail.com>
2024-10-25 03:39:53 +0200

    ⛓️‍💥 SynthWishes: Using nameof in Obsolete attributes for Operator methods.

--- Business/JJ.Business.Synthesizer.Wishes/SynthWishes.Obsolete.cs
+++ Business/JJ.Business.Synthesizer.Wishes/SynthWishes.Obsolete.cs
@@ -11,13 +11,13 @@ namespace JJ.Business.Synthesizer.Wishes
         [Obsolete("Use _[123] instead."), UsedImplicitly]
         public Outlet Value(double value = 0) => _[value];
 
-        [Obsolete("Use Add instead.", true), UsedImplicitly]
+        [Obsolete("Use " + nameof(Add) + " instead.", true), UsedImplicitly]
         public Adder Adder(params Outlet[] operands) => throw new NotSupportedException();
 
-        [Obsolete("Use Add instead.", true), UsedImplicitly]
+        [Obsolete("Use " + nameof(Add) + " instead.", true), UsedImplicitly]
         public Adder Adder(IList<Outlet> operands) => throw new NotSupportedException();
         
-        [Obsolete("Typo. Use Subtract instead.", true), UsedImplicitly]
+        [Obsolete("Typo. Use " + nameof(Subtract) + " instead.", true), UsedImplicitly]
         public Substract Substract(Outlet operandA = null, Outlet operandB = null) => throw new NotSupportedException();
 
         [Obsolete("Origin parameter obsolete.", true), UsedImplicitly]
@@ -35,31 +35,31 @@ namespace JJ.Business.Synthesizer.Wishes
         [Obsolete("Use Delay(Add(Multiply(Sine(pitch), volume), level), phaseStart) instead of Sine(volume, pitch, level, phaseStart).", true), UsedImplicitly]
         public Sine Sine(Outlet volume, Outlet pitch, Outlet level, Outlet phaseStart) => throw new NotSupportedException();
 
-        [Obsolete("Use Delay instead.", true), UsedImplicitly]
+        [Obsolete("Use " + nameof(Delay) + " instead.", true), UsedImplicitly]
         public TimeAdd TimeAdd(Outlet signal = null, Outlet timeDifference = null) => throw new NotSupportedException();
 
         [Obsolete("Origin parameter obsolete.", true), UsedImplicitly]
         public Outlet Stretch(Outlet signal, Outlet timeFactor, Outlet origin) => throw new NotSupportedException();
         
-        [Obsolete("Use Stretch instead.", true), UsedImplicitly]
+        [Obsolete("Use " + nameof(Stretch) + " instead.", true), UsedImplicitly]
         public TimeMultiply TimeMultiply(Outlet signal = null, Outlet timeMultiplier = null, Outlet origin = null) => throw new NotSupportedException();
 
         [Obsolete("Origin parameter obsolete.", true), UsedImplicitly]
-        public TimeDivide Squash(Outlet signal, Outlet timeDivider, Outlet origin) => throw new NotSupportedException();
+        public TimeDivide SpeedUp(Outlet signal, Outlet timeDivider, Outlet origin) => throw new NotSupportedException();
 
-        [Obsolete("Use Squash instead.", true), UsedImplicitly]
+        [Obsolete("Use " + nameof(SpeedUp) + " instead.", true), UsedImplicitly]
         public TimeDivide TimeDivide(Outlet signal = null, Outlet timeDivider = null, Outlet origin = null) => throw new NotSupportedException();
 
-        [Obsolete("Typo. Use TimeSubtract instead.", true), UsedImplicitly]
+        [Obsolete("Typo. Use " + nameof(Skip) + " instead.", true), UsedImplicitly]
         public TimeSubstract TimeSubstract(Outlet signal = null, Outlet timeDifference = null) => throw new NotSupportedException();
+        
+        [Obsolete("Use " + nameof(Skip) + " instead.", true), UsedImplicitly]
+        public TimeSubstract TimeSubtract(Outlet signal = null, Outlet timeDifference = null)            => throw new NotSupportedException();
 
         [Obsolete("Origin parameter obsolete.", true), UsedImplicitly]
         public TimePower TimePower(Outlet signal, Outlet exponent, Outlet origin) => throw new NotSupportedException();
 
         [Obsolete("Prefer other parameters.", true), UsedImplicitly]
         public SampleOperatorWrapper Sample(Sample sample) => throw new NotSupportedException();
-
-        [Obsolete("Use Skip instead.", true), UsedImplicitly]
-        public TimeSubstract TimeSubtract(Outlet signal = null, Outlet timeDifference = null)            => throw new NotSupportedException();
     }
 }
