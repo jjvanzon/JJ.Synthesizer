82c4c46ffea884ec8f6fc23da95dda346119a0fb
jjvanzon <jjvanzon79@gmail.com>
2024-10-30 11:40:10 +0100

    🎚️ Synth tests: Correction to multiplication by volume in "classic" test.

--- Business/JJ.Business.Synthesizer.Tests/SynthesizerTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/SynthesizerTests.cs
@@ -255,7 +255,7 @@ namespace JJ.Business.Synthesizer.Tests
                 Outlet sampleOperator = operatorFactory.Sample(sample);
                 Outlet effect = EntityFactory.CreateTimePowerEffectWithEcho(operatorFactory, sampleOperator);
 
-                effect = operatorFactory.Multiply(operatorFactory.Value(1 / 32768.0));
+                effect = operatorFactory.Multiply(effect, operatorFactory.Value(1 / 32768.0));
                 new SynthWishes(context).Mono().WithAudioLength(6.5).Play(() => effect);
                 return;
                 
