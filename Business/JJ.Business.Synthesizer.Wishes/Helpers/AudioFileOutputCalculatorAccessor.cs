9927e6518ee8a5639b97494190701c6621ef40e3
jjvanzon <jjvanzon79@gmail.com>
2024-11-01 02:30:29 +0100

    🦹 SynthWishes: Accessor for hacking in a stream into the AudioFileOutputCalculator. (Sets a private field of an internal class.)

---
+++ Business/JJ.Business.Synthesizer.Wishes/Helpers/AudioFileOutputCalculatorAccessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using JJ.Business.Synthesizer.Calculation.AudioFileOutputs;
+using JJ.Framework.Reflection;
+
+namespace JJ.Business.Synthesizer.Wishes.Helpers
+{
+    internal class AudioFileOutputCalculatorAccessor
+    {
+        private readonly Accessor _accessor;
+        
+        public AudioFileOutputCalculatorAccessor(IAudioFileOutputCalculator obj)
+        {
+            Assembly assembly = typeof(IAudioFileOutputCalculator).Assembly;
+            string typeName = "JJ.Business.Synthesizer.Calculation.AudioFileOutputs.AudioFileOutputCalculatorBase";
+            Type type = assembly.GetType(typeName, true);
+            _accessor = new Accessor(obj, type);
+        }
+
+        public Stream _stream
+        {
+            get => (Stream)_accessor.GetFieldValue(nameof(_stream));
+            set => _accessor.SetFieldValue(nameof(_stream), value);
+        }
+    }
+}
