f3ae3004bf4c96d916a8aead544d7b7e5f729087
jjvanzon <jjvanzon79@gmail.com>
2024-11-05 22:08:01 +0100

    🗄️ Archive unused code.

---
+++ Archive/Backups of Loose Source Files/2024-11-05 Backup of WavHeaderManagerAccessor.cs
@@ -0,0 +1,12 @@
+﻿using JJ.Business.Synthesizer.Managers;
+using JJ.Framework.Reflection;
+
+namespace JJ.Business.Synthesizer.Wishes.Helpers
+{
+    internal static class WavHeaderManagerAccessor
+    {
+        private static Accessor _accessor = new Accessor(typeof(WavHeaderManager));
+        
+           
+    }
+}
