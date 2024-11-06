5abc4b991a5d91f378b2e1f7c2262211a6b5fdf8
jjvanzon <jjvanzon79@gmail.com>
2024-11-06 17:36:13 +0100

    🗄️ Archive snippets

---
+++ Archive/Backups of Loose Source Files/2024-11-03 Backup of WrapperWishes Snippet.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using JJ.Business.Synthesizer.EntityWrappers;
+
+//// Moving wrapper-related extension methods here, marking them as obsolete,
+//// still available for compatibility where needed.
+
+//namespace JJ.Business.Synthesizer.Wishes
+//{
+//    // From AudioFileWishes
+
+//    public static partial class AudioFileExtensionWishes
+//    { 
+//        public static int SizeOfSampleDataType(this SampleOperatorWrapper wrapper)
+//        {
+//            if (wrapper == null) throw new ArgumentNullException(nameof(wrapper));
+//            return SizeOfSampleDataType(wrapper.Sample);
+//        }
+//    }
+
+//}
