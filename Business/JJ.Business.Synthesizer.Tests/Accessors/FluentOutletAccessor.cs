01a54f901fc4aa3e898db5089010e417e758ce7b
jjvanzon <jjvanzon79@gmail.com>
2024-11-09 19:15:22 +0100

    🔬 Synth tests: Add FluentOutletAccessor for access to FluentOutlet internals.

---
+++ Business/JJ.Business.Synthesizer.Tests/Accessors/FluentOutletAccessor.cs
@@ -0,0 +1,18 @@
+﻿using JJ.Business.Synthesizer.Wishes;
+using JJ.Framework.Reflection;
+using static JJ.Business.Synthesizer.Wishes.NameHelper;
+
+namespace JJ.Business.Synthesizer.Tests.Accessors
+{
+    internal class FluentOutletAccessor
+    {
+        private readonly Accessor _accessor;
+        
+        public FluentOutletAccessor(FluentOutlet obj)
+        {
+            _accessor = new Accessor(obj);
+        }
+
+        public string DebuggerDisplay => (string)_accessor.GetPropertyValue(MemberName());
+    }
+}
