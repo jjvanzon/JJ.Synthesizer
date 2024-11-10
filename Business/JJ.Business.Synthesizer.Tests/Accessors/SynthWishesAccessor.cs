94a09d8b2eb569072a665d171b3b19cde4aa627e
jjvanzon <jjvanzon79@gmail.com>
2024-11-10 21:52:18 +0100

    🧪 Synth tests: Extended SynthWishes accessor with echo methods for future use.

--- Business/JJ.Business.Synthesizer.Tests/Accessors/SynthWishesAccessor.cs
+++ Business/JJ.Business.Synthesizer.Tests/Accessors/SynthWishesAccessor.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using JJ.Business.Synthesizer.EntityWrappers;
 using JJ.Business.Synthesizer.Wishes;
 using JJ.Framework.Reflection;
+using static JJ.Business.Synthesizer.Wishes.NameHelper;
 
 namespace JJ.Business.Synthesizer.Tests.Accessors
 {
@@ -9,33 +12,51 @@ namespace JJ.Business.Synthesizer.Tests.Accessors
     {
         private readonly Accessor _accessor;
         
-        public SynthWishesAccessor(SynthWishes obj)
-        {
-            _accessor = new Accessor(obj, typeof(SynthWishes));
-        }
-        
+        public SynthWishesAccessor(SynthWishes obj) 
+            => _accessor = new Accessor(obj, typeof(SynthWishes));
+
         /// <inheritdoc cref="docs._captureindexer" />
         public SynthWishes.CaptureIndexer _ 
             => (SynthWishes.CaptureIndexer)_accessor.GetFieldValue(nameof(_));
 
-        public IList<FluentOutlet> FlattenTerms(FluentOutlet sumOrAdd)
-        {
-            return (IList<FluentOutlet>)_accessor.InvokeMethod(nameof(FlattenTerms), sumOrAdd);
-        }
+        public IList<FluentOutlet> FlattenTerms(FluentOutlet sumOrAdd) 
+            => (IList<FluentOutlet>)_accessor.InvokeMethod(MemberName(), sumOrAdd);
 
-        public IList<FluentOutlet> FlattenFactors(IList<FluentOutlet> operands)
-        { 
-            return (IList<FluentOutlet>)_accessor.InvokeMethod(nameof(FlattenFactors), operands);
-        }
+        public IList<FluentOutlet> FlattenFactors(IList<FluentOutlet> operands) 
+            => (IList<FluentOutlet>)_accessor.InvokeMethod(MemberName(), operands);
 
-        public IList<FluentOutlet> FlattenFactors(FluentOutlet multiply)
-        {
-            return (IList<FluentOutlet>)_accessor.InvokeMethod(nameof(FlattenFactors), multiply);
-        }
+        public IList<FluentOutlet> FlattenFactors(FluentOutlet multiply) 
+            => (IList<FluentOutlet>)_accessor.InvokeMethod(MemberName(), multiply);
 
-        public void RunParallelsRecursive(FluentOutlet op)
-        {
-            _accessor.InvokeMethod(nameof(RunParallelsRecursive), op);
-        }
+        public void RunParallelsRecursive(FluentOutlet op) 
+            => _accessor.InvokeMethod(MemberName(), op);
+
+        // EchoAdditive
+        
+        public FluentOutlet EchoAdditive(FluentOutlet sound, int count, FluentOutlet magnitude = null, FluentOutlet delay = null, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude ?? _[0.66], delay ?? _[0.25], callerMemberName);
+
+        public FluentOutlet EchoAdditive(FluentOutlet sound, int count, double magnitude, FluentOutlet delay = null, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude, delay ?? _[0.25], callerMemberName);
+
+        public FluentOutlet EchoAdditive(FluentOutlet sound, int count, FluentOutlet magnitude, double delay, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude ?? _[0.66], delay, callerMemberName);
+
+        public FluentOutlet EchoAdditive(FluentOutlet sound, int count, double magnitude, double delay, [CallerMemberName] string callerMemberName = null) 
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude, delay, callerMemberName);
+
+        // EchoFeedBack
+        
+        public FluentOutlet EchoFeedBack(FluentOutlet sound, int count, FluentOutlet magnitude = null, FluentOutlet delay = null, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude ?? _[0.66], delay ?? _[0.25], callerMemberName);
+
+        public FluentOutlet EchoFeedBack(FluentOutlet sound, int count, double magnitude, FluentOutlet delay = null, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude, delay ?? _[0.25], callerMemberName);
+
+        public FluentOutlet EchoFeedBack(FluentOutlet sound, int count, FluentOutlet magnitude, double delay, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude ?? _[0.66], delay, callerMemberName);
+        
+        public FluentOutlet EchoFeedBack(FluentOutlet sound, int count, double magnitude, double delay, [CallerMemberName] string callerMemberName = null)
+            => (FluentOutlet)_accessor.InvokeMethod(MemberName(), sound, count, magnitude, delay, callerMemberName);
     }
 }
