fe80f2b9575d3f3c6b80d5e2db283550ce5237d2
jjvanzon <jjvanzon79@gmail.com>
2024-11-21 06:05:33 +0100

    🔬 Synth tests: Distinctive tests for different Tape and Play situations.

--- Business/JJ.Business.Synthesizer.Tests/Technical/ParallelWishesTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/Technical/ParallelWishesTests.cs
@@ -7,20 +7,48 @@ namespace JJ.Business.Synthesizer.Tests.Technical
     public class ParallelWishesTests : SynthWishes
     {
         [TestMethod]
-        public void TapeMustPlayTest() => new ParallelWishesTests().TapeMustPlay();
+        public void SelectiveTape_InconsistentDelay_BecauseASineIsForever_Test() 
+            => new ParallelWishesTests().SelectiveTape_InconsistentDelay_BecauseASineIsForever();
         
-        private void TapeMustPlay()
+        private void SelectiveTape_InconsistentDelay_BecauseASineIsForever()
         {
-            var freq = G4;
+            Play(() => Sine(A4).Tape() + Sine(A5));
+        }
+        
+        [TestMethod]
+        public void PlayAllTapesTest() => new ParallelWishesTests().PlayAllTapes();
+        
+        private void PlayAllTapes()
+        {
+            WithAudioLength(0.25);
+            WithPlayAllTapes();
             
-            Play(() => 0.8 * Add
+            var pitch = G4;
+            
+            Play(() => ParallelAdd
                  (
-                     Sine(freq * 1).Volume(1.0).Play(),
-                     Sine(freq * 2).Volume(0.2),
-                     Sine(freq * 3).Volume(0.3).Play(),
-                     Sine(freq * 4).Volume(0.4),
-                     Sine(freq * 5).Volume(0.2).Play()
-                 ));
+                     Sine(pitch * 1).Volume(1.0),
+                     Sine(pitch * 2).Volume(0.2),
+                     Sine(pitch * 3).Volume(0.3)
+                 ).Curve(0.4, 0.4));
+        }
+        
+        
+        [TestMethod]
+        public void TapeThatPlaysTest() => new ParallelWishesTests().TapeThatPlays();
+        
+        private void TapeThatPlays()
+        {
+            var pitch = G4;
+            
+            Play(() => Add
+                 (
+                     Sine(pitch * 1).Volume(1.0).Play(),
+                     Sine(pitch * 2).Volume(0.2),
+                     Sine(pitch * 3).Volume(0.3).Play(),
+                     Sine(pitch * 4).Volume(0.4),
+                     Sine(pitch * 5).Play().Volume(0.2)
+                 ).Curve(0.4, 0.4));
         }
     }
 }
