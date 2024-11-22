3b72c5a549b48a54a7840cbf5cd642daa7b33385
jjvanzon <jjvanzon79@gmail.com>
2024-11-22 16:41:04 +0100

    📼 Detunica test: Pass note duration to the Tapes. Even though it added some code repetition for now, it did shave off 5 seconds from the 45 second test, which includes 17 seconds for audio playback.

--- Business/JJ.Business.Synthesizer.Tests/Functional/ModulationTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/Functional/ModulationTests.cs
@@ -10,9 +10,9 @@ namespace JJ.Business.Synthesizer.Tests.Functional
     [TestCategory("Functional")]
     public class ModulationTests : SynthWishes
     {
-        int          MildEchoCount  => 4;
+        int      MildEchoCount  => 4;
         FlowNode MildEchoDelay  => _[0.33];
-        int          DeepEchoCount  => 4;
+        int      DeepEchoCount  => 4;
         FlowNode DeepEchoDelayL => _[0.5];
         FlowNode DeepEchoDelayR => _[0.53];
 
@@ -131,11 +131,11 @@ namespace JJ.Business.Synthesizer.Tests.Functional
         /// <inheritdoc cref="docs._detunica" />
         FlowNode DetunicaJingle => Add
         (
-            _[ beat[1], E0, DetunicaBass, 1.00, l[5.25] ].Tape(),
-            _[ beat[2], B4, Detunica2   , 0.70, l[1.50] ].Tape(),
-            _[ beat[3], C5, Detunica3   , 0.75, l[1.60] ].Tape(),
-            _[ beat[4], D5, Detunica4   , 0.90, l[1.50] ].Tape(),
-            _[ beat[5], E5, Detunica5   , 1.00, l[3.00] ].Tape()
+            _[ beat[1], E0, DetunicaBass, 1.00, l[5.25] ].Tape(l[5.25]),
+            _[ beat[2], B4, Detunica2   , 0.70, l[1.50] ].Tape(l[1.50]),
+            _[ beat[3], C5, Detunica3   , 0.75, l[1.60] ].Tape(l[1.60]),
+            _[ beat[4], D5, Detunica4   , 0.90, l[1.50] ].Tape(l[1.50]),
+            _[ beat[5], E5, Detunica5   , 1.00, l[3.00] ].Tape(l[3.00])
         ).SetName();
 
         // Notes
@@ -147,11 +147,11 @@ namespace JJ.Business.Synthesizer.Tests.Functional
 
             return Add
             (
-                0.600 * Detunica1(freq * 1, duration, depth: _[0.6], chorusRate: _[0.040]).Tape(),
-                0.800 * Detunica2(freq * 2, duration).Tape(),
-                1.000 * Detunica3(freq * 4, duration).Tape(),
-                0.015 * Detunica4(freq * 8, duration).Tape(),
-                0.001 * Detunica5(freq * 16, duration).Tape()
+                0.600 * Detunica1(freq * 1, duration, depth: _[0.6], chorusRate: _[0.040]).Tape(duration),
+                0.800 * Detunica2(freq * 2, duration).Tape(duration),
+                1.000 * Detunica3(freq * 4, duration).Tape(duration),
+                0.015 * Detunica4(freq * 8, duration).Tape(duration),
+                0.001 * Detunica5(freq * 16, duration).Tape(duration)
             ).SetName().Panbrello(2, 0.2);
         }
 
