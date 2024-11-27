9588b4607b35aff0ebd97313414958ec465698a1
jjvanzon <jjvanzon79@gmail.com>
2024-11-28 00:50:08 +0100

    ⏲️ NoteWishes: Update note arrangement doc comments to show freq-less variations playing the same note at the same time, avoiding overlap with other notes in the tests.

--- Business/JJ.Business.Synthesizer.Wishes/NoteWishes.cs
+++ Business/JJ.Business.Synthesizer.Wishes/NoteWishes.cs
@@ -79,7 +79,7 @@ namespace JJ.Business.Synthesizer.Wishes
         /// <summary><c>
         /// FlowNode Flute1() <br/><br/>
         /// _[ 0.00, Flute1, 0.8 ], <br/>
-        /// _[ 0.25, Flute1, 0.8, l[0.5] ]
+        /// _[ 0.00, Flute1, 0.8, l[0.5] ]
         /// </c></summary>
         public FlowNode this[
             double t, Func<FlowNode> sound,
@@ -89,8 +89,8 @@ namespace JJ.Business.Synthesizer.Wishes
         /// <summary><c>
         /// FlowNode Flute1() <br/><br/>
         /// _[ 0.00, Flute1 ], <br/>
-        /// _[ 0.25, Flute1, MyCurve ], <br/>
-        /// _[ 0.50, Flute1, MyCurve, l[0.5] ]
+        /// _[ 0.00, Flute1, MyCurve ], <br/>
+        /// _[ 0.00, Flute1, MyCurve, l[0.5] ]
         /// </c></summary>
         public FlowNode this[
             double t, Func<FlowNode> sound,
@@ -100,7 +100,7 @@ namespace JJ.Business.Synthesizer.Wishes
         /// <summary><c>
         /// FlowNode Flute1() <br/><br/>
         /// _[ t[1, 1], Flute1, 0.8 ], <br/>
-        /// _[ t[1, 2], Flute1, 0.8, l[0.5] ]
+        /// _[ t[1, 1], Flute1, 0.8, l[0.5] ]
         /// </c></summary>
         public FlowNode this[
             FlowNode t, Func<FlowNode> sound,
@@ -110,8 +110,8 @@ namespace JJ.Business.Synthesizer.Wishes
         /// <summary><c>
         /// FlowNode Flute1() <br/><br/>
         /// _[ t[1, 1], Flute1 ], <br/>
-        /// _[ t[1, 2], Flute1, MyCurve ], <br/>
-        /// _[ t[1, 3], Flute1, MyCurve, l[0.5] ]
+        /// _[ t[1, 1], Flute1, MyCurve ], <br/>
+        /// _[ t[1, 1], Flute1, MyCurve, l[0.5] ]
         /// </c></summary>
         public FlowNode this[
             FlowNode t, Func<FlowNode> sound,
