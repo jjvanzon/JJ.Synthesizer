e8bad1fd65693bc8e59ab250c1b81057edabd61b
jjvanzon <jjvanzon79@gmail.com>
2024-11-30 23:46:13 +0100

    🧪 Synth tests: Add (random) NameWishes tests to address doubts about functionality.

---
+++ Business/JJ.Business.Synthesizer.Tests/Technical/NameWishesTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using JJ.Business.Synthesizer.Wishes;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Business.Synthesizer.Tests.Technical
+{
+    [TestClass]
+    public class NameWishesTests
+    {
+        [TestMethod]
+        public void TestFileNameWithoutExtension()
+        {
+            string input = @"C:\Repositories\JJ.Synthesizer\Business\JJ.Business.Synthesizer.Tests\bin\Release\Additive_Metallophone_Jingle.wav";
+
+            string expected = "Additive_Metallophone_Jingle";
+            
+            AssertHelper.AreEqual(expected, () => Path.GetFileNameWithoutExtension(input));
+        }
+        
+        [TestMethod]
+        public void TestPrettifyFileNameExtensionRemoval()
+        {
+            string input = @"C:\Repositories\JJ.Synthesizer\Business\JJ.Business.Synthesizer.Tests\bin\Release\Additive_Metallophone_Jingle.wav";
+
+            string expected = @"Additive Metallophone Jingle";
+            
+            AssertHelper.AreEqual(expected, () => NameHelper.PrettifyName(input));
+        }
+    }
+}
