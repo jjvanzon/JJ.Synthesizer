a19d9dca94b1f79750456a3f9dba65d489d774b6
jjvanzon <jjvanzon79@gmail.com>
2024-12-02 21:50:43 +0100

    📻 SynthWishes: Add StereoTapeRecombiner to stream two channel Tapes together into one stereo Buff.

---
+++ Business/JJ.Business.Synthesizer.Wishes/TapeWishes/StereoTapeRecombiner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JJ.Framework.Reflection;
+
+namespace JJ.Business.Synthesizer.Wishes.TapeWishes
+{
+    internal class StereoTapeRecombiner
+    {
+        private readonly SynthWishes _synthWishes;
+
+        public StereoTapeRecombiner(SynthWishes synthWishes)
+        {
+            _synthWishes = synthWishes ?? throw new ArgumentNullException(nameof(synthWishes));
+        }
+        
+        public Tape RecombineChannels((Tape Left, Tape Right) tapePair)
+        {
+            if (tapePair.Left == null) throw new NullException(() => tapePair.Left);
+            if (tapePair.Left.Buff == null) throw new NullException(() => tapePair.Left.Buff);
+            if (tapePair.Right == null) throw new NullException(() => tapePair.Right);
+            if (tapePair.Right == null) throw new NullException(() => tapePair.Right);
+            if (tapePair.Right.Buff == null) throw new NullException(() => tapePair.Right.Buff);
+            
+            Buff stereoBuff = _synthWishes.Cache(() => _synthWishes.Sample(tapePair.Left.Buff ).Panning(0) +
+                                                       _synthWishes.Sample(tapePair.Right.Buff).Panning(1));
+            var stereoTape = new Tape
+            {
+                Buff = stereoBuff,
+                Duration = tapePair.Left.Duration,
+                WithPlay = tapePair.Left.WithPlay,
+                WithSave = tapePair.Left.WithSave,
+                WithCache = tapePair.Left.WithCache,
+                FilePath = tapePair.Left.FilePath,
+                Callback = tapePair.Left.Callback,
+                FallBackName = tapePair.Left.FallBackName
+            };
+            
+            return stereoTape;
+        }
+    }
+}
