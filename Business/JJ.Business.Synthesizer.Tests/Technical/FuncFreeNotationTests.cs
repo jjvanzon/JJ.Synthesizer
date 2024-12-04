c1fa923585765c53e4e478c17bddb0724ee032f4
jjvanzon <jjvanzon79@gmail.com>
2024-12-04 18:22:20 +0100

    🧫 Synth tests: FuncFreeNotationTests - Reformat and refactor for overview. Add comment that rates each notation experiment.

--- Business/JJ.Business.Synthesizer.Tests/Technical/FuncFreeNotationTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/Technical/FuncFreeNotationTests.cs
@@ -1,140 +1,98 @@
 ﻿using JJ.Business.Synthesizer.Wishes;
-using JJ.Framework.Persistence;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using JJ.Business.Synthesizer.Tests.Helpers;
+// ReSharper disable PublicConstructorInAbstractClass
+// ReSharper disable ArrangeStaticMemberQualifier
 // ReSharper disable ObjectCreationAsStatement
 // ReSharper disable CheckNamespace
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation1
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.Run
 {
-    public class Synth : SynthWishes
-    { }
-
+    /// <summary> Not func free </summary>
     [TestClass]
+    [TestCategory("Technical")]
+    public class MySound : MySynthWishes
+    {
+        [TestMethod]
+        public void FuncFree_Run() => new MySound().Start();
+        
+        void Start() => Run(() => Sine(E4).Curve(RecorderCurve).Play());
+    }
+}
+
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.MethodGroup
+{
+    /// <summary> Better </summary>
+    [TestClass]
+    [TestCategory("Technical")]
+    public class MySound : MySynthWishes
+    {
+        [TestMethod]
+        public void FuncFree_MethodGroup() => new MySound().Run(Start);
+        
+        FlowNode Start() => Sine(E4).Curve(RecorderCurve).Play();
+    }
+}
+
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.ConstructorFunc
+{
+    /// <summary> Not better </summary>
+    [TestClass]
+    [TestCategory("Technical")]
     public class MySound : Synth
     {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
+        public MySound() { }
+        public MySound(Func<FlowNode> func) : base(func) { }
         
         [TestMethod]
-        public void FuncFreeTest1() => new MySound().FuncFree();
-        void FuncFree() => Run(() => Sine(E4).Curve(RecorderCurve).Play());
+        public void FuncFree_ConstructorFunc() => new MySound(Start);
+        
+        FlowNode Start() => Sine(E4).Curve(RecorderCurve).Play();
+    }
+
+    public class Synth : MySynthWishes
+    {
+        public Synth() { }
+        public Synth(Func<FlowNode> func) => Run(func, GetType().Name);
     }
 }
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation2
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.ConstructorFunc2
 {
-    public class Synth : SynthWishes
-    {
-    }
-
+    /// <summary> Same </summary>
     [TestClass]
+    [TestCategory("Technical")]
     public class MySound : Synth
     {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
-
+        public MySound() { }
+        public MySound(Func<FlowNode> func) : base(func) { }
+        
         [TestMethod]
-        public void FuncFreeNotationTest2() => new MySound().Run(FuncFreeNotation2);
-        FlowNode FuncFreeNotation2() => Sine(E4).Curve(RecorderCurve).Play();
+        public void FuncFree_ConstructorFunc2() => new Synth(Start);
+        
+        FlowNode Start() => Sine(E4).Curve(RecorderCurve).Play();
+    }
+
+    public class Synth : MySynthWishes
+    {
+        public Synth() { }
+        public Synth(Func<FlowNode> func) => Run(func, GetType().Name);
     }
 }
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation3
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.LocalFunc_ConstructorFunc
 {
-    public class Synth : SynthWishes
-    {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
-        
-        protected virtual FlowNode Start() => null;
-    }
-
+    /// <summary> Confusing </summary>
     [TestClass]
+    [TestCategory("Technical")]
     public class MySound : Synth
     {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
-        
-        public MySound() 
-        { }
-
-        public MySound(Func<FlowNode> func) 
-            : base(func)
-        { }
-
+        public MySound() { }
+        public MySound(Func<FlowNode> func) : base(func) { }
         
         [TestMethod]
-        public void FuncFreeNotationTest3() => new MySound(FuncFreeNotation3);
-        FlowNode FuncFreeNotation3() => Sine(E4).Curve(RecorderCurve).Play();
-    }
-}
-
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation4
-{
-    public class Synth : SynthWishes
-    {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
-        
-        protected virtual FlowNode Start() => null;
-    }
-
-    [TestClass]
-    public class FuncFreeNotationTests4 : Synth
-    {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
-        
-        public FuncFreeNotationTests4() 
-        { }
-
-        public FuncFreeNotationTests4(Func<FlowNode> func) 
-            : base(func)
-        { }
-
-        [TestMethod]
-        public void FuncFreeNotationTest4() => new Synth(FuncFreeNotation4);
-        FlowNode FuncFreeNotation4() => Sine(E4).Curve(RecorderCurve).Play();
-    }
-}
-
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation5
-{
-    public class Synth : SynthWishes
-    {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
-        
-        protected virtual FlowNode Start() => null;
-    }
-
-    [TestClass]
-    public class FuncFreeNotationTests5 : Synth
-    {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
-        
-        public FuncFreeNotationTests5() 
-        { }
-
-        public FuncFreeNotationTests5(Func<FlowNode> func) 
-            : base(func)
-        { }
-
-        
-        [TestMethod]
-        public void FuncFreeNotationTest5()
+        public void FuncFree_LocalFunc_ConstructorFunc()
         {
             FlowNode MySynth()
             {
@@ -144,121 +102,88 @@ namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation5
             new Synth(MySynth);
         }
     }
+
+    public class Synth : MySynthWishes
+    {
+        public Synth() { }
+        public Synth(Func<FlowNode> func) => Run(func, GetType().Name);
+    }
 }
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation6
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.VirtualStart
 {
-    public class Synth : SynthWishes
-    {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
-        
-        protected virtual FlowNode Start() => null;
-    }
-
+    /// <summary> It's magic </summary>
     [TestClass]
-    public class FuncFreeNotationTests6 : Synth
+    [TestCategory("Technical")]
+    public class MySound : Synth
     {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
-        
-        public FuncFreeNotationTests6() 
-        { }
-
-        public FuncFreeNotationTests6(Func<FlowNode> func) 
-            : base(func)
-        { }
-
-        
         [TestMethod]
-        public void FuncFreeNotationTest6() { }
+        public void FuncFree_VirtualStart() { }
         
         protected override FlowNode Start() => Sine(E4).Curve(RecorderCurve).Play();
     }
-}
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation7
-{
-    public class Synth : SynthWishes
+    public class Synth : MySynthWishes
     {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
+        public Synth() => Run(Start, GetType().Name);
         
         protected virtual FlowNode Start() => null;
     }
-
-    [TestClass]
-    public class FuncFreeNotationTests7 : Synth
-    {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
-        
-        public FuncFreeNotationTests7() 
-        { }
-
-        public FuncFreeNotationTests7(Func<FlowNode> func) 
-            : base(func)
-        { }
-
-                
-        //[TestMethod]
-        //public void FuncFreeNotationTest7() => SynthWishes.Run(Start7);
-        
-        //FlowNode Start7() => Sine(E4).Curve(RecorderCurve).Play();
-    }
 }
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation8
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.AbstractStart
 {
-    public abstract class Synth : SynthWishes
-    {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
-        
-        protected abstract FlowNode Start();
-    }
-
+    /// <summary> It's forceful magic </summary>
     [TestClass]
-    public class FuncFreeNotationTests8 : Synth
+    [TestCategory("Technical")]
+    public class MySound : Synth
     {
-        FlowNode RecorderCurve => Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
+        [TestMethod]
+        public void FuncFree_AbstractStart() { }
         
         protected override FlowNode Start() => Sine(E4).Curve(RecorderCurve).Play();
     }
-}
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation9
-{
-    public abstract class Synth : SynthWishes
+    public abstract class Synth : MySynthWishes
     {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
+        public Synth() => Run(Start, GetType().Name);
         
         protected abstract FlowNode Start();
     }
+}
 
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.StaticRun
+{
+    /// <summary>
+    /// More terse.
+    /// Context contention?
+    /// </summary>
     [TestClass]
-    public class FuncFreeNotationTests9 : Synth
+    [TestCategory("Technical")]
+    public class MySound : Synth
     {
-        protected override FlowNode Start()
+        [TestMethod]
+        public void FuncFree_StaticRun() => Synth.Run(Start);
+        
+        FlowNode Start() => Sine(E4).Curve(RecorderCurve).Play();
+    }
+
+    public class Synth : MySynthWishes
+    {
+        public static void Run(Func<FlowNode> func) => new Synth().Run(func, func.Method.Name);
+    }
+}
+
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.MoreLocals
+{
+    [TestClass]
+    [TestCategory("Technical")]
+    public class MySound : Synth
+    {
+        [TestMethod]
+        public void FuncFree_MoreLocals() => Synth.Run(Start);
+
+        FlowNode Start()
         {
             var recorderCurve = Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
             
@@ -267,29 +192,24 @@ namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation9
             return flow;
         }
     }
+
+    public class Synth : MySynthWishes
+    {
+        public static void Run(Func<FlowNode> func) => new Synth().Run(func, func.Method.Name);
+
+    }
 }
 
-namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation10
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.LocalProblems
 {
-    public abstract class Synth : SynthWishes
-    {
-        public Synth(IContext context = null)
-            : base(context)
-            => Run(Start, GetType().Name);
-
-        public Synth(Func<FlowNode> func, IContext context = null)
-            : base(context)
-            => Run(func, GetType().Name);
-        
-        protected abstract FlowNode Start();
-    }
-
     [TestClass]
-    public class FuncFreeNotationTests10 : Synth
+    [TestCategory("Technical")]
+    public class MySound : Synth
     {
-        // TODO: Where'd my TestMethod go?
-        
-        protected override FlowNode Start()
+        [TestMethod]
+        public void FuncFree_LocalProblems() => Synth.Run(Start);
+
+        FlowNode Start()
         {
             var recorderCurve = Curve((0, 0), (0.05, 1), (0.95, 1), (1.00, 0));
             
@@ -303,4 +223,34 @@ namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation10
             
             return flow;
         }
-    }}
+    }
+
+    public class Synth : MySynthWishes
+    {
+        public static void Run(Func<FlowNode> func) => new Synth().Run(func, func.Method.Name);
+    }
+}
+
+namespace JJ.Business.Synthesizer.Tests.Technical.FuncFreeNotation.StaticRun2
+{
+    /// <summary>
+    /// Cool.
+    /// Takes no main flow node.
+    /// Will it work?
+    /// Context contention?
+    /// </summary>
+    [TestClass]
+    [TestCategory("Technical")]
+    public class MySound : Synth
+    {
+        [TestMethod]
+        public void FuncFree_StaticRun2() => Synth.Run(Start);
+        
+        void Start() => Sine(E4).Curve(RecorderCurve).Play();
+    }
+
+    public class Synth : MySynthWishes
+    {
+        public static void Run(Action action) => new Synth().Run(() => { action(); return null; }, action.Method.Name);
+    }
+}
