21d6e4f1772880b70a3bb3b1e3e7df0f071204b2
jjvanzon <jjvanzon79@gmail.com>
2024-12-06 22:26:45 +0100

    🗄️ Archive

---
+++ Archive/Backups of Loose Source Files/2024-12-06 Backup of TapeScribbler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JJ.Business.Synthesizer.Wishes.TapeWishes
+{
+    /// <inheritdoc cref="docs._tapecopyscribbler" />
+    internal static class TapeScribbler
+    {
+        ///// <inheritdoc cref="docs._tapecopyscribbler" />
+        //public static Tape CopyMetaData(Tape original)
+        //{
+        //    var copy = new Tape();
+        //    CopyMetaData(original, copy);
+        //    return copy;
+        //}
+
+        /// <inheritdoc cref="docs._tapecopyscribbler" />
+        public static void CopyMetaData(Tape original, Tape copy)
+        {
+            if (original == null) throw new ArgumentNullException(nameof(original));
+            if (copy == null) throw new ArgumentNullException(nameof(copy));
+            copy.Duration = original.Duration;
+            copy.FallBackName = original.FallBackName;
+            copy.IsPlay = original.IsPlay;
+            copy.IsSave = original.IsSave;
+            copy.IsCache = original.IsCache;
+            copy.IsPadding = original.IsPadding;
+            copy.FilePath = original.FilePath;
+        }
+    }
+}
