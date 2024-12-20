deadfd9f54e10adc78207cb4d71ca8ab6fff0257
jjvanzon <jjvanzon79@gmail.com>
2024-12-20 23:12:04 +0100

    ⚠️ SynthWishes: Add missing using directives and FilledIn helper extensions for new DebuggerDisplays. (Made the build fail earlier.)

--- Business/JJ.Business.Synthesizer.Wishes/Helpers/DebuggerDisplayFormatter.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Helpers/DebuggerDisplayFormatter.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using JJ.Business.Synthesizer.Wishes.TapeWishes;
+using static System.IO.File;
+using static System.String;
+using static JJ.Business.Synthesizer.Wishes.Helpers.JJ_Framework_Common_Wishes.FilledInWishes;
+using static JJ.Business.Synthesizer.Wishes.Helpers.JJ_Framework_Text_Wishes.StringExtensionWishes;
 using static JJ.Business.Synthesizer.Wishes.LogWishes;
 
 namespace JJ.Business.Synthesizer.Wishes.Helpers
---
+++ Business/JJ.Business.Synthesizer.Wishes/Helpers/FilledInHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JJ.Business.Synthesizer.Wishes.Helpers
+{
+    /// <summary>
+    /// Extensions to the FrameworkWishes' FilledIn methods,
+    /// specific to Synth objects.
+    /// </summary>
+    internal static class FilledInHelper
+    {
+        public static bool Has(FlowNode flowNode) => FilledIn(flowNode);
+        public static bool FilledIn(FlowNode flowNode)
+        {
+            if (flowNode == null) return false;
+            if (flowNode.IsConst && flowNode.Value == 0) return false;
+            return true;
+        }
+    }
+}
