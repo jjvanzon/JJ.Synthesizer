dbe1b28efa7668c9de08d411222eeb5797ed3610
jjvanzon <jjvanzon79@gmail.com>
2024-12-22 14:41:40 +0100

    🧽 LogWishes: Polish up code of obsolete legacy SynthLog (untested).

--- Business/JJ.Business.Synthesizer.Wishes/Obsolete/Obsolete.LogWishes.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Obsolete/Obsolete.LogWishes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using JJ.Persistence.Synthesizer;
 using static System.IO.Path;
 using static JJ.Business.Synthesizer.Wishes.Helpers.JJ_Framework_Text_Wishes.StringWishes;
 using static JJ.Business.Synthesizer.Wishes.LogWishes;
@@ -14,15 +15,6 @@ namespace JJ.Business.Synthesizer.Wishes.Obsolete
     {
         public static IList<string> GetSynthLogOld(Buff buff, double calculationDuration)
         {
-            // Get Info
-            var stringifiedChannels = new List<string>();
-            
-            foreach (var audioFileOutputChannel in buff.UnderlyingAudioFileOutput.AudioFileOutputChannels)
-            {
-                string stringify = audioFileOutputChannel.Outlet?.Stringify() ?? "";
-                stringifiedChannels.Add(stringify);
-            }
-            
             // Gather Lines
             var lines = new List<string>();
             
@@ -38,31 +30,25 @@ namespace JJ.Business.Synthesizer.Wishes.Obsolete
             lines.Add("Audio Length: " + ConfigLog(buff));
             lines.Add("");
             
-            //IList<string> warnings = buff.Messages.ToArray();
-            //if (warnings.Any())
-            //{
-            //    lines.Add("Warnings:");
-            //    lines.AddRange(warnings.Select(warning => $"- {warning}"));
-            //    lines.Add("");
-            //}
-            
-            for (var i = 0; i < buff.UnderlyingAudioFileOutput.AudioFileOutputChannels.Count; i++)
+            int channels = buff.UnderlyingAudioFileOutput.AudioFileOutputChannels.Count;
+            foreach (var audioFileOutputChannel in buff.UnderlyingAudioFileOutput.AudioFileOutputChannels)
             {
-                var channelString = stringifiedChannels[i];
+                string channelString = GetChannelDescriptor(channels, audioFileOutputChannel.Index);
+                string calculationString = audioFileOutputChannel.Outlet?.Stringify() ?? "";
                 
-                lines.Add($"Calculation Channel {i + 1}:");
+                lines.Add($"Calculation {channelString}:");
                 lines.Add("");
-                lines.Add(channelString);
+                lines.Add(calculationString);
                 lines.Add("");
             }
             
             if (buff.Bytes != null)
             {
-                lines.Add($"{PrettyByteCount(buff.Bytes.Length)} written to memory.");
+                lines.Add($"  {PrettyByteCount(buff.Bytes.Length)} written to memory.");
             }
             if (File.Exists(buff.FilePath))
             {
-                lines.Add($"Output file: {GetFullPath(buff.FilePath)}");
+                lines.Add(FormatOutputFile(GetFullPath(buff.FilePath)));
             }
             
             lines.Add("");
