b11354a14665b9f2dc5c09de3bb6943b787549eb
jjvanzon <jjvanzon79@gmail.com>
2024-12-23 15:08:23 +0100

    🍬 SynthWishes: Add `PropertyNameWishes` for syntactic sugar, with constant strings like Play = "Play".

---
+++ Business/JJ.Business.Synthesizer.Wishes/Helpers/PropertyNameWishes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JJ.Business.Synthesizer.Wishes.Helpers
+{
+    internal static class PropertyNameWishes
+    {
+        public static string Play = nameof(Play);
+        public static string Save = nameof(Save);
+        public static string Intercept = nameof(Intercept);
+    }
+}
