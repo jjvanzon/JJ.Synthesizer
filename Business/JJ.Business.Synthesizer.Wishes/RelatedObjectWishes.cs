a5e87ca5769542681126e07bbc27c0a8d55c3d64
jjvanzon <jjvanzon79@gmail.com>
2024-12-30 01:44:59 +0100

    ➕ RelatedObjectWishes: Added extensions for setting `UnderlyingSample` and `UnderlyingCurve`, complementing the existing getter methods to provide more flexibility.

--- Business/JJ.Business.Synthesizer.Wishes/RelatedObjectWishes.cs
+++ Business/JJ.Business.Synthesizer.Wishes/RelatedObjectWishes.cs
@@ -13,41 +13,80 @@ namespace JJ.Business.Synthesizer.Wishes
         /// <inheritdoc cref="docs._underlyingcurve" />"/>
         public Curve UnderlyingCurve() => _underlyingOutlet.UnderlyingCurve();
 
+        /// <inheritdoc cref="docs._underlyingcurve" />"/>
+        public FlowNode UnderlyingCurve(Curve curve) { _underlyingOutlet.UnderlyingCurve(curve); return this; }
+
         /// <inheritdoc cref="docs._underlyingsample" />
         public Sample UnderlyingSample() => _underlyingOutlet.UnderlyingSample();
+        
+        /// <inheritdoc cref="docs._underlyingsample" />
+        public FlowNode UnderlyingSample(Sample sample) { _underlyingOutlet.UnderlyingSample(sample); return this; }
     }
 
     /// <inheritdoc cref="docs._relatedobjectextensions"/>
     public static class RelatedObjectExtensionWishes
     {
         /// <inheritdoc cref="docs._underlyingcurve" />"/>
-        public static Curve UnderlyingCurve(this Outlet entity)
+        public static Curve UnderlyingCurve(this Outlet outlet)
         {
-            if (entity == null) throw new ArgumentNullException(nameof(entity));
-            return UnderlyingCurve(entity.Operator);
+            if (outlet == null) throw new ArgumentNullException(nameof(outlet));
+            return UnderlyingCurve(outlet.Operator);
+        }
+        
+        /// <inheritdoc cref="docs._underlyingcurve" />"/>
+        public static Outlet UnderlyingCurve(this Outlet outlet, Curve curve)
+        {
+            if (outlet == null) throw new ArgumentNullException(nameof(outlet));
+            UnderlyingCurve(outlet.Operator, curve);
+            return outlet;
         }
 
         /// <inheritdoc cref="docs._underlyingsample" />
-        public static Sample UnderlyingSample(this Outlet entity)
+        public static Sample UnderlyingSample(this Outlet outlet)
         {
-            if (entity == null) throw new ArgumentNullException(nameof(entity));
-            return UnderlyingSample(entity.Operator);
+            if (outlet == null) throw new ArgumentNullException(nameof(outlet));
+            return UnderlyingSample(outlet.Operator);
+        }
+        
+        public static Outlet UnderlyingSample(this Outlet outlet, Sample sample)
+        {
+            if (outlet == null) throw new ArgumentNullException(nameof(outlet));
+            UnderlyingSample(outlet.Operator, sample);
+            return outlet;
         }
 
         /// <inheritdoc cref="docs._underlyingcurve" />"/>
-        public static Curve UnderlyingCurve(this Operator entity)
+        public static Curve UnderlyingCurve(this Operator op)
         {
-            if (entity == null) throw new ArgumentNullException(nameof(entity));
-            if (entity.AsCurveIn == null) throw new NullException(() => entity.AsCurveIn);
-            return entity.AsCurveIn.Curve;
+            if (op == null) throw new ArgumentNullException(nameof(op));
+            if (op.AsCurveIn == null) throw new NullException(() => op.AsCurveIn);
+            return op.AsCurveIn.Curve;
+        }
+        
+        /// <inheritdoc cref="docs._underlyingcurve" />
+        public static Operator UnderlyingCurve(this Operator op, Curve curve)
+        {
+            if (op == null) throw new ArgumentNullException(nameof(op));
+            if (op.AsCurveIn == null) throw new NullException(() => op.AsCurveIn);
+            op.AsCurveIn.Curve = curve;
+            return op;
+        }
+        
+        /// <inheritdoc cref="docs._underlyingsample" />
+        public static Sample UnderlyingSample(this Operator op)
+        {
+            if (op == null) throw new ArgumentNullException(nameof(op));
+            if (op.AsSampleOperator == null) throw new NullException(() => op.AsSampleOperator);
+            return op.AsSampleOperator.Sample;
         }
 
         /// <inheritdoc cref="docs._underlyingsample" />
-        public static Sample UnderlyingSample(this Operator entity)
+        public static Operator UnderlyingSample(this Operator op, Sample sample)
         {
-            if (entity == null) throw new ArgumentNullException(nameof(entity));
-            if (entity.AsSampleOperator == null) throw new NullException(() => entity.AsSampleOperator);
-            return entity.AsSampleOperator.Sample;
+            if (op == null) throw new ArgumentNullException(nameof(op));
+            if (op.AsSampleOperator == null) throw new NullException(() => op.AsSampleOperator);
+            op.AsSampleOperator.Sample = sample;
+            return op;
         }
     }
 }
