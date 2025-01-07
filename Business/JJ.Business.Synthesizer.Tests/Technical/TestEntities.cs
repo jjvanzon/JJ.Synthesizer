5e5aa61d25d8f58d7a833c42614afcb8df45c0c6
jjvanzon <jjvanzon79@gmail.com>
2025-01-07 21:15:50 +0100

    🧫 Synth tests: ChannelWishesTests - Added more entities to `ChannelEntities`, including `Buff` and `WavHeader`, as they are also produced per `Channel` alongside the `Stereo Tape`.

--- Business/JJ.Business.Synthesizer.Tests/Technical/TestEntities.cs
+++ Business/JJ.Business.Synthesizer.Tests/Technical/TestEntities.cs
@@ -19,12 +19,30 @@ namespace JJ.Business.Synthesizer.Tests.Technical
     internal class TapeEntities
     {
         // Tape-Bound
-        public Tape               Tape                { get; set; }
-        public TapeConfig         TapeConfig          { get; set; }
-        public TapeActions        TapeActions         { get; set; }
-        public TapeAction         TapeAction          { get; set; }
-    }
-    
+        public Tape              Tape                { get; set; }
+        public TapeConfig        TapeConfig          { get; set; }
+        public TapeActions       TapeActions         { get; set; }
+        public TapeAction        TapeAction          { get; set; }
+                                 
+        // Buff-Bound            
+        public Buff               Buff                { get; set; }
+        public AudioFileOutput    AudioFileOutput     { get; set; }
+                                                      
+        // Independent after Taping
+        public Sample             Sample              { get; set; }
+        public AudioInfoWish      AudioInfoWish       { get; set; }
+        public AudioFileInfo      AudioFileInfo       { get; set; }
+                                                     
+        // Immutable                                 
+        public WavHeaderStruct    WavHeader           { get; set; }
+        public SampleDataTypeEnum SampleDataTypeEnum  { get; set; }
+        public SampleDataType     SampleDataType      { get; set; }
+        public Type               Type                { get; set; }
+        public SpeakerSetupEnum   SpeakerSetupEnum    { get; set; }
+        public SpeakerSetup       SpeakerSetup        { get; set; }
+        public ChannelEnum        ChannelEnum         { get; set; }
+        public Channel            ChannelEntity       { get; set; }
+    }     
     internal class TestEntities : TapeEntities
     {   
         // Global-Bound (read-only)
@@ -40,24 +58,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
         // Tape-Bound
         public IList<TapeEntities> ChannelEntities { get; private set; }
         
-        // Buff-Bound
-        public Buff                Buff                { get; private set; }
-        public AudioFileOutput     AudioFileOutput     { get; private set; }
-                                                      
-        // Independent after Taping                   
-        public Sample              Sample              { get; private set; }
-        public AudioInfoWish       AudioInfoWish       { get; private set; }
-        public AudioFileInfo       AudioFileInfo       { get; private set; }
-                                                      
-        // Immutable                                  
-        public WavHeaderStruct     WavHeader           { get; private set; }
-        public SampleDataTypeEnum  SampleDataTypeEnum  { get; private set; }
-        public SampleDataType      SampleDataType      { get; private set; }
-        public Type                Type                { get; private set; }
-        public SpeakerSetupEnum    SpeakerSetupEnum    { get; private set; }
-        public SpeakerSetup        SpeakerSetup        { get; private set; }
-        public ChannelEnum         ChannelEnum         { get; private set; }
-        public Channel             ChannelEntity       { get; private set; }
         
         public TestEntities(int? bits = default, int? channels = default, int? channel = default) 
             => Initialize(bits, channels, channel);
@@ -102,41 +102,54 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             }
             
             // Record
-            SynthWishes.RunOnThis(() => (SynthWishes.GetChannel == 0 ? FlowNode : FlowNode2)
-                                        // Tape-Bound
-                                        .AfterRecord(x => 
-                                        {
-                                            Tape = x;
-                                            TapeConfig  = x.Config;
-                                            TapeActions = x.Actions;
-                                            TapeAction  = x.Actions.AfterRecord;
-                                        })
-                                        .AfterRecordChannel(x =>
-                                        {
-                                            ChannelEntities[x.i].Tape = x;
-                                            ChannelEntities[x.i].TapeConfig = x.Config;
-                                            ChannelEntities[x.i].TapeActions = x.Actions;
-                                            ChannelEntities[x.i].TapeAction = x.Actions.AfterRecordChannel;
-                                        }));
+            SynthWishes.RunOnThis(
+                () => (SynthWishes.GetChannel == 0 ? FlowNode : FlowNode2)
+                      .AfterRecord(x =>
+                      {
+                          // Tape-Bound
+                          Tape               = x;
+                          TapeConfig         = x.Config;
+                          TapeActions        = x.Actions;
+                          TapeAction         = x.Actions.AfterRecord;
+                          // Buff-Bound
+                          Buff               = x.Buff;
+                          AudioFileOutput    = x.UnderlyingAudioFileOutput;
+                          // Independent after Taping
+                          Sample             = x.UnderlyingSample;
+                          AudioInfoWish      = Sample.ToWish();
+                          AudioFileInfo      = AudioInfoWish.FromWish();
+                          // Immutable
+                          WavHeader          = Sample.ToWavHeader();
+                          SampleDataTypeEnum = Sample.GetSampleDataTypeEnum();
+                          SampleDataType     = Sample.SampleDataType;
+                          SpeakerSetupEnum   = Sample.GetSpeakerSetupEnum();
+                          SpeakerSetup       = Sample.SpeakerSetup;
+                      })
+                      .AfterRecordChannel(x =>
+                      {
+                          var e = ChannelEntities[x.i];
+                          // Tape-Bound for Channel
+                          e.Tape               = x;
+                          e.TapeConfig         = x.Config;
+                          e.TapeActions        = x.Actions;
+                          e.TapeAction         = x.Actions.AfterRecordChannel;
+                          // Buff-Bound for Channel
+                          e.Buff               = x.Buff;
+                          e.AudioFileOutput    = x.UnderlyingAudioFileOutput;
+                          // Independent after Taping Channel
+                          e.Sample             = x.UnderlyingSample;
+                          e.AudioInfoWish      = e.Sample.ToWish();
+                          e.AudioFileInfo      = e.AudioInfoWish.FromWish();
+                          // Immutables for Channel
+                          e.WavHeader          = e.Sample.ToWavHeader();
+                          e.SampleDataTypeEnum = e.Sample.GetSampleDataTypeEnum();
+                          e.SampleDataType     = e.Sample.SampleDataType;
+                          e.SpeakerSetupEnum   = e.Sample.GetSpeakerSetupEnum();
+                          e.SpeakerSetup       = e.Sample.SpeakerSetup;
+                      }));
             
             IsNotNull(() => Tape);
             
-            // Buff-Bound
-            Buff            = Tape.Buff;
-            AudioFileOutput = Tape.UnderlyingAudioFileOutput;
-            
-            // Independent after Taping
-            Sample        = Tape.UnderlyingSample;
-            AudioInfoWish = Sample.ToWish();
-            AudioFileInfo = AudioInfoWish.FromWish();
-            
-            // Immutable
-            WavHeader          = Sample.ToWavHeader();
-            SampleDataTypeEnum = Sample.GetSampleDataTypeEnum();
-            SampleDataType     = Sample.SampleDataType;
-            SpeakerSetupEnum   = Sample.GetSpeakerSetupEnum();
-            SpeakerSetup       = Sample.SpeakerSetup;
-
             int bits = SynthWishes.GetBits;
             switch (bits)
             {
