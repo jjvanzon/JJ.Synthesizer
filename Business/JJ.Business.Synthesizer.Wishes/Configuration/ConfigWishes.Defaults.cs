afa71b2988c2ed7e0c51e044bc9b435f1143fa79
jjvanzon <jjvanzon79@gmail.com>
2025-01-13 04:47:28 +0100

    🍌 ConfigWishes: Split off default values to a partial class in a separate file.

---
+++ Business/JJ.Business.Synthesizer.Wishes/Configuration/ConfigWishes.Defaults.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JJ.Business.Synthesizer.Enums;
+using static JJ.Business.Synthesizer.Enums.AudioFileFormatEnum;
+using static JJ.Business.Synthesizer.Enums.InterpolationTypeEnum;
+using static JJ.Business.Synthesizer.Wishes.Configuration.TimeOutActionEnum;
+
+namespace JJ.Business.Synthesizer.Wishes.Configuration
+{
+    public partial class ConfigWishes
+    {
+                
+        // Defaults
+
+        // Audio Quality
+        
+        public const int                   DefaultBits          = 32;
+        public const int                   DefaultChannels      = 1;
+        public static readonly int?        DefaultChannel       = null;
+        public const int                   DefaultSamplingRate  = 48000;
+        public const AudioFileFormatEnum   DefaultAudioFormat   = Wav;
+        public const InterpolationTypeEnum DefaultInterpolation = Line;
+
+        // Audio Lengths
+        
+        /// <inheritdoc cref="docs._notelength" />
+        public const double DefaultNoteLength      = 0.50;
+        public const double DefaultBarLength       = 1.00;
+        public const double DefaultBeatLength      = 0.25;
+        public const double DefaultAudioLength     = 1.00;
+        public const double DefaultLeadingSilence  = 0.25;
+        public const double DefaultTrailingSilence = 0.25;
+        
+        // Feature Toggles
+        
+        public const bool   DefaultAudioPlayback      = true;
+        public const bool   DefaultDiskCache          = false;
+        public const bool   DefaultMathBoost          = true;
+        public const bool   DefaultParallelProcessing = true;
+        public const bool   DefaultPlayAllTapes       = false;
+
+        // Tooling
+        
+        public const bool   DefaultToolingAudioPlayback                  = false;
+        public const bool   DefaultToolingImpersonation                  = false;
+        public const int    DefaultNCrunchSamplingRate                   = 150;
+        public const int    DefaultNCrunchSamplingRateLongRunning        = 8;
+        public const int    DefaultAzurePipelinesSamplingRate            = 1500;
+        public const int    DefaultAzurePipelinesSamplingRateLongRunning = 100;
+        
+        // Misc Settings
+        
+        /// <inheritdoc cref="docs._leafchecktimeout" />
+        public const double            DefaultLeafCheckTimeOut       = 60;
+        /// <inheritdoc cref="docs._leafchecktimeout" />
+        public const TimeOutActionEnum DefaultTimeOutAction          = Continue;
+        public const int               DefaultCourtesyFrames         = 4;
+        public const int               DefaultFileExtensionMaxLength = 4;
+        public const string            DefaultLongTestCategory       = "Long";
+
+        // Derive Defaults
+        
+        public static int DefaultFrameSize => DefaultBits / 8 * DefaultChannels;
+    }
+}
--- Business/JJ.Business.Synthesizer.Wishes/Configuration/ConfigWishes.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Configuration/ConfigWishes.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Xml.Serialization;
 using JetBrains.Annotations;
 using JJ.Business.Synthesizer.Enums;
 using JJ.Business.Synthesizer.Wishes.Configuration;
@@ -10,7 +9,6 @@ using JJ.Framework.Persistence;
 using JJ.Framework.Reflection;
 using static JJ.Business.Synthesizer.Enums.AudioFileFormatEnum;
 using static JJ.Business.Synthesizer.Enums.InterpolationTypeEnum;
-using static JJ.Business.Synthesizer.Wishes.Configuration.TimeOutActionEnum;
 using static JJ.Framework.Wishes.Common.EnvironmentHelperWishes;
 using static JJ.Framework.Wishes.Common.FilledInWishes;
 using static JJ.Framework.Wishes.Configuration.ConfigurationManagerWishes;
@@ -31,58 +29,6 @@ namespace JJ.Business.Synthesizer.Wishes.Configuration
 
         /// <summary> For static contexts use this. </summary>
         internal static ConfigWishes Static { get; } = new ConfigWishes();
-        
-        // Defaults
-
-        // Audio Quality
-        
-        public const int                   DefaultBits          = 32;
-        public const int                   DefaultChannels      = 1;
-        public static readonly int?        DefaultChannel       = null;
-        public const int                   DefaultSamplingRate  = 48000;
-        public const AudioFileFormatEnum   DefaultAudioFormat   = Wav;
-        public const InterpolationTypeEnum DefaultInterpolation = Line;
-
-        // Audio Lengths
-        
-        /// <inheritdoc cref="docs._notelength" />
-        public const double DefaultNoteLength      = 0.50;
-        public const double DefaultBarLength       = 1.00;
-        public const double DefaultBeatLength      = 0.25;
-        public const double DefaultAudioLength     = 1.00;
-        public const double DefaultLeadingSilence  = 0.25;
-        public const double DefaultTrailingSilence = 0.25;
-        
-        // Feature Toggles
-        
-        public const bool   DefaultAudioPlayback      = true;
-        public const bool   DefaultDiskCache          = false;
-        public const bool   DefaultMathBoost          = true;
-        public const bool   DefaultParallelProcessing = true;
-        public const bool   DefaultPlayAllTapes       = false;
-
-        // Tooling
-        
-        public const bool   DefaultToolingAudioPlayback                  = false;
-        public const bool   DefaultToolingImpersonation                  = false;
-        public const int    DefaultNCrunchSamplingRate                   = 150;
-        public const int    DefaultNCrunchSamplingRateLongRunning        = 8;
-        public const int    DefaultAzurePipelinesSamplingRate            = 1500;
-        public const int    DefaultAzurePipelinesSamplingRateLongRunning = 100;
-        
-        // Misc Settings
-        
-        /// <inheritdoc cref="docs._leafchecktimeout" />
-        public const double            DefaultLeafCheckTimeOut       = 60;
-        /// <inheritdoc cref="docs._leafchecktimeout" />
-        public const TimeOutActionEnum DefaultTimeOutAction          = Continue;
-        public const int               DefaultCourtesyFrames         = 4;
-        public const int               DefaultFileExtensionMaxLength = 4;
-        public const string            DefaultLongTestCategory       = "Long";
-
-        // Derive Defaults
-        
-        public static int DefaultFrameSize => DefaultBits / 8 * DefaultChannels;
 
         // Environment Variables
 
