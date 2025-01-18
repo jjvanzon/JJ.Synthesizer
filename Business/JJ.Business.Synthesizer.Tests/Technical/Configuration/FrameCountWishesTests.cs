0af3a9463da6a9f6d5462b4845970ac432a467fd
jjvanzon <jjvanzon79@gmail.com>
2025-01-18 01:21:26 +0100

    🥼 Synth Tests: Create `FrameCountWishesTests` boilerplate by adapting `SamplingRateWishesTests`. Replaced `SamplingRate` by `FrameCount` and tailored to relevant members.

---
+++ Business/JJ.Business.Synthesizer.Tests/Technical/Configuration/FrameCountWishesTests.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JJ.Business.Synthesizer.Infos;
+using JJ.Business.Synthesizer.Structs;
+using JJ.Business.Synthesizer.Tests.Accessors;
+using JJ.Business.Synthesizer.Wishes.Configuration;
+using JJ.Persistence.Synthesizer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using static JJ.Framework.Testing.AssertHelper;
+using static JJ.Business.Synthesizer.Wishes.Configuration.ConfigWishes;
+
+#pragma warning disable CS0611
+#pragma warning disable MSTEST0018
+
+namespace JJ.Business.Synthesizer.Tests.Technical.Configuration
+{
+    [TestClass]
+    [TestCategory("Technical")]
+    public class FrameCountWishesTests
+    {
+        
+        [DataTestMethod]
+        [DynamicData(nameof(TestParametersInit))]
+        public void Init_FrameCount(int? init)
+        {
+            var x = CreateTestEntities(init);
+            Assert_All_Getters(x, Coalesce(init));
+        }
+        
+        [TestMethod] 
+        [DynamicData(nameof(TestParametersWithEmpty))]
+        public void SynthBound_FrameCount(int? init, int? value)
+        {            
+            void AssertProp(Action<TestEntities> setter)
+            {
+                var x = CreateTestEntities(init);
+                Assert_All_Getters(x, Coalesce(init));
+                
+                setter(x);
+                
+                Assert_SynthBound_Getters(x, Coalesce(value));
+                Assert_TapeBound_Getters(x, Coalesce(init));
+                Assert_BuffBound_Getters(x, Coalesce(init));
+                Assert_Independent_Getters(x, Coalesce(init));
+                Assert_Immutable_Getters(x, Coalesce(init));
+                
+                x.Record();
+                Assert_All_Getters(x, Coalesce(value));
+            }
+
+            AssertProp(x => AreEqual(x.SynthBound.SynthWishes,    x.SynthBound.SynthWishes   .FrameCount(value)));
+            AssertProp(x => AreEqual(x.SynthBound.FlowNode,       x.SynthBound.FlowNode      .FrameCount(value)));
+            AssertProp(x => AreEqual(x.SynthBound.ConfigResolver, x.SynthBound.ConfigResolver.FrameCount(value, x.SynthBound.SynthWishes)));
+        }
+
+        [TestMethod] 
+        [DynamicData(nameof(TestParameters))]
+        public void TapeBound_FrameCount(int init, int value)
+        {
+            void AssertProp(Action<TestEntities> setter)
+            {
+                var x = CreateTestEntities(init);
+                Assert_All_Getters(x, init);
+                
+                setter(x);
+                
+                Assert_SynthBound_Getters(x, init);
+                Assert_TapeBound_Getters(x, value);
+                Assert_BuffBound_Getters(x, init);
+                Assert_Independent_Getters(x, init);
+                Assert_Immutable_Getters(x, init);
+                
+                x.Record();
+                Assert_All_Getters(x, init); // By Design: Currently you can't record over the same tape. So you always get a new tape, resetting the values.
+            }
+
+            AssertProp(x => AreEqual(x.TapeBound.Tape,        () => x.TapeBound.Tape       .FrameCount(value)));
+            AssertProp(x => AreEqual(x.TapeBound.TapeConfig,  () => x.TapeBound.TapeConfig .FrameCount(value)));
+            AssertProp(x => AreEqual(x.TapeBound.TapeActions, () => x.TapeBound.TapeActions.FrameCount(value)));
+            AssertProp(x => AreEqual(x.TapeBound.TapeAction,  () => x.TapeBound.TapeAction .FrameCount(value)));
+        }
+
+        [TestMethod] 
+        [DynamicData(nameof(TestParameters))]
+        public void BuffBound_FrameCount(int init, int value)
+        {
+            void AssertProp(Action<TestEntities> setter)
+            {
+                var x = CreateTestEntities(init);
+                Assert_All_Getters(x, init);
+                
+                setter(x);
+                
+                Assert_SynthBound_Getters(x, init);
+                Assert_TapeBound_Getters(x, init);
+                Assert_BuffBound_Getters(x, value);
+                Assert_Independent_Getters(x, init);
+                Assert_Immutable_Getters(x, init);
+                
+                x.Record();
+                Assert_All_Getters(x, init);
+            }
+
+            AssertProp(x => AreEqual(x.BuffBound.AudioFileOutput, () => x.BuffBound.AudioFileOutput.FrameCount(value)));
+        }
+        
+        [TestMethod]
+        [DynamicData(nameof(TestParameters))]
+        public void Independent_FrameCount(int init, int value)
+        {
+            // Independent after Taping
+
+            // AudioInfoWish
+            {
+                TestEntities x = default;
+
+                void AssertProp(Action setter)
+                {
+                    x = CreateTestEntities(init);
+                    Assert_All_Getters(x, init);
+                    
+                    setter();
+                    
+                    Assert_Bound_Getters(x, init);
+                    Assert_Independent_Getters(x.Independent.AudioInfoWish, value);
+                    Assert_Independent_Getters(x.Independent.AudioFileInfo, init);
+                    Assert_Independent_Getters(x.Independent.Sample, init);
+                    Assert_Immutable_Getters(x, init);
+
+                    x.Record();
+                    Assert_All_Getters(x, init);
+                }
+
+                AssertProp(() => AreEqual(x.Independent.AudioInfoWish, () => x.Independent.AudioInfoWish.FrameCount(value)));
+                AssertProp(() =>                                             x.Independent.AudioInfoWish.FrameCount = value);
+            }
+                        
+            // AudioFileInfo
+            {
+                TestEntities x = default;
+                
+                void AssertProp(Action setter)
+                {
+                    x = CreateTestEntities(init);
+                    Assert_All_Getters(x, init);
+                    
+                    setter();
+                    
+                    Assert_Bound_Getters(x, init);
+                    Assert_Independent_Getters(x.Independent.AudioFileInfo, value);
+                    Assert_Independent_Getters(x.Independent.AudioInfoWish, init);
+                    Assert_Independent_Getters(x.Independent.Sample, init);
+                    Assert_Immutable_Getters(x, init);
+
+                    x.Record();
+                    Assert_All_Getters(x, init);
+                }
+
+                AssertProp(() => AreEqual(x.Independent.AudioFileInfo, () => x.Independent.AudioFileInfo.FrameCount(value)));
+                AssertProp(() =>                                             x.Independent.AudioFileInfo.SampleCount = value);
+            }
+        }
+        
+        [TestMethod] 
+        [DynamicData(nameof(TestParameters))]
+        public void Immutable_FrameCount(int init, int value)
+        {
+            TestEntities x = CreateTestEntities(init);
+
+            // WavHeader
+            
+            var wavHeaders = new List<WavHeaderStruct>();
+            {
+                void AssertProp(Func<WavHeaderStruct> setter)
+                {
+                    Assert_Immutable_Getters(x.Immutable.WavHeader, init);
+                    
+                    WavHeaderStruct wavHeader2 = setter();
+                    
+                    Assert_Immutable_Getters(x.Immutable.WavHeader, init);
+                    Assert_Immutable_Getters(wavHeader2, value);
+                    
+                    wavHeaders.Add(wavHeader2);
+                }
+
+                AssertProp(() => x.Immutable.WavHeader.FrameCount(value));
+            }
+            
+            // After-Record
+            x.Record();
+            
+            // All is reset
+            Assert_All_Getters(x, init);
+            
+            // Except for our variables
+            wavHeaders.ForEach(w => Assert_Immutable_Getters(w, value));
+        }
+
+        [TestMethod] public void ConfigSections_FrameCount()
+        {
+            // Global-Bound. Immutable. Get-only.
+            var configSection = TestEntities.GetConfigSectionAccessor();
+            
+            AreEqual(DefaultFrameCount, () => configSection.FrameCount());
+            AreEqual( 1 /*sec*/ * 48000 /*Hz*/, () => DefaultFrameCount);
+        }
+
+        // Getter Helpers
+        
+        private void Assert_All_Getters(TestEntities x, int frameCount)
+        {
+            Assert_Bound_Getters(x, frameCount);
+            Assert_Independent_Getters(x, frameCount);
+            Assert_Immutable_Getters(x, frameCount);
+        }
+
+        private void Assert_Bound_Getters(TestEntities x, int frameCount)
+        {
+            Assert_SynthBound_Getters(x, frameCount);
+            Assert_TapeBound_Getters(x, frameCount);
+            Assert_BuffBound_Getters(x, frameCount);
+        }
+        
+        private void Assert_Independent_Getters(TestEntities x, int frameCount)
+        {
+            // Independent after Taping
+            Assert_Independent_Getters(x.Independent.Sample, frameCount);
+            Assert_Independent_Getters(x.Independent.AudioInfoWish, frameCount);
+            Assert_Independent_Getters(x.Independent.AudioFileInfo, frameCount);
+        }
+
+        private void Assert_Immutable_Getters(TestEntities x, int frameCount)
+        {
+            Assert_Immutable_Getters(x.Immutable.WavHeader, frameCount);
+        }
+
+        private void Assert_SynthBound_Getters(TestEntities x, int frameCount)
+        {
+            AreEqual(frameCount, () => x.SynthBound.SynthWishes.FrameCount());
+            AreEqual(frameCount, () => x.SynthBound.FlowNode.FrameCount());
+            AreEqual(frameCount, () => x.SynthBound.ConfigResolver.FrameCount(x.SynthBound.SynthWishes));
+        }
+        
+        private void Assert_TapeBound_Getters(TestEntities x, int frameCount)
+        {
+            AreEqual(frameCount, () => x.TapeBound.Tape.FrameCount());
+            AreEqual(frameCount, () => x.TapeBound.TapeConfig.FrameCount());
+            AreEqual(frameCount, () => x.TapeBound.TapeActions.FrameCount());
+            AreEqual(frameCount, () => x.TapeBound.TapeAction.FrameCount());
+        }
+        
+        private void Assert_BuffBound_Getters(TestEntities x, int frameCount)
+        {
+            AreEqual(frameCount, () => x.BuffBound.Buff.FrameCount());
+            AreEqual(frameCount, () => x.BuffBound.AudioFileOutput.FrameCount());
+        }
+        
+        private void Assert_Independent_Getters(Sample sample, int frameCount)
+        {
+            AreEqual(frameCount, () => sample.FrameCount());
+        }
+        
+        private void Assert_Independent_Getters(AudioInfoWish audioInfoWish, int frameCount)
+        {
+            AreEqual(frameCount, () => audioInfoWish.FrameCount());
+            AreEqual(frameCount, () => audioInfoWish.FrameCount);
+        }
+
+        private void Assert_Independent_Getters(AudioFileInfo audioFileInfo, int frameCount)
+        {
+            AreEqual(frameCount, () => audioFileInfo.FrameCount());
+            AreEqual(frameCount, () => audioFileInfo.SampleCount);
+        }
+
+        private void Assert_Immutable_Getters(WavHeaderStruct wavHeader, int frameCount)
+        {
+            AreEqual(frameCount, () => wavHeader.FrameCount());
+        }
+ 
+        // Test Data Helpers
+        
+        private TestEntities CreateTestEntities(int? frameCount) => new TestEntities(x => x.FrameCount(frameCount));
+        
+        private int Coalesce(int? frameCount) => CoalesceFrameCount(frameCount, defaultValue: 1 * 48000);
+
+        // ncrunch: no coverage start
+        
+        static IEnumerable<object[]> TestParametersInit => new[]
+        {
+            new object[] { 0 },
+            new object[] { null },
+            new object[] { 96000 },
+            new object[] { 88200 },
+            new object[] { 48000 },
+            new object[] { 44100 },
+            new object[] { 22050 },
+            new object[] { 11025 },
+            new object[] { 1 },
+            new object[] { 8 },
+            new object[] { 16 },
+            new object[] { 32 },
+            new object[] { 64 },
+            new object[] { 100 },
+            new object[] { 1000 },
+            new object[] { 12345 },
+            new object[] { 1234567 } 
+        };
+        
+        static IEnumerable<object[]> TestParametersWithEmpty => new[]
+        {
+            new object[] {   22050 ,     0 },
+            new object[] {       0 , 11025 },
+            new object[] { 1234567 ,  null },
+            new object[] {    null , 12345 },
+            
+        }.Concat(TestParameters);
+
+        static IEnumerable<object[]> TestParameters => new[] 
+        {
+            new object[] { 48000, 96000 },
+            new object[] { 48000, 88200 },
+            new object[] { 48000, 48000 },
+            new object[] { 48000, 44100 },
+            new object[] { 48000, 22050 },
+            new object[] { 48000, 11025 },
+            new object[] { 48000, 1 },
+            new object[] { 48000, 8 },
+            new object[] { 96000, 48000 },
+            new object[] { 88200, 44100 },
+            new object[] { 44100, 48000 },
+            new object[] { 22050, 44100 },
+            new object[] { 11025, 44100 },
+            new object[] { 1, 48000 },
+            new object[] { 8, 48000 },
+            new object[] { 48000, 16 },
+            new object[] { 48000, 32 },
+            new object[] { 48000, 64 },
+            new object[] { 48000, 100 },
+            new object[] { 48000, 1000 },
+            new object[] { 48000, 12345 },
+            new object[] { 48000, 1234567 },
+        };
+
+        // ncrunch: no coverage end
+    } 
+}
