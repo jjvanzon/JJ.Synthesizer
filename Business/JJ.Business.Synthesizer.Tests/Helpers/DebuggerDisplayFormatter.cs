52bf6bc3f0078c98b2e0b73e450369b8e73bd0e1
jjvanzon <jjvanzon79@gmail.com>
2025-01-20 23:54:21 +0100

    [Draft] Synth Tests: Add DebuggerDisplays for Case, CaseProp, and Values classes for easier debugging.

---
+++ Business/JJ.Business.Synthesizer.Tests/Helpers/DebuggerDisplayFormatter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JJ.Business.Synthesizer.Tests.Technical.Configuration;
+using JJ.Framework.Reflection;
+
+namespace JJ.Business.Synthesizer.Tests.Helpers
+{
+    internal static class DebuggerDisplayFormatter
+    {
+        internal static string GetDebuggerDisplay(FrameCountWishesTests.Case testCase)
+        {
+            return "{Case} " + testCase;
+        }
+        
+        internal static string GetDebuggerDisplay<T>(
+            FrameCountWishesTests.CaseProp<T> caseProp) where T : struct
+        {
+            return "{CaseProp} " + caseProp;
+        }
+
+        internal static string GetDebuggerDisplay<T>(
+            FrameCountWishesTests.Values<T> values) where T : struct
+        {
+            return "{Values} " + values;
+        }
+    }
+}
