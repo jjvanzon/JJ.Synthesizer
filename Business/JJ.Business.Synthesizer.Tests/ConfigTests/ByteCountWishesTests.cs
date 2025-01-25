12389df0b4b981335edbe88025e6ad7b514e8c0c
jjvanzon <jjvanzon79@gmail.com>
2025-01-25 03:51:49 +0100

    🧫 Synth Tests: ByteCountWishesTests - Extract intermediate variables and test conversion formula with different input numbers for different stages of the calculation.

--- Business/JJ.Business.Synthesizer.Tests/ConfigTests/ByteCountWishesTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/ConfigTests/ByteCountWishesTests.cs
@@ -14,19 +14,22 @@ namespace JJ.Business.Synthesizer.Tests.ConfigTests
         [TestMethod]
         public void ByteCount_ConversionFormula_Basics()
         {
-            // FromAudioLength
-            {
-                const double audioLength    = 0.5;
-                const int    samplingRate   = 24000;
-                const int    bits           = 16;
-                const int    channels       = 2;
-                const int    headerLength   = 44;
-                const int    courtesyFrames = 3;
-                
-                int expected = ((int)(audioLength * samplingRate) + courtesyFrames) * bits / 8 * channels + headerLength;
-                
-                AreEqual(expected, () => audioLength.ByteCount(samplingRate, bits, channels, headerLength, courtesyFrames));
-            }
+            const double audioLength    = 0.5;
+            const int    samplingRate   = 24000;
+            const int    bits           = 16;
+            const int    channels       = 2;
+            const int    headerLength   = 44;
+            const int    courtesyFrames = 3;
+            
+            const int frameCount = (int)(audioLength * samplingRate) + courtesyFrames;
+            const int frameSize  = bits / 8 * channels;
+            
+            const int byteCountExpected = frameCount * frameSize + headerLength;
+            
+            AreEqual(byteCountExpected, () => audioLength.ByteCount(samplingRate, bits, channels, headerLength, courtesyFrames));
+            AreEqual(byteCountExpected, () => audioLength.ByteCount(samplingRate, frameSize, headerLength, courtesyFrames));
+            AreEqual(byteCountExpected, () => frameCount.ByteCount(bits, channels, headerLength));
+            AreEqual(byteCountExpected, () => frameCount.ByteCount(frameSize, headerLength));
         }
     }
 }
