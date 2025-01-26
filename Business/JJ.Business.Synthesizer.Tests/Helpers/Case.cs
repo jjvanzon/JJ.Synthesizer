dbbadcaf0fdd55c2dd4bb6f4b6b46abe814561b2
jjvanzon <jjvanzon79@gmail.com>
2025-01-26 20:14:09 +0100

    🧫 Synth Tests: Generalize test Case class - Busy moving shared logic and data from specialized Case class to CaseBase.

--- Business/JJ.Business.Synthesizer.Tests/Helpers/Case.cs
+++ Business/JJ.Business.Synthesizer.Tests/Helpers/Case.cs
@@ -10,39 +10,34 @@ using static JJ.Framework.Wishes.Common.FilledInWishes;
 namespace JJ.Business.Synthesizer.Tests.Helpers
 {
     [DebuggerDisplay("{DebuggerDisplay}")]
-    internal class Case : CaseProp<int>
+    internal class Case : CaseBase<int>
     {
-        /// <inheritdoc cref="docs._strict />
-        public bool Strict { get; set; } = true;
-
         // FrameCount: The main property being tested, adjusted directly or via dependencies.
         public CaseProp<int> FrameCount => this;
         public CaseProp<int> Frames => this;
         
         // SamplingRate: Scales FrameCount
-        public CaseProp<int> SamplingRate { get; set; } = new CaseProp<int>();
-        public CaseProp<int> Hertz { get => SamplingRate; set => SamplingRate = value; }
-        public CaseProp<int> Hz    { get => SamplingRate; set => SamplingRate = value; }
+        //public CaseProp<int> SamplingRate { get; set; } = new CaseProp<int>();
+        public CaseProp<int> SamplingRate { get => GetProp<int>(0); set => SetProp(0, value); }
+        public CaseProp<int> Hertz        { get => SamplingRate; set => SamplingRate = value; }
+        public CaseProp<int> Hz           { get => SamplingRate; set => SamplingRate = value; }
 
         // AudioLength: Scales FrameCount + FrameCount setters adjust AudioLength.
-        public CaseProp<double> AudioLength { get; set; } = new CaseProp<double>();
-        public CaseProp<double> Length   { get => AudioLength; set => AudioLength = value; }
-        public CaseProp<double> Len      { get => AudioLength; set => AudioLength = value; }
-        public CaseProp<double> Duration { get => AudioLength; set => AudioLength = value; }
-        public CaseProp<double> seconds  { get => AudioLength; set => AudioLength = value; }
-        public CaseProp<double> sec      { get => AudioLength; set => AudioLength = value; }
+        public CaseProp<double> AudioLength { get => GetProp<double>(1); set => SetProp(1, value); }
+        public CaseProp<double> Length      { get => AudioLength; set => AudioLength = value; }
+        public CaseProp<double> Len         { get => AudioLength; set => AudioLength = value; }
+        public CaseProp<double> Duration    { get => AudioLength; set => AudioLength = value; }
+        public CaseProp<double> seconds     { get => AudioLength; set => AudioLength = value; }
+        public CaseProp<double> sec         { get => AudioLength; set => AudioLength = value; }
         
         // CourtesyFrames: AudioLength does not incorporate CourtesyFrames, but FrameCount does.
-        public CaseProp<int> CourtesyFrames { get; set; } = new CaseProp<int>();
-        public CaseProp<int> PlusFrames { get => CourtesyFrames; set => CourtesyFrames = value; }
-        public CaseProp<int> Plus       { get => CourtesyFrames; set => CourtesyFrames = value; }
+        public CaseProp<int> CourtesyFrames { get => GetProp<int>(2); set => SetProp(2, value); }
+        public CaseProp<int> PlusFrames     { get => CourtesyFrames; set => CourtesyFrames = value; }
+        public CaseProp<int> Plus           { get => CourtesyFrames; set => CourtesyFrames = value; }
 
         // Descriptions            
         
         string DebuggerDisplay => DebuggerDisplay(this);
-        public override string ToString() => Descriptor;
-        public string Name { get; set; }
-        public object[] DynamicData => new object[] { Descriptor };
 
         public override string Descriptor
         {
--- Business/JJ.Business.Synthesizer.Tests/Helpers/CaseBase.cs
+++ Business/JJ.Business.Synthesizer.Tests/Helpers/CaseBase.cs
@@ -3,25 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JJ.Framework.Reflection;
 
 namespace JJ.Business.Synthesizer.Tests.Helpers
 {
-    internal class CaseBase<TMainProp>
+    internal class CaseBase<TMainProp> : CaseProp<TMainProp>
         where TMainProp : struct
     {
-        public CaseProp<TMainProp> MainProp { get; set; }
-            
         /// <inheritdoc cref="docs._strict />
         public bool Strict { get; set; } = true;
 
-        public CaseBase()
+        // Descriptions
+        
+        public string Name { get; set; }
+        public override string ToString() => Descriptor;
+        public object[] DynamicData => new object[] { Descriptor };
+        
+        // Properties
+        
+        private readonly List<ICaseProp> _caseProps = new List<ICaseProp>();
+        
+        protected CaseProp<T> GetProp<T>(int index) where T: struct
         {
-            MainProp = new CaseProp<TMainProp>();
+            // Auto-size collection
+            while (_caseProps.Count <= index) _caseProps.Add(null);
+            
+            // Get
+            ICaseProp obj = _caseProps[index]; 
+            
+            // TODO: Throw better exception message in case of wrong cast.
+
+            // Cast
+            var prop = (CaseProp<T>)obj; 
+            
+            // Or overwrite
+            prop = prop ?? new CaseProp<T>(); 
+            
+            // Store
+            _caseProps[index] = prop;
+            
+            return prop;
         }
         
-        public CaseBase(CaseProp<TMainProp> mainProp)
+        protected void SetProp<T>(int index, CaseProp<T> prop) where T: struct
         {
-            MainProp = mainProp;
+            while (_caseProps.Count <= index) _caseProps.Add(null);
+            _caseProps[index] = prop ?? throw new NullException(() => prop);;
         }
+        
+        // Constructors
+        
+        public CaseBase() : base() { }
+        public CaseBase( TMainProp  value) : base(value) { }
+        public CaseBase( TMainProp? value) : base(value) { }
+        public CaseBase( TMainProp  from ,  TMainProp  to) : base(from, to) { }
+        public CaseBase( TMainProp  from ,  TMainProp? to) : base(from, to) { }
+        public CaseBase( TMainProp? from ,  TMainProp  to) : base(from, to) { }
+        public CaseBase( TMainProp? from ,  TMainProp? to) : base(from, to) { }
+        public CaseBase((TMainProp  from ,  TMainProp  to) values) : base(values) { }
+        public CaseBase((TMainProp? from ,  TMainProp  to) values) : base(values) { }
+        public CaseBase((TMainProp  from ,  TMainProp? to) values) : base(values) { }
+        public CaseBase((TMainProp? from ,  TMainProp? to) values) : base(values) { }
+        public CaseBase( TMainProp  from , (TMainProp? nully         ,  TMainProp coalesced) to) : base(from, to) { }
+        public CaseBase((TMainProp? nully,  TMainProp coalesced) from,  TMainProp to) : base(from, to) { }
+        public CaseBase((TMainProp? nully,  TMainProp coalesced) from, (TMainProp? nully, TMainProp coalesced) to) : base(from, to) { }
     }
 }
