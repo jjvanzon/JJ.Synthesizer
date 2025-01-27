15ee7f23c75d928740ff893f66e6abd6fe06a922
jjvanzon <jjvanzon79@gmail.com>
2025-01-27 05:15:15 +0100

    🧪 Synth Tests - Case structure: Add test to debug the Case Descriptor code for correcting errors.

---
+++ Business/JJ.Business.Synthesizer.Tests/Technical/CaseTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JJ.Business.Synthesizer.Tests.Helpers;
+using JJ.Business.Synthesizer.Wishes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static JJ.Framework.Testing.AssertHelper;
+
+namespace JJ.Business.Synthesizer.Tests.Technical
+{
+    [TestClass]
+    public class CaseTests
+    {
+        [TestMethod]
+        public void Test_Case_Descriptor()
+        { 
+            var testCase = new Case();
+            string descriptor = testCase.Descriptor;
+            LogWishes.Log(descriptor);
+            NotNullOrEmpty(() => descriptor);
+        }
+    }
+}
