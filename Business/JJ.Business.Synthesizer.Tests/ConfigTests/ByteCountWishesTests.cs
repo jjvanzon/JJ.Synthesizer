66675b8914c2abb183af8aa9981eb6c24a57ae86
jjvanzon <jjvanzon79@gmail.com>
2025-01-28 04:48:42 +0100

    🔬 [Draft] Synth Tests: ByteCountWishesTests - Remove lambda syntax in basic tests for simpler step-through debugging, trading clearer error messages for simplicity. Complex tests now ensure detailed validation.

--- Business/JJ.Business.Synthesizer.Tests/ConfigTests/ByteCountWishesTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/ConfigTests/ByteCountWishesTests.cs
@@ -33,10 +33,10 @@ namespace JJ.Business.Synthesizer.Tests.ConfigTests
             
             const int byteCountExpected = frameCount * frameSize + headerLength;
             
-            AreEqual(byteCountExpected, () => audioLength.ByteCount(samplingRate, bits, channels, headerLength, courtesyFrames));
-            AreEqual(byteCountExpected, () => audioLength.ByteCount(samplingRate, frameSize, headerLength, courtesyFrames));
-            AreEqual(byteCountExpected, () => frameCount.ByteCount(bits, channels, headerLength));
-            AreEqual(byteCountExpected, () => frameCount.ByteCount(frameSize, headerLength));
+            AreEqual(byteCountExpected, audioLength.ByteCount(samplingRate, bits, channels, headerLength, courtesyFrames));
+            AreEqual(byteCountExpected, audioLength.ByteCount(samplingRate, frameSize, headerLength, courtesyFrames));
+            AreEqual(byteCountExpected, frameCount.ByteCount(bits, channels, headerLength));
+            AreEqual(byteCountExpected, frameCount.ByteCount(frameSize, headerLength));
         }
 
         [TestMethod]
@@ -50,50 +50,47 @@ namespace JJ.Business.Synthesizer.Tests.ConfigTests
         [TestMethod]
         public void ByteCount_Basic_Getters()
         {
-            // Lambdas `() =>` improve assertion messages but can complicate debugging.
-            // TODO: Remove `() =>` when complex tests ensure clear assertion messages.
-            
             int byteCount = 100;
             var entities = new ConfigTestEntities(x => x.ByteCount(byteCount));
             
-            AreEqual(DefaultByteCount, () => entities.SynthBound.ConfigSection.ByteCount());
+            AreEqual(DefaultByteCount, entities.SynthBound.ConfigSection.ByteCount());
             
-            AreEqual(byteCount, () => entities.SynthBound.SynthWishes   .ByteCount());
-            AreEqual(byteCount, () => entities.SynthBound.FlowNode      .ByteCount());
-            AreEqual(byteCount, () => entities.SynthBound.FlowNode2     .ByteCount());
-            AreEqual(byteCount, () => entities.SynthBound.ConfigResolver.ByteCount(entities.SynthBound.SynthWishes));
+            AreEqual(byteCount, entities.SynthBound.SynthWishes   .ByteCount());
+            AreEqual(byteCount, entities.SynthBound.FlowNode      .ByteCount());
+            AreEqual(byteCount, entities.SynthBound.FlowNode2     .ByteCount());
+            AreEqual(byteCount, entities.SynthBound.ConfigResolver.ByteCount(entities.SynthBound.SynthWishes));
             
-            AreEqual(byteCount, () => entities.TapeBound.Tape           .ByteCount());
-            AreEqual(byteCount, () => entities.TapeBound.TapeConfig     .ByteCount());
-            AreEqual(byteCount, () => entities.TapeBound.TapeActions    .ByteCount());
-            AreEqual(byteCount, () => entities.TapeBound.TapeAction     .ByteCount());
+            AreEqual(byteCount, entities.TapeBound.Tape           .ByteCount());
+            AreEqual(byteCount, entities.TapeBound.TapeConfig     .ByteCount());
+            AreEqual(byteCount, entities.TapeBound.TapeActions    .ByteCount());
+            AreEqual(byteCount, entities.TapeBound.TapeAction     .ByteCount());
             
-            AreEqual(byteCount, () => entities.BuffBound.Buff           .ByteCount(DefaultCourtesyFrames));
-            AreEqual(byteCount, () => entities.BuffBound.AudioFileOutput.ByteCount(DefaultCourtesyFrames));
-            AreEqual(byteCount, () => entities.Independent.Sample       .ByteCount());
-            AreEqual(byteCount, () => entities.Immutable.WavHeader      .ByteCount());
+            AreEqual(byteCount, entities.BuffBound.Buff           .ByteCount(DefaultCourtesyFrames));
+            AreEqual(byteCount, entities.BuffBound.AudioFileOutput.ByteCount(DefaultCourtesyFrames));
+            AreEqual(byteCount, entities.Independent.Sample       .ByteCount());
+            AreEqual(byteCount, entities.Immutable.WavHeader      .ByteCount());
             
-            AreEqual(DefaultSizeOfBitDepth, () => entities.Immutable.Bits              .ByteCount());
-            AreEqual(DefaultSizeOfBitDepth, () => entities.Immutable.Type              .ByteCount());
-            AreEqual(DefaultSizeOfBitDepth, () => entities.Immutable.SampleDataType    .ByteCount());
-            AreEqual(DefaultSizeOfBitDepth, () => entities.Immutable.SampleDataTypeEnum.ByteCount());
+            AreEqual(DefaultSizeOfBitDepth, entities.Immutable.Bits              .ByteCount());
+            AreEqual(DefaultSizeOfBitDepth, entities.Immutable.Type              .ByteCount());
+            AreEqual(DefaultSizeOfBitDepth, entities.Immutable.SampleDataType    .ByteCount());
+            AreEqual(DefaultSizeOfBitDepth, entities.Immutable.SampleDataTypeEnum.ByteCount());
             
             foreach (var channelEntities in entities.ChannelEntities)
             {
-                AreEqual(byteCount, () => channelEntities.TapeBound.Tape           .ByteCount());
-                AreEqual(byteCount, () => channelEntities.TapeBound.TapeConfig     .ByteCount());
-                AreEqual(byteCount, () => channelEntities.TapeBound.TapeActions    .ByteCount());
-                AreEqual(byteCount, () => channelEntities.TapeBound.TapeAction     .ByteCount());
+                AreEqual(byteCount, channelEntities.TapeBound.Tape           .ByteCount());
+                AreEqual(byteCount, channelEntities.TapeBound.TapeConfig     .ByteCount());
+                AreEqual(byteCount, channelEntities.TapeBound.TapeActions    .ByteCount());
+                AreEqual(byteCount, channelEntities.TapeBound.TapeAction     .ByteCount());
                 
-                AreEqual(byteCount, () => channelEntities.BuffBound.Buff           .ByteCount(DefaultCourtesyFrames));
-                AreEqual(byteCount, () => channelEntities.BuffBound.AudioFileOutput.ByteCount(DefaultCourtesyFrames));
-                AreEqual(byteCount, () => channelEntities.Independent.Sample       .ByteCount());
-                AreEqual(byteCount, () => channelEntities.Immutable.WavHeader      .ByteCount());
+                AreEqual(byteCount, channelEntities.BuffBound.Buff           .ByteCount(DefaultCourtesyFrames));
+                AreEqual(byteCount, channelEntities.BuffBound.AudioFileOutput.ByteCount(DefaultCourtesyFrames));
+                AreEqual(byteCount, channelEntities.Independent.Sample       .ByteCount());
+                AreEqual(byteCount, channelEntities.Immutable.WavHeader      .ByteCount());
 
-                AreEqual(DefaultSizeOfBitDepth, () => channelEntities.Immutable.Bits              .ByteCount());
-                AreEqual(DefaultSizeOfBitDepth, () => channelEntities.Immutable.Type              .ByteCount());
-                AreEqual(DefaultSizeOfBitDepth, () => channelEntities.Immutable.SampleDataType    .ByteCount());
-                AreEqual(DefaultSizeOfBitDepth, () => channelEntities.Immutable.SampleDataTypeEnum.ByteCount());
+                AreEqual(DefaultSizeOfBitDepth, channelEntities.Immutable.Bits              .ByteCount());
+                AreEqual(DefaultSizeOfBitDepth, channelEntities.Immutable.Type              .ByteCount());
+                AreEqual(DefaultSizeOfBitDepth, channelEntities.Immutable.SampleDataType    .ByteCount());
+                AreEqual(DefaultSizeOfBitDepth, channelEntities.Immutable.SampleDataTypeEnum.ByteCount());
             }
         }
         
