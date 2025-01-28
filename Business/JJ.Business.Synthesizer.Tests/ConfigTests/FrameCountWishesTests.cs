12731015c8d9c42c7fe98acbd0c93f93717a23bf
jjvanzon <jjvanzon79@gmail.com>
2025-01-28 21:01:37 +0100

    🚚 Synth Tests: FrameCountWishesTests - Move specialized Case class into TestClass to encapsulate its usage.

--- Business/JJ.Business.Synthesizer.Tests/ConfigTests/FrameCountWishesTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/ConfigTests/FrameCountWishesTests.cs
@@ -32,6 +32,52 @@ namespace JJ.Business.Synthesizer.Tests.ConfigTests
         private const int DefaultHz = DefaultSamplingRate;
         private const int DefaultHertz = DefaultSamplingRate;
 
+        internal class Case : CaseBase<int>
+        {
+            internal override IList<object> KeyElements 
+                => new object[] { Name, "~", PropDescriptor, "f", "(", SamplingRate, "Hz", "+", CourtesyFrames, (",", AudioLength, "s"), ")" };
+
+            // FrameCount: The main property being tested, adjusted directly or via dependencies.
+            public CaseProp<int> FrameCount => this;
+            public CaseProp<int> Frames => this;
+            
+            // SamplingRate: Scales FrameCount
+            public CaseProp<int> SamplingRate { get; set; } = new CaseProp<int>();
+            public CaseProp<int> Hertz        { get => SamplingRate; set => SamplingRate = value; }
+            public CaseProp<int> Hz           { get => SamplingRate; set => SamplingRate = value; }
+            
+            // CourtesyFrames: AudioLength does not incorporate CourtesyFrames, but FrameCount does.
+            public CaseProp<int> CourtesyFrames { get; set; } = new CaseProp<int>();
+            public CaseProp<int> PlusFrames     { get => CourtesyFrames; set => CourtesyFrames = value; }
+            public CaseProp<int> Plus           { get => CourtesyFrames; set => CourtesyFrames = value; }
+
+            // AudioLength: Scales FrameCount + FrameCount setters adjust AudioLength.
+            public CaseProp<double> AudioLength { get; set; } = new CaseProp<double>();
+            public CaseProp<double> Length      { get => AudioLength; set => AudioLength = value; }
+            public CaseProp<double> Len         { get => AudioLength; set => AudioLength = value; }
+            public CaseProp<double> Duration    { get => AudioLength; set => AudioLength = value; }
+            public CaseProp<double> seconds     { get => AudioLength; set => AudioLength = value; }
+            public CaseProp<double> sec         { get => AudioLength; set => AudioLength = value; }
+
+            // Constructors
+            
+            public Case(
+                int?    frameCount     = null,
+                int?    samplingRate   = null,
+                double? audioLength    = null,
+                int?    courtesyFrames = null)
+            {
+                if (frameCount     != null) From = To      = frameCount.Value;
+                if (samplingRate   != null) SamplingRate   = samplingRate.Value;
+                if (audioLength    != null) AudioLength    = audioLength.Value;
+                if (courtesyFrames != null) CourtesyFrames = courtesyFrames.Value;
+            }
+            
+            public Case() { }
+            public Case(int frameCount) : base(frameCount) { }
+            public Case(int from, int to) : base(from, to) { }
+        }
+
         // ncrunch: no coverage start
 
         static CaseCollection<Case> Cases { get; } = new CaseCollection<Case>();
