4e345b4c62cbef90abca4a1233e9f82d3cd8f18f
jjvanzon <jjvanzon79@gmail.com>
2025-01-29 02:23:49 +0100

    ❔ [Draft] SynthWishes: Add FilledIn methods to WavHeaderStruct to check if it’s zeroed out or populated.

---
+++ Business/JJ.Business.Synthesizer.Wishes/Helpers/FilledInExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JJ.Business.Synthesizer.Structs;
+
+namespace JJ.Business.Synthesizer.Wishes.Helpers
+{
+    /// <inheritdoc cref="docs._filledinhelper" />
+    internal static class FilledInExtensions
+    {
+        public static bool IsNully(this  FlowNode flowNode) => FilledInHelper.IsNully(flowNode);
+        public static bool FilledIn(this FlowNode flowNode) => FilledInHelper.FilledIn(flowNode);
+        
+        public static bool FilledIn(this WavHeaderStruct wavHeader) => FilledInHelper.FilledIn(wavHeader);
+        public static bool IsNully(this  WavHeaderStruct wavHeader) => FilledInHelper.IsNully(wavHeader);
+    }
+}
--- Business/JJ.Business.Synthesizer.Wishes/Helpers/FilledInHelper.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Helpers/FilledInHelper.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using JJ.Business.Synthesizer.Structs;
 using JJ.Framework.Wishes.Common;
 
 namespace JJ.Business.Synthesizer.Wishes.Helpers
 {
-    /// <summary>
-    /// Extensions to the FrameworkWishes' FilledIn methods,
-    /// specific to Synth objects.
-    /// </summary>
-    internal static class FilledInHelper
+    /// <inheritdoc cref="docs._filledinhelper" />
+    internal class FilledInHelper
     {
+        // FlowNode
+        
+        public static bool IsNully(FlowNode flowNode) => !FilledIn(flowNode);
         public static bool Has(FlowNode flowNode) => FilledIn(flowNode);
         public static bool FilledIn(FlowNode flowNode)
         {
@@ -25,5 +26,24 @@ namespace JJ.Business.Synthesizer.Wishes.Helpers
 
             return true;
         }
+        
+        // WavHeader
+        
+        public static bool FilledIn(WavHeaderStruct wavHeader) => !wavHeader.IsNully();
+        public static bool Has(WavHeaderStruct wavHeader) => !wavHeader.IsNully();
+        public static bool IsNully(WavHeaderStruct wavHeader) 
+            => wavHeader.ChunkID        == 0 &&
+               wavHeader.ChunkSize      == 0 &&
+               wavHeader.Format         == 0 &&
+               wavHeader.SubChunk1ID    == 0 &&
+               wavHeader.SubChunk1Size  == 0 &&
+               wavHeader.AudioFormat    == 0 &&
+               wavHeader.ChannelCount   == 0 &&
+               wavHeader.SamplingRate   == 0 &&
+               wavHeader.BytesPerSecond == 0 &&
+               wavHeader.BytesPerSample == 0 &&
+               wavHeader.BitsPerValue   == 0 &&
+               wavHeader.SubChunk2ID    == 0 &&
+               wavHeader.SubChunk2Size  == 0;
     }
 }
