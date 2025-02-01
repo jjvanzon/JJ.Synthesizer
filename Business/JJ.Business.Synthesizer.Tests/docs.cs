d88e4669520466cc546edbb8a4f0613e5ec2d228
jjvanzon <jjvanzon79@gmail.com>
2025-02-01 02:04:34 +0100

    ✍️ Synth Tests: Converted docs members to nested structs so inheritdoc fades into the background with syntax coloring.

--- Business/JJ.Business.Synthesizer.Tests/docs.cs
+++ Business/JJ.Business.Synthesizer.Tests/docs.cs
@@ -1,8 +1,8 @@
 ﻿using wishes = JJ.Business.Synthesizer.Wishes;
+// ReSharper disable UnusedType.Global
 
 #pragma warning disable CS0649
 #pragma warning disable CS0169 // Field is never used
-#pragma warning disable IDE0044
 
 // ReSharper disable InconsistentNaming
 // ReSharper disable IdentifierTypo
@@ -34,13 +34,13 @@ namespace JJ.Business.Synthesizer.Tests
         /// <summary>
         /// Copied from newer JJ.Framework version.
         /// </summary>
-        public static object _copiedfromframework;
+        public struct _copiedfromframework { }
             
         /// <summary>
         /// Beating audible further along the sound.
         /// Mod speed much below sound freq, changes sound freq drastically * [0.5, 1.5]
         /// </summary>
-        public static object _createfmnoisebeating;
+        public struct _createfmnoisebeating { }
         
         /// <summary> Generates a detuned harmonic sound by altering the frequencies slightly. </summary>
         /// <param name="detuneRate">
@@ -48,7 +48,7 @@ namespace JJ.Business.Synthesizer.Tests
         /// If not provided, a default value is used.
         /// </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _detune;
+        public struct _detune { }
 
         /// <summary>
         /// A detuned note characterized by a rich and slightly eerie sound due to the detuned harmonics.
@@ -70,50 +70,50 @@ namespace JJ.Business.Synthesizer.Tests
         /// 2 gives the newer with a move even fade in and out.
         /// </param>
         /// <inheritdoc cref="docs._docs._default" />
-        public static object _detunica;
+        public struct _detunica { }
 
         /// <summary>
         /// Applies an echo effect to the given sound.
         /// </summary>
         /// <param name="sound"> The original sound to which the echo effect will be applied. </param>
         /// <returns> A FlowNode representing the sound with the applied echo effect. </returns>
-        public static object _echo;
+        public struct _echo { }
 
         /// <summary>
         /// When harmonics thicken near the center, this curve can even out the volume over time.
         /// </summary>
-        public static object _evenoutcurve;
+        public struct _evenoutcurve { }
             
         /// <summary>
         /// High hard flute: mod speed above sound freq, changes sound freq * [-0.005, 0.005] (erroneously)
         /// </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _flute1;
+        public struct _flute1 { }
 
         /// <summary> Yet another flute: mod speed above sound freq, changes sound freq * 1 +/- 0.005 </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _flute2;
+        public struct _flute2 { }
 
         /// <summary> Yet another flute: mod speed above sound freq, changes sound freq * 1 +/- 0.005 </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _flute3;
+        public struct _flute3 { }
 
         /// <summary> Modulated hard flute: mod speed below sound freq, changes sound freq * [-0.005, 0.005] (erroneously) </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _flute4;
+        public struct _flute4 { }
 
         /// <summary> FM with (faulty) multiplication around 0. </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _fmaround0;
+        public struct _fmaround0 { }
 
         /// <summary> FM with multiplication around 1. </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _fmaroundfreq;
+        public struct _fmaroundfreq { }
             
         /// <summary> FM sound synthesis modulating with addition. Modulates sound freq to +/- a number of Hz. </summary>
         /// <param name="modDepth"> In Hz </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _fminhertz;
+        public struct _fminhertz { }
 
         /// <summary>
         /// Tests various algorithms for FM sound synthesis, culminating in an atmospheric FM jingle.
@@ -124,7 +124,7 @@ namespace JJ.Business.Synthesizer.Tests
         /// with more dynamic timbres that can be harder to control.
         /// </para>
         /// </summary>
-        public static object _fmtests;
+        public struct _fmtests { }
        
         /// <summary>
         /// <c>From</c>, <c>Init</c> and <c>Source</c> are synonyms.
@@ -139,27 +139,27 @@ namespace JJ.Business.Synthesizer.Tests
         /// </summary>
         /// <param name="freq"> The base frequency of the sound in Hz (default A2/110Hz). </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _horn;
+        public struct _horn { }
 
         /// <summary>
         /// An airy sound with harmonics, a high-pitch sample for attack,
         /// separate curves for each partial, triggers a wav header auto-detect.
         /// </summary>
         /// <inheritdoc cref="docs._docs._default" />
-        public static object _metallophone;
+        public struct _metallophone { }
 
         /// <summary>
         /// A curve that can be applied to the modulator depth to tame the modulation.
         /// In this version of FM synthesis, the modulation depth accumulates over time without such taming.
         /// This is because of a lack of time tracking in the oscillators in this version.
         /// </summary>
-        public static object _modtamingcurve;
+        public struct _modtamingcurve { }
         
         /// <summary>
         /// Loads a sample, skip some old header's bytes, maximize volume and tune to 440Hz.
         /// Returns the initialized Sample if already loaded.
         /// </summary>
-        public static object _mysample;
+        public struct _mysample { }
 
         /// <summary>
         /// <strong> "NullyLen Lullaby" </strong> <br/>
@@ -168,43 +168,43 @@ namespace JJ.Business.Synthesizer.Tests
         /// or explicit specification in the note commands.
         /// Ends with a soothing lullaby to help this big baby calm down after all the bleeps.
         /// </summary>
-        public static object _notelengthfallbacktests;
+        public struct _notelengthfallbacktests { }
         
         /// <summary> Mod speed way below sound freq, changes sound freq * 1 ± 0.005 </summary>
         /// <param name="freq"> The base frequency of the sound in Hz (default A1/55Hz). </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _ripplebass;
+        public struct _ripplebass { }
 
         /// <summary> Mod speed below sound freq, changes sound freq ±10Hz </summary>
         /// <param name="freq"> The base frequency of the sound in Hz (default A3/220Hz). </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _ripplenotesharpmetallic;
+        public struct _ripplenotesharpmetallic { }
 
         /// <summary> Mod speed way below sound freq, changes sound freq * 1 ± 0.005 </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _ripplesoundclean;
+        public struct _ripplesoundclean { }
 
         /// <summary> Mod speed way below sound freq, changes sound freq * 1 ± 0.05 </summary>
         /// <inheritdoc cref="docs._default" />
-        public static object _ripplesoundcooldouble;
+        public struct _ripplesoundcooldouble { }
             
         /// <summary> Mod speed way below sound freq, changes sound freq * 1 ± 0.02 </summary>
         /// <param name="duration"> The audioLength of the sound in seconds (default is 2.5). </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _ripplesoundfantasyeffect;
+        public struct _ripplesoundfantasyeffect { }
         
         /// <summary>
         /// Generates a mild sawtooth-like waveform by combining multiple sine waves with different frequencies.
         /// </summary>
         /// <returns> A FlowNode representing the semi-sawtooth waveform. </returns>
         /// <inheritdoc cref="docs._default" />
-        public static object _semisaw;
+        public struct _semisaw { }
 
         /// <summary> Shapes a ripple effect sound giving it a volume envelope and a delay, volume and audioLength. </summary>
         /// <param name="duration"> The audioLength of the sound in seconds (default is 2.5). </param>
         /// <param name="fmSignal"> A ripple sound to be shaped </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _shaperipplesound;
+        public struct _shaperipplesound { }
 
         /// <summary>
         /// Specifies whether strict validation is applied to ensure consistency between 
@@ -225,15 +225,20 @@ namespace JJ.Business.Synthesizer.Tests
         public struct _strict { }
         
         /// <summary>
+        /// Tests if members can be called without a <c>this.</c> qualifier.
+        /// </summary>
+        public struct _synthwishesderived { }
+        
+        /// <summary>
         /// Testing extension methods in <see cref="docs._AudioFileExtensionWishes" />
         /// that didn't get any coverage elsewhere.
         /// </summary>
-        public static object _testattributewishesold;
+        public struct _testattributewishesold { }
 
         /// <summary>
         /// Aims to test rare exception for code coverage.
         /// </summary>
-        public static object _throwtests;
+        public struct _throwtests { }
         
         /// <summary>
         /// <c>To</c>, <c>Value</c>, <c>Val</c> and <c>Dest</c> are synonyms.
@@ -249,7 +254,7 @@ namespace JJ.Business.Synthesizer.Tests
         /// <param name="freq"> The base frequency of the sound in Hz (default A1/55Hz). </param>
         /// <param name="durationFactor"> Duration varies with pitch, but can be multiplied by this factor (default is 1). </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _trombone;
+        public struct _trombone { }
 
         /// <summary>
         /// Applies a jitter effect to notes, with adjustable depths.
@@ -261,24 +266,24 @@ namespace JJ.Business.Synthesizer.Tests
         /// <param name="depthAdjust1"> The first depth adjustment for the jitter effect. Defaults to 0.005 if not provided. </param>
         /// <param name="depthAdjust2"> The second depth adjustment for the jitter effect. Defaults to 0.250 if not provided. </param>
         /// <inheritdoc cref="docs._default" />
-        public static object _vibraphase;
+        public struct _vibraphase { }
 
         // Aliases for ones in SynthWishes
 
         /// <inheritdoc
         ///     cref="wishes.docs._default" />
-        public static object _default;
+        public struct _default { }
 
         /// <inheritdoc
         ///     cref="wishes.docs._samplingrate" />
-        public static object _samplingrate;
+        public struct _samplingrate { }
 
         /// <inheritdoc
         ///     cref="wishes.docs._tremolo" />
-        public static object _tremolo;
+        public struct _tremolo { }
 
         /// <inheritdoc
         ///     cref="wishes.docs._vibrato" />
-        public static object _vibrato;
+        public struct _vibrato { }
     }
 }
