aeb3984160b1cdeba45d2575d7ddf9e6bc52ea84
jjvanzon <jjvanzon79@gmail.com>
2025-02-15 21:42:31 +0100

    🔮 WavHeaderWishes: Add small accessor and re-enable dependent tests.

---
+++ Business/JJ.Business.Synthesizer.Tests/Accessors/ToWishExtensionsAccessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JJ.Business.Synthesizer.Tests.Helpers;
+using JJ.Business.Synthesizer.Wishes;
+using JJ.Business.Synthesizer.Wishes.Config;
+using JJ.Framework.Reflection;
+using JJ.Framework.Wishes.Reflection;
+
+namespace JJ.Business.Synthesizer.Tests.Accessors
+{
+    internal static class ToWishExtensionsAccessor
+    {
+        private static readonly AccessorEx _accessor = new AccessorEx(typeof(ToWishExtensions));
+        
+        internal static AudioInfoWish ToWish(this ConfigResolverAccessor entity, SynthWishes synthWishes)
+            => _accessor.InvokeMethod<AudioInfoWish>(entity?.Obj, synthWishes);
+        
+        internal static AudioInfoWish ToWish(this ConfigSectionAccessor entity)            
+            => _accessor.InvokeMethod<AudioInfoWish>(entity?.Obj);
+    }
+}
+
