8b3050a0c7ec0786426dd214da8ad72f0404968e
jjvanzon <jjvanzon79@gmail.com>
2025-02-19 19:47:33 +0100

    🧪 Tests: WavHeaderWishes - Add remaining FromWish tests requiring accessor methods. Adjust whitespace for better readability in table layout.

--- Business/JJ.Business.Synthesizer.Tests/Accessors/WavHeaderWishesAccessor.cs
+++ Business/JJ.Business.Synthesizer.Tests/Accessors/WavHeaderWishesAccessor.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using JJ.Business.Synthesizer.Wishes;
 using JJ.Business.Synthesizer.Wishes.Config;
+using JJ.Framework.Reflection;
 using JJ.Framework.Wishes.Reflection;
 
 namespace JJ.Business.Synthesizer.Tests.Accessors
@@ -13,10 +14,21 @@ namespace JJ.Business.Synthesizer.Tests.Accessors
     {
         private static AccessorEx _accessor = new AccessorEx(typeof(WavHeaderWishes));
         
+        // With ConfigResolver
+        
         internal static AudioInfoWish ToWish(ConfigResolverAccessor entity, SynthWishes synthWishes) 
             => _accessor.InvokeMethod<AudioInfoWish>(entity.Obj, synthWishes);
 
+        internal static void ApplyTo(AudioInfoWish infoWish, ConfigResolverAccessor entity, SynthWishes synthWishes) 
+            => _accessor.InvokeMethod(infoWish, entity.Obj, synthWishes);
+
+        internal static void FromWish(ConfigResolverAccessor entity, AudioInfoWish infoWish, SynthWishes synthWishes)
+            => _accessor.InvokeMethod(entity.Obj, infoWish, synthWishes);
+        
+        // With ConfigSection
+        
         internal static AudioInfoWish ToWish(ConfigSectionAccessor entity)
             => _accessor.InvokeMethod<AudioInfoWish>(entity.Obj);
+
     }
 }
