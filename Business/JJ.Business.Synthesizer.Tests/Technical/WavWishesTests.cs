206c944b6fca2693f449acb8d27ba165cb344411
jjvanzon <jjvanzon79@gmail.com>
2025-02-22 19:30:00 +0100

    🧹 [Draft] Tests: WavWishes - Remove redundant setter cases after parameter removal streamlined them.

--- Business/JJ.Business.Synthesizer.Tests/Technical/WavWishesTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/Technical/WavWishesTests.cs
@@ -912,14 +912,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction     , () => x.TapeBound.TapeAction       .WriteWavHeader(bin.DestBytes                 )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction     , () => x.TapeBound.TapeAction       .WriteWavHeader(bin.DestStream                )), ForDestStream,   test);
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction     , () => x.TapeBound.TapeAction       .WriteWavHeader(bin.BinaryWriter              )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff           , () => x.BuffBound.Buff             .WriteWavHeader(bin.DestFilePath              )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff           , () => x.BuffBound.Buff             .WriteWavHeader(bin.DestBytes                 )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff           , () => x.BuffBound.Buff             .WriteWavHeader(bin.DestStream                )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff           , () => x.BuffBound.Buff             .WriteWavHeader(bin.BinaryWriter              )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => x.BuffBound.AudioFileOutput  .WriteWavHeader(bin.DestFilePath              )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => x.BuffBound.AudioFileOutput  .WriteWavHeader(bin.DestBytes                 )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => x.BuffBound.AudioFileOutput  .WriteWavHeader(bin.DestStream                )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => x.BuffBound.AudioFileOutput  .WriteWavHeader(bin.BinaryWriter              )), ForBinaryWriter, test); // By Design: explicit FrameCount() call omitted; would fall between two internal WriteWavHeader calls.
             AssertWrite(bin => AreEqual(x.Independent.Sample       , () => x.Independent.Sample         .WriteWavHeader(bin.DestFilePath              )), ForDestFilePath, test);
             AssertWrite(bin => AreEqual(x.Independent.Sample       , () => x.Independent.Sample         .WriteWavHeader(bin.DestBytes                 )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(x.Independent.Sample       , () => x.Independent.Sample         .WriteWavHeader(bin.DestStream                )), ForDestStream,   test);
@@ -968,14 +960,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             AssertWrite(bin => AreEqual(bin.DestBytes              , () => bin.DestBytes   .WriteWavHeader(x.TapeBound.TapeAction                     )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(bin.DestStream             , () => bin.DestStream  .WriteWavHeader(x.TapeBound.TapeAction                     )), ForDestStream,   test);
             AssertWrite(bin => AreEqual(bin.BinaryWriter           , () => bin.BinaryWriter.WriteWavHeader(x.TapeBound.TapeAction                     )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(bin.DestFilePath           , () => bin.DestFilePath.WriteWavHeader(x.BuffBound.Buff                           )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(bin.DestBytes              , () => bin.DestBytes   .WriteWavHeader(x.BuffBound.Buff                           )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(bin.DestStream             , () => bin.DestStream  .WriteWavHeader(x.BuffBound.Buff                           )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(bin.BinaryWriter           , () => bin.BinaryWriter.WriteWavHeader(x.BuffBound.Buff                           )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(bin.DestFilePath           , () => bin.DestFilePath.WriteWavHeader(x.BuffBound.AudioFileOutput                )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(bin.DestBytes              , () => bin.DestBytes   .WriteWavHeader(x.BuffBound.AudioFileOutput                )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(bin.DestStream             , () => bin.DestStream  .WriteWavHeader(x.BuffBound.AudioFileOutput                )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(bin.BinaryWriter           , () => bin.BinaryWriter.WriteWavHeader(x.BuffBound.AudioFileOutput                )), ForBinaryWriter, test); // By Design: explicit FrameCount() call omitted; would fall between two internal WriteWavHeader calls.
             AssertWrite(bin => AreEqual(bin.DestFilePath           , () => bin.DestFilePath.WriteWavHeader(x.Independent.Sample                       )), ForDestFilePath, test);
             AssertWrite(bin => AreEqual(bin.DestBytes              , () => bin.DestBytes   .WriteWavHeader(x.Independent.Sample                       )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(bin.DestStream             , () => bin.DestStream  .WriteWavHeader(x.Independent.Sample                       )), ForDestStream,   test);
@@ -1024,14 +1008,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction,      () => WriteWavHeader(x.TapeBound.TapeAction,        bin.DestBytes                )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction,      () => WriteWavHeader(x.TapeBound.TapeAction,        bin.DestStream               )), ForDestStream,   test);
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction,      () => WriteWavHeader(x.TapeBound.TapeAction,        bin.BinaryWriter             )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WriteWavHeader(x.BuffBound.Buff,              bin.DestFilePath             )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WriteWavHeader(x.BuffBound.Buff,              bin.DestBytes                )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WriteWavHeader(x.BuffBound.Buff,              bin.DestStream               )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WriteWavHeader(x.BuffBound.Buff,              bin.BinaryWriter             )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.DestFilePath             )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.DestBytes                )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.DestStream               )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.BinaryWriter             )), ForBinaryWriter, test); // By Design: explicit FrameCount() call omitted; would fall between two internal WriteWavHeader calls.
             AssertWrite(bin => AreEqual(x.Independent.Sample,        () => WriteWavHeader(x.Independent.Sample,          bin.DestFilePath             )), ForDestFilePath, test);
             AssertWrite(bin => AreEqual(x.Independent.Sample,        () => WriteWavHeader(x.Independent.Sample,          bin.DestBytes                )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(x.Independent.Sample,        () => WriteWavHeader(x.Independent.Sample,          bin.DestStream               )), ForDestStream,   test);
@@ -1080,14 +1056,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             AssertWrite(bin => AreEqual(bin.DestBytes,               () => WriteWavHeader(bin.DestBytes,    x.TapeBound.TapeAction                    )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(bin.DestStream,              () => WriteWavHeader(bin.DestStream,   x.TapeBound.TapeAction                    )), ForDestStream,   test);
             AssertWrite(bin => AreEqual(bin.BinaryWriter,            () => WriteWavHeader(bin.BinaryWriter, x.TapeBound.TapeAction                    )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(bin.DestFilePath,            () => WriteWavHeader(bin.DestFilePath, x.BuffBound.Buff                          )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(bin.DestBytes,               () => WriteWavHeader(bin.DestBytes,    x.BuffBound.Buff                          )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(bin.DestStream,              () => WriteWavHeader(bin.DestStream,   x.BuffBound.Buff                          )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(bin.BinaryWriter,            () => WriteWavHeader(bin.BinaryWriter, x.BuffBound.Buff                          )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(bin.DestFilePath,            () => WriteWavHeader(bin.DestFilePath, x.BuffBound.AudioFileOutput               )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(bin.DestBytes,               () => WriteWavHeader(bin.DestBytes,    x.BuffBound.AudioFileOutput               )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(bin.DestStream,              () => WriteWavHeader(bin.DestStream,   x.BuffBound.AudioFileOutput               )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(bin.BinaryWriter,            () => WriteWavHeader(bin.BinaryWriter, x.BuffBound.AudioFileOutput               )), ForBinaryWriter, test); // By Design: explicit FrameCount() call omitted; would fall between two internal WriteWavHeader calls.
             AssertWrite(bin => AreEqual(bin.DestFilePath,            () => WriteWavHeader(bin.DestFilePath, x.Independent.Sample                      )), ForDestFilePath, test);
             AssertWrite(bin => AreEqual(bin.DestBytes,               () => WriteWavHeader(bin.DestBytes,    x.Independent.Sample                      )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(bin.DestStream,              () => WriteWavHeader(bin.DestStream,   x.Independent.Sample                      )), ForDestStream,   test);
@@ -1136,14 +1104,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction,      () => WavWishes.WriteWavHeader(x.TapeBound.TapeAction,        bin.DestBytes   )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction,      () => WavWishes.WriteWavHeader(x.TapeBound.TapeAction,        bin.DestStream  )), ForDestStream,   test);
             AssertWrite(bin => AreEqual(x.TapeBound.TapeAction,      () => WavWishes.WriteWavHeader(x.TapeBound.TapeAction,        bin.BinaryWriter)), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WavWishes.WriteWavHeader(x.BuffBound.Buff,              bin.DestFilePath)), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WavWishes.WriteWavHeader(x.BuffBound.Buff,              bin.DestBytes   )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WavWishes.WriteWavHeader(x.BuffBound.Buff,              bin.DestStream  )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(x.BuffBound.Buff,            () => WavWishes.WriteWavHeader(x.BuffBound.Buff,              bin.BinaryWriter)), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WavWishes.WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.DestFilePath)), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WavWishes.WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.DestBytes   )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WavWishes.WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.DestStream  )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(x.BuffBound.AudioFileOutput, () => WavWishes.WriteWavHeader(x.BuffBound.AudioFileOutput,   bin.BinaryWriter)), ForBinaryWriter, test); // By Design: explicit FrameCount() call omitted; would fall between two internal WriteWavHeader calls.
             AssertWrite(bin => AreEqual(x.Independent.Sample,        () => WavWishes.WriteWavHeader(x.Independent.Sample,          bin.DestFilePath)), ForDestFilePath, test);
             AssertWrite(bin => AreEqual(x.Independent.Sample,        () => WavWishes.WriteWavHeader(x.Independent.Sample,          bin.DestBytes   )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(x.Independent.Sample,        () => WavWishes.WriteWavHeader(x.Independent.Sample,          bin.DestStream  )), ForDestStream,   test);
@@ -1192,14 +1152,6 @@ namespace JJ.Business.Synthesizer.Tests.Technical
             AssertWrite(bin => AreEqual(bin.DestBytes,               () => WavWishes.WriteWavHeader(bin.DestBytes,    x.TapeBound.TapeAction       )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(bin.DestStream,              () => WavWishes.WriteWavHeader(bin.DestStream,   x.TapeBound.TapeAction       )), ForDestStream,   test);
             AssertWrite(bin => AreEqual(bin.BinaryWriter,            () => WavWishes.WriteWavHeader(bin.BinaryWriter, x.TapeBound.TapeAction       )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(bin.DestFilePath,            () => WavWishes.WriteWavHeader(bin.DestFilePath, x.BuffBound.Buff             )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(bin.DestBytes,               () => WavWishes.WriteWavHeader(bin.DestBytes,    x.BuffBound.Buff             )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(bin.DestStream,              () => WavWishes.WriteWavHeader(bin.DestStream,   x.BuffBound.Buff             )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(bin.BinaryWriter,            () => WavWishes.WriteWavHeader(bin.BinaryWriter, x.BuffBound.Buff             )), ForBinaryWriter, test);
-            AssertWrite(bin => AreEqual(bin.DestFilePath,            () => WavWishes.WriteWavHeader(bin.DestFilePath, x.BuffBound.AudioFileOutput  )), ForDestFilePath, test);
-            AssertWrite(bin => AreEqual(bin.DestBytes,               () => WavWishes.WriteWavHeader(bin.DestBytes,    x.BuffBound.AudioFileOutput  )), ForDestBytes,    test);
-            AssertWrite(bin => AreEqual(bin.DestStream,              () => WavWishes.WriteWavHeader(bin.DestStream,   x.BuffBound.AudioFileOutput  )), ForDestStream,   test);
-            AssertWrite(bin => AreEqual(bin.BinaryWriter,            () => WavWishes.WriteWavHeader(bin.BinaryWriter, x.BuffBound.AudioFileOutput  )), ForBinaryWriter, test); // By Design: explicit FrameCount() call omitted; would fall between two internal WriteWavHeader calls.
             AssertWrite(bin => AreEqual(bin.DestFilePath,            () => WavWishes.WriteWavHeader(bin.DestFilePath, x.Independent.Sample         )), ForDestFilePath, test);
             AssertWrite(bin => AreEqual(bin.DestBytes,               () => WavWishes.WriteWavHeader(bin.DestBytes,    x.Independent.Sample         )), ForDestBytes,    test);
             AssertWrite(bin => AreEqual(bin.DestStream,              () => WavWishes.WriteWavHeader(bin.DestStream,   x.Independent.Sample         )), ForDestStream,   test);
