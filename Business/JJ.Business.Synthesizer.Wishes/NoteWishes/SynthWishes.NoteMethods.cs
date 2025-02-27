0272b3be484739ec66a59805b3086628042493d0
jjvanzon <jjvanzon79@gmail.com>
2025-02-27 20:30:34 +0100

    🍌 Split SynthWishes' NoteWishes up into 4 files each covering one concern.

---
+++ Business/JJ.Business.Synthesizer.Wishes/NoteWishes/SynthWishes.NoteMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using JJ.Business.Synthesizer.Extensions;
+using static JJ.Framework.Wishes.Common.FilledInWishes;
+using static JJ.Business.Synthesizer.Wishes.NameWishes;
+
+// ReSharper disable once CheckNamespace
+namespace JJ.Business.Synthesizer.Wishes
+{
+    public partial class SynthWishes
+    {
+        // Note Operator
+        
+        /// <inheritdoc cref="docs._note" />
+        public FlowNode Note(
+            FlowNode sound, FlowNode delay = default, FlowNode volume = default, FlowNode noteLength = default, 
+            string name = null, [CallerMemberName] string callerMemberName = null)
+        {
+            // A little optimization, because so slow...
+            bool delayFilledIn = delay != null && delay.AsConst != 0;
+            bool volumeFilledIn = volume != null && volume.AsConst != 1;
+
+            // Resolve Name
+            string resolvedName = ResolveName(name, sound, callerMemberName);
+            if (FilledIn(resolvedName))
+            {
+                resolvedName += " " + MemberName();
+            }
+            
+            // Resolve NoteLength
+            noteLength = GetNoteLengthSnapShot(noteLength);
+            
+            // Apply Volume
+            if (volumeFilledIn)
+            {
+                sound *= volume.Stretch(noteLength / GetVolumeDuration(volume));
+            }
+            
+            // Defer Taping
+            sound = sound.Tape(noteLength).SetName(resolvedName);
+            
+            // Apply Delay
+            if (delayFilledIn) sound = Delay(sound, delay);
+            
+            // Extend AudioLength
+            EnsureAudioLength(delay + noteLength);
+            
+            return sound.SetName(resolvedName);
+        }
+        
+        /// <inheritdoc cref="docs._note" />
+        public FlowNode Note(
+            FlowNode sound, FlowNode delay, double volume, FlowNode noteLength, 
+            string name = null, [CallerMemberName] string callerMemberName = null)
+            => Note(sound, delay, _[volume], noteLength, name, callerMemberName);
+        
+        /// <inheritdoc cref="docs._note" />
+        public FlowNode Note(
+            FlowNode sound, FlowNode delay, double volume,
+            string name = null, [CallerMemberName] string callerMemberName = null) 
+            => Note(sound, delay, _[volume], default, name, callerMemberName);
+               
+        private static double GetVolumeDuration(FlowNode volume)
+        {
+            if (volume.IsCurve)
+            {
+                return volume.UnderlyingCurve().Nodes.Max(x => x.Time);
+            }
+            
+            if (volume.IsSample)
+            {
+                return volume.UnderlyingSample().GetDuration();
+            }
+            
+            return 1;
+        }
+    }
+}
+
---
+++ Business/JJ.Business.Synthesizer.Wishes/NoteWishes/SynthWishes.Timing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JJ.Business.Synthesizer.Wishes.NoteWishes;
+
+// ReSharper disable once CheckNamespace
+namespace JJ.Business.Synthesizer.Wishes
+{
+    public partial class SynthWishes
+    {
+        // Timing
+        
+        /// <inheritdoc cref="docs._barindexer"/>
+        public BarIndexer bar { get; }
+        /// <inheritdoc cref="docs._barsindexer"/>
+        public BarsIndexer bars { get; }
+        /// <inheritdoc cref="docs._beatindexer"/>
+        public BeatIndexer beat { get; }
+        /// <inheritdoc cref="docs._beatindexer"/>
+        public BeatIndexer b { get; }
+        /// <inheritdoc cref="docs._beatsindexer"/>
+        public BeatsIndexer beats { get; }
+        /// <inheritdoc cref="docs._timeindexer"/>
+        public TimeIndexer t { get; }
+        /// <inheritdoc cref="docs._beatsindexer"/>
+        public BeatsIndexer l { get; }
+        /// <inheritdoc cref="docs._beatsindexer"/>
+        public BeatsIndexer len { get; }
+        /// <inheritdoc cref="docs._beatsindexer"/>
+        public BeatsIndexer length { get; }
+    }
+}
