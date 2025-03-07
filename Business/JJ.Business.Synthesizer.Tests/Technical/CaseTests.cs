9f6012884c065a4aeb333617bb846c4ed7022d4a
jjvanzon <jjvanzon79@gmail.com>
2025-03-07 23:43:51 +0100

    🧹 [Draft] Tests: Lingering case where LogWishes was still directly used. Replaced that with use of SynthWishes for logging.

--- Business/JJ.Business.Synthesizer.Tests/Technical/CaseTests.cs
+++ Business/JJ.Business.Synthesizer.Tests/Technical/CaseTests.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using JJ.Business.Synthesizer.Tests.ConfigTests;
-using JJ.Business.Synthesizer.Tests.Helpers;
-using JJ.Business.Synthesizer.Wishes.Logging;
+using JJ.Business.Synthesizer.Wishes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using static JJ.Business.Synthesizer.Wishes.Logging.LogWishes;
 using static JJ.Framework.Testing.AssertHelper;
 
 namespace JJ.Business.Synthesizer.Tests.Technical
 {
     [TestClass]
-    public class CaseTests
+    public class CaseTests : SynthWishes
     {
         [TestMethod]
         public void Test_Case_Key()
         { 
             var    testCase = new FrameCountWishesTests.Case();
             string key      = testCase.Key;
-            LogWishes.Static.Log(key);
+            Log(key);
             NotNullOrEmpty(() => key);
         }
     }
