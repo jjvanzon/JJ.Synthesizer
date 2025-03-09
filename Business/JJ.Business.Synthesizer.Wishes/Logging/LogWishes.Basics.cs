17555c09eab2c88eaee86dd6ffe7786c3196d876
jjvanzon <jjvanzon79@gmail.com>
2025-03-09 21:14:16 +0100

    🚚 Moved SynthWishes log basics from LogWishes.Basics.cs to LogWishes.SynthWishes.cs. While it doesn’t strictly group all SynthWishes logging, the new location makes it easier to find and gives a better overview.

--- Business/JJ.Business.Synthesizer.Wishes/Logging/LogWishes.Basics.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Logging/LogWishes.Basics.cs
@@ -7,16 +7,13 @@ using JJ.Framework.Wishes.Logging.Loggers;
 using JJ.Framework.Wishes.Text;
 using JJ.Persistence.Synthesizer;
 using JJ.Business.Synthesizer.Wishes.Config;
-using JJ.Business.Synthesizer.Wishes.Logging;
 using JJ.Business.Synthesizer.Wishes.TapeWishes;
-using JJ.Framework.Wishes.Collections;
 using static System.Environment;
 using static JJ.Business.Synthesizer.Wishes.Logging.LogCategories;
 using static JJ.Framework.Wishes.Text.StringWishes;
 using static JJ.Business.Synthesizer.Wishes.NameWishes;
 using static JJ.Framework.Wishes.Logging.LoggingFactory;
 using static JJ.Business.Synthesizer.Wishes.Logging.LogWishes;
-using JJ.Framework.Wishes.Logging.Config;
 
 namespace JJ.Business.Synthesizer.Wishes.Logging
 {
@@ -151,119 +148,10 @@ namespace JJ.Business.Synthesizer.Wishes.Logging
     }
 }
 
-// ReSharper disable once CheckNamespace
-namespace JJ.Business.Synthesizer.Wishes
-{
-    public partial class SynthWishes
-    {
-        /// <summary>
-        /// Always filled in. Holds the main LogWishes instance to use.
-        /// </summary>
-        internal LogWishes Logging { get; }
-
-        // TODO: Synonyms
-        
-        //public SynthWishes WithLog(bool enabled = true){ Logging.Enabled = enabled; return this; }
-        //public SynthWishes WithLogEnabled() => WithLog(true);
-        //public SynthWishes WithLogDisabled() => WithLog(false);
-        //public SynthWishes WithLogCats(params string[] categories) { Logging.SetCategories(categories); return this; }
-        
-        // Defined here to ditch `this` qualifier in case of inheritance.
-        
-        public   void Log           (                 string message = null) => Logging.Log(message);
-        internal void Log           (string category, string message       ) => Logging.Log(category, message);
-        public   void LogSpaced     (                 string message       ) => Logging.LogSpaced(message);
-        internal void LogSpaced     (string category, string message       ) => Logging.LogSpaced(category, message);
-        public   void LogTitle      (                 string title         ) => Logging.LogTitle(title);
-        internal void LogTitle      (string category, string title         ) => Logging.LogTitle(category, title);
-        public   void LogTitleStrong(                 string title         ) => Logging.LogTitleStrong(title);
-        internal void LogTitleStrong(string category, string title         ) => Logging.LogTitleStrong(category, title);
-        
-        public SynthWishes SetLogActive(bool value)
-        {
-            _config.SetLogActive(value);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-        
-        public SynthWishes WithLog()
-        {
-            _config.WithLog();
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-        
-        public SynthWishes NoLog()
-        {
-            _config.NoLog();
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-        
-        public SynthWishes WithLogCats(params string[] categories)
-        {
-            _config.WithLogCats(categories);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-        
-        public SynthWishes WithLogCats(IList<string> categories)
-        {
-            _config.WithLogCats(categories);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-        
-        public SynthWishes AddLogCat(string category)
-        {
-            _config.AddLogCat(category);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-            
-        public SynthWishes AddLogCats(params string[] categories)
-        {
-            _config.AddLogCats(categories);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-        
-        public SynthWishes AddLogCats(IList<string> categories)
-        {
-            _config.AddLogCats(categories);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-
-        public SynthWishes RemoveLogCat(string category)
-        {
-            _config.RemoveLogCat(category);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-
-        public SynthWishes RemoveLogCats(params string[] categories) 
-        {
-            _config.RemoveLogCats(categories);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-            
-        public SynthWishes RemoveLogCats(IList<string> categories)
-        {
-            _config.RemoveLogCats(categories);
-            Logging.UpdateLogger(_config);
-            return this;
-        }
-
-    }
-}
-
 namespace JJ.Business.Synthesizer.Wishes.Logging
 {
     public static partial class LogExtensionWishes
     {
-
         // The target objects aren't used for anything other than resolving a SynthWishes object,
         // and availability on multiple target types for convenience.
         
