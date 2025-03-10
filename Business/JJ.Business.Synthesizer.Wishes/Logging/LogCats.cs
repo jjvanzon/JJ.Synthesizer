6e4d852c426cff6e9de5c5db1900f8bbad480daa
jjvanzon <jjvanzon79@gmail.com>
2025-03-10 01:51:47 +0100

    ✏️ Trivials

--- Business/JJ.Business.Synthesizer.Wishes/Logging/LogCats.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Logging/LogCats.cs
@@ -9,14 +9,14 @@ namespace JJ.Business.Synthesizer.Wishes.Logging
     public static class LogCats
     {
         public static readonly string MathBoost = MemberName();
-        public static readonly string SynthLog = MemberName();
-        public static readonly string TapeTree = MemberName();
-        public static readonly string Config = MemberName();
-        public static readonly string Actions = MemberName();
-        public static readonly string Out = MemberName();
-        public static readonly string File = MemberName();
-        public static readonly string Memory = MemberName();
-        public static readonly string Misc = MemberName();
-        public static readonly string Operator = MemberName();
+        public static readonly string SynthLog  = MemberName();
+        public static readonly string TapeTree  = MemberName();
+        public static readonly string Config    = MemberName();
+        public static readonly string Actions   = MemberName();
+        public static readonly string Out       = MemberName();
+        public static readonly string File      = MemberName();
+        public static readonly string Memory    = MemberName();
+        public static readonly string Misc      = MemberName();
+        public static readonly string Operator  = MemberName();
     }
 }
