3387ca579b621d60a24f08455b7635cc9551acc4
jjvanzon <jjvanzon79@gmail.com>
2025-03-15 18:49:15 +0100

    🚀 Upgrade Synth Tests & SynthWishes to .NET 9. Added temporary System.Windows.Extensions dependency for WAV playback, to be replaced later with a more compatible solution.

--- Business/JJ.Business.Synthesizer.Tests/Properties/AssemblyInfo.cs
+++ Business/JJ.Business.Synthesizer.Tests/Properties/AssemblyInfo.cs
@@ -2,16 +2,6 @@
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics.CodeAnalysis;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JJ.Business.Synthesizer.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JJ.Business.Synthesizer.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2015 - 2025 Jan Joost van Zon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,19 +12,6 @@ using System.Diagnostics.CodeAnalysis;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("13f29762-103c-4ff9-b178-e6c5202d1254")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)] 
 [assembly: SuppressMessage("MSTest", "MSTEST0018", Justification = "CaseCollection can be used as DynamicData.")]
--- Business/JJ.Business.Synthesizer.Wishes/Properties/AssemblyInfo.cs
+++ Business/JJ.Business.Synthesizer.Wishes/Properties/AssemblyInfo.cs
@@ -2,16 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Diagnostics.CodeAnalysis;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JJ.Business.Synthesizer.Wishes")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JJ.Business.Synthesizer.Wishes")]
-[assembly: AssemblyCopyright("Copyright © JJ van Zon 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,16 +12,3 @@ using System.Diagnostics.CodeAnalysis;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("13f29762-103c-4ff9-b178-e6c5202d1254")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
