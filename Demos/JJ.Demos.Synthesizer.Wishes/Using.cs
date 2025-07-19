805c34e5c92656776e119c412ce20f2dc580bfd8
jjvanzon <jjvanzon79@gmail.com>
2025-07-19 21:35:48 +0200

    📛 SynthWishes Demo: Added global usings for system namespaces.

--- Demos/JJ.Demos.Synthesizer.Wishes/Using.cs
+++ Demos/JJ.Demos.Synthesizer.Wishes/Using.cs
@@ -1,3 +1,7 @@
-﻿global using JJ.Persistence.Synthesizer;
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using JJ.Persistence.Synthesizer;
 global using JJ.Business.Synthesizer.Wishes;
 global using static System.Console;
