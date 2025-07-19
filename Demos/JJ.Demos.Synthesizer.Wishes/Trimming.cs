b1e1fc5a2e21a23951fb9c5ac49eb26f145287ce
jjvanzon <jjvanzon79@gmail.com>
2025-07-19 21:53:57 +0200

    🧹 SynthWishes Demo: Add (outcommented) Trimming.cs to exclude classes centrally from trimming.

---
+++ Demos/JJ.Demos.Synthesizer.Wishes/Trimming.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+using System.Diagnostics.CodeAnalysis;
+using static System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes;
+
+namespace JJ.Demos.Synthesizer.Wishes;
+
+internal static class Trimming
+{
+    //[DynamicDependency(All, "System.Configuration.ClientConfigurationHost", "System.Configuration.ConfigurationManager")]
+    internal static void Trim() { }
+}
